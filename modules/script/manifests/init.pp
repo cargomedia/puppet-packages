b2e7ea015ea7b9ed8fbae6d33502ae3a63199984
Tomasz Durka <tomasz@durka.pl>
2013-06-12 02:45:49 +0200

    md5 content for script name

--- modules/script/manifests/init.pp
+++ modules/script/manifests/init.pp
@@ -2,13 +2,16 @@ define script ($content, $unless = false, $onlyif = true) {
 
 	if !$unless and $onlyif {
 
-		file { 'scriptfile':
-			path => "/tmp/script",
+		$script = md5($content)
+
+		file { "/tmp/script-${script}":
 			content => $content,
 			mode => 755,
 		}
+		->
 
-		exec {'/tmp/script':
+		exec {'run script':
+			command => "/tmp/script-${script}",
 			path => ['/usr/local/bin', '/usr/bin', '/bin']
 		}
 	}
