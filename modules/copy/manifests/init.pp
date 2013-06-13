feefd72e187534722799394a20e1943f9e414e05
Tomasz Durka <tomasz@durka.pl>
2013-06-13 20:49:35 +0200

    copy: allow template instead of files

--- modules/copy/manifests/init.pp
+++ modules/copy/manifests/init.pp
@@ -1,7 +1,14 @@
-define copy ($file = $title, $module) {
-	file { $file:
-		source => "puppet:///modules/${module}${file}",
-		ensure => present,
-		recurse => true,
+define copy ($file = $title, $module, $template = false) {
+	if $template {
+		file { $file:
+			content => template("${module}${file}.erb"),
+			ensure => present,
+		}
+	} else {
+		file { $file:
+			source => "puppet:///modules/${module}${file}",
+			ensure => present,
+			recurse => true,
+		}
 	}
 }
