8028045f8181e994d80c780851d57763af2434e7
Tomasz Durka <tomasz@durka.pl>
2013-06-13 18:32:05 +0200

    debian: add sources list

--- modules/debian/manifests/base.pp
+++ modules/debian/manifests/base.pp
@@ -2,6 +2,11 @@ class debian::base {
 
 	require 'php53::cli'
 
+	file { '/etc/apt/sources.list':
+		source => 'puppet:///modules/debian/sources.list',
+		ensure => present,
+	}
+
 	$packages = split(template('debian/dpkg.list'), "\n")
 	package { $packages: ensure => installed }
 
