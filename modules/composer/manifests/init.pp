01109df08b32621be993dcace5296c9845fd743f
Tomasz Durka <tomasz@durka.pl>
2013-06-13 15:36:40 +0200

    Rename actions

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -2,16 +2,15 @@ class composer($version = '1.0.0-alpha7') {
 
 	require debian::base
 
-	exec {'download':
+	exec {'curl /usr/local/lib/composer.phar':
 		command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > /usr/local/lib/composer.phar",
 		creates => '/usr/local/lib/composer.phar',
 		path => ['/usr/local/bin', '/usr/bin', '/bin'],
 		require => Package['curl'],
 	}
 
-	file {'composer':
+	file {'/usr/local/bin/composer':
 		source => 'puppet:///modules/composer/composer.sh',
-		path => '/usr/local/bin/composer',
 		mode => 0755,
 		ensure => present,
 		require => [Exec['download'], Package['php5-cli']]
