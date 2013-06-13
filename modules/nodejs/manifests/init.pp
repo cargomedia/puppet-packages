b0b2426deb37eb94ca9b8ed9ecb67b827580f09c
Tomasz Durka <tomasz@durka.pl>
2013-06-13 16:48:54 +0200

    Fix nodejs user creation

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -3,13 +3,11 @@ class nodejs ($version = '0.10.4') {
 	require debian::base
 
 	package { ['python', 'libevent-1.4-2', 'libssl-dev']: ensure => present }
-	->
 
 	user {'nodejs':
+		ensure => present,
 		system => true,
-		# gid => 'nodejs',
 	}
-	->
 
 	script {'install nodejs':
 		content => template('nodejs/install.erb'),
