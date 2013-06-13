832f0f0f82da3ad72e5b8ffc01246c1465662e6c
Tomasz Durka <tomasz@durka.pl>
2013-06-13 19:31:36 +0200

    debian: add base monits and files

--- modules/debian/manifests/base.pp
+++ modules/debian/manifests/base.pp
@@ -1,6 +1,9 @@
 class debian::base {
 
 	require 'php53::cli'
+	require 'monit'
+
+	$ipPrivate = hiera('ipPrivate')
 
 	file { '/etc/apt/':
 		source => 'puppet:///modules/debian/etc/apt',
@@ -14,7 +17,43 @@ class debian::base {
 		ensure => present,
 	}
 
-	$packages = split(template('debian/dpkg.list'), "\n")
+	file { '/etc/security':
+		source => 'puppet:///modules/debian/etc/security',
+		recurse => true,
+		ensure => present,
+	}
+
+	file { '/etc/ssh':
+		source => 'puppet:///modules/debian/etc/ssh',
+		recurse => true,
+		ensure => present,
+	}
+
+	monit::entry { 'monit cron':
+		name => 'cron',
+		content => template('debian/monits/cron.erb'),
+		ensure => present
+	}
+
+	monit::entry { 'monit postfix':
+		name => 'postfix',
+		content => template('debian/monits/postfix.erb'),
+		ensure => present
+	}
+
+	monit::entry { 'monit snmpd':
+		name => 'snmpd',
+		content => template('debian/monits/snmpd.erb'),
+		ensure => present
+	}
+
+	monit::entry { 'monit system':
+		name => 'system',
+		content => template('debian/monits/system.erb'),
+		ensure => present
+	}
+
+	$packages = split(template('debian/dpkg.list.erb'), "\n")
 	package { $packages: ensure => installed }
 
 }
