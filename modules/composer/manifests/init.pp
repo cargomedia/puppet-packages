e672e7502d6eac0410222f9e274adcf550eb67b4
Tomasz Durka <tomasz@durka.pl>
2013-06-13 18:24:40 +0200

    composer: Add composer.ini file to

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -6,13 +6,17 @@ class composer($version = '1.0.0-alpha7') {
 		command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > /usr/local/lib/composer.phar",
 		creates => '/usr/local/lib/composer.phar',
 		path => ['/usr/local/bin', '/usr/bin', '/bin'],
-		require => Package['curl'],
 	}
+	->
 
 	file {'/usr/local/bin/composer':
 		source => 'puppet:///modules/composer/composer.sh',
 		mode => 0755,
 		ensure => present,
-		require => [Exec['download'], Package['php5-cli']]
+	}
+
+	file {'/etc/php5/conf.d/composer.ini':
+		source => 'puppet:///modules/composer/composer.ini',
+		ensure => present,
 	}
 }
