7601b0b1a14173878efa3bf295ecc1d1410c0acf
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-06-21 16:34:35 +0200

    composer: Fix paths, add requires

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -1,20 +1,23 @@
 class composer($version = '1.0.0-alpha7') {
 
-	$binary = '/usr/local/lib/composer.phar'
+	$phar = '/usr/local/lib/composer.phar'
+	$binary = '/usr/local/bin/composer'
+	$config = '/etc/php5/conf.d/composer.ini'
 
-	exec {"curl ${binary}":
-		command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > ${binary}",
+	exec {"curl ${phar}":
+		command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > ${phar}",
 		path => ['/usr/local/bin', '/usr/bin', '/bin'],
-		unless => "test -f ${binary} && ${binary} --version | grep -w '${version}'"
+		unless => "test -f ${phar} && ${binary} --version | grep -w '${version}'",
+		require => [File[$binary], File[$config]],
 	}
 
-	file {'/usr/local/bin/composer':
+	file {$binary:
 		source => 'puppet:///modules/composer/composer.sh',
 		mode => 0755,
 		ensure => present,
 	}
 
-	file {'/etc/php5/conf.d/composer.ini':
+	file {$config:
 		source => 'puppet:///modules/composer/composer.ini',
 		ensure => present,
 	}
