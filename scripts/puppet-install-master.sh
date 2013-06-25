ad70a1dfe20d55af515a23b386d615331571b39c
Tomasz Durka <tomasz@durka.pl>
2013-06-25 07:37:31 +0200

    Add install master script

---
+++ scripts/puppet-install-master.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+bash <(curl -Ls https://raw.github.com/cargomedia/puppet-packages/master/scripts/puppet-install.sh)
+
+if (test -f /etc/debian_version && cat /etc/debian_version | grep -q '^6\.'); then
+	apt-get install -y puppetmaster
+
+else
+	echo 'Your operating system is not supported' > 1>&2
+	exit 1
+fi
