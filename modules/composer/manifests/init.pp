72ddb7c3abfff04bcff5fc47baac7ee157289ed0
Tomasz Durka <tomasz@durka.pl>
2013-06-25 13:14:48 +0200

    Require php5

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -8,7 +8,7 @@ class composer($version = '1.0.0-alpha7') {
 		command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > ${phar}",
 		path => ['/usr/local/bin', '/usr/bin', '/bin'],
 		unless => "test -f ${phar} && ${binary} --version | grep -w '${version}'",
-		require => [File[$binary], File[$config]],
+		require => [File[$binary], File[$config], Class['php5']],
 	}
 
 	file {$binary:
