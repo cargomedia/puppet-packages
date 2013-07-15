a9d4cc2f27f677862b9a12633ff77adb8e9dc969
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-07-15 16:17:52 +0200

    Add puppet.conf, remove puppet-get-config script

--- scripts/puppet-get-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-REPOSITORY=$1
-
-curl -Ls https://raw.github.com/cargomedia/${REPOSITORY}/master/scripts/resources/puppet.conf > $(puppet agent --configprint confdir)/puppet.conf
