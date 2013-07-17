3870beea705889d09af863069879c73bc716094c
Tomasz Durka <tomasz@durka.pl>
2013-07-18 01:29:11 +0200

    Implement basic apache manifest

---
+++ modules/apache2/manifests/init.pp
@@ -0,0 +1,11 @@
+class apache2 {
+
+	package { 'apache2':
+		ensure => present,
+	}
+
+	file { '/etc/apache2/apache2.conf':
+		source => 'puppet:///modules/apache2/apache2.conf',
+		ensure => present,
+	}
+}
