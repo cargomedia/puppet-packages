921255008d7c95ccec990188f4b01212c4b8a785
Tomasz Durka <tomasz@durka.pl>
2013-07-23 15:08:56 +0200

    User arrows instead of require keyword

--- modules/php5/manifests/apache2.pp
+++ modules/php5/manifests/apache2.pp
@@ -5,11 +5,11 @@ class php5::apache2 {
 	package { ['php5', 'libapache2-mod-php5']:
 		ensure => present,
 	}
+	->
 
 	file { '/etc/php5/apache2/php.ini':
 		source => 'puppet:///modules/php5/apache2/php.ini',
 		ensure => present,
 		owner => 0, group => 0, mode => 0644,
-		require => Package['php5', 'libapache2-mod-php5'],
 	}
 }
