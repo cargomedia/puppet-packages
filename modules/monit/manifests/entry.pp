cd56ef492a4ce815312d754c59b22a2cb549bdce
Tomasz Durka <tomasz@durka.pl>
2013-07-23 15:39:07 +0200

    Add monit entry definition

---
+++ modules/monit/manifests/entry.pp
@@ -0,0 +1,10 @@
+define monit::entry ($content, $ensure) {
+
+	require 'monit'
+
+	file { "/etc/monit/conf.d/${name}":
+		content => $content,
+		ensure => $ensure,
+		group => 0, owner => 0, mode => 644,
+	}
+}
