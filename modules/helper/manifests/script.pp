951e4d2ba56ce3e96a2f27e1dabcad2779daac8c
Tomasz Durka <tomasz@durka.pl>
2013-07-23 14:58:21 +0200

    Create directory within temp, cleanup afterwards

--- modules/helper/manifests/script.pp
+++ modules/helper/manifests/script.pp
@@ -1,20 +1,33 @@
 define helper::script ($content, $unless = false) {
 
 	$scriptName = md5($title)
-	$scriptFilename = "/tmp/script-${scriptName}"
+	$scriptDirname = "/tmp/${scriptName}"
+	$scriptFilename = "${scriptDirname}/${scriptName}"
 
 	unless $unless {
 
+		file { $scriptDirname:
+			content => $content,
+			group => 0, owner => 0, mode => 644
+		}
+
 		file { $scriptFilename:
 			content => $content,
-			mode => 755,
+			group => 0, owner => 0, mode => 755,
+			require => File[$scriptDirname],
 		}
 
 		exec {"exec ${title}":
-			command => "/tmp/script-${scriptName}",
+			command => "${scriptFilename}",
 			cwd => "/tmp",
 			path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
 			require => File[$scriptFilename],
+		}
+
+		exec {"cleanup ${title}":
+			command => "rm -rf ${scriptDirname}",
+			path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+			require => Exec["exec ${title}"],
 		}
 	}
 }
