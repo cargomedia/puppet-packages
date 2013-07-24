55fecc7f8523062bf58c0552919fd8345cac448b
Tomasz Durka <tomasz@durka.pl>
2013-07-24 19:29:15 +0200

    Add puppet config and autostart

--- modules/puppet/manifests/agent.pp
+++ modules/puppet/manifests/agent.pp
@@ -1,12 +1,27 @@
-class puppet::agent {
+class puppet::agent ($server = 'puppet') {
 
 	helper::script {'install puppet apt sources':
 		content => template('puppet/install-apt-sources.sh'),
 		unless => "dpkg -l puppetlabs-release | grep '^ii '",
 	}
+	->
 
 	package {'puppet':
 		ensure => present,
+	}
+	->
+
+	file {'/etc/puppet/puppet.conf':
+		content => template('puppet/puppet.conf'),
+		ensure => present,
+		group => 0, owner => 0, mode => 644,
+	}
+	->
+
+	file {'/etc/default/puppet':
+		content => "START=yes\nDAEMON_OPTS=\"\"\n",
+		ensure => present,
+		group => 0, owner => 0, mode => 644,
 	}
 
 }
