930503fee7a65fa1e46501602c2b50784786f30e
Tomasz Durka <tomasz@durka.pl>
2013-07-25 15:58:12 +0200

    Add file dependencies for puppetmaster, separate ssl dirs

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -2,21 +2,20 @@ class puppet::master ($certname) {
 
 	require 'puppet::common'
 
-	package {'puppetmaster':
-		ensure => present,
-	}
-	->
-
 	file {'/etc/puppet/manifests/site.pp':
 		content => template('puppet/site.pp'),
 		ensure => present,
 		group => 0, owner => 0, mode => 644,
 	}
-	->
 
 	file {'/etc/puppet/hiera.yaml':
 		content => template('puppet/hiera.yaml'),
 		ensure => present,
 		group => 0, owner => 0, mode => 644,
+	}
+
+	package {'puppetmaster':
+		ensure => present,
+		require => [File['/etc/puppet/manifests/site.pp'], File['/etc/puppet/hiera.yaml']],
 	}
 }
