e61b507f3cab649c26bf3f0a9ed8b332cfe5f63c
Tomasz Durka <tomasz@durka.pl>
2013-07-25 13:08:01 +0200

    Remove $scriptFilename

--- modules/helper/manifests/script.pp
+++ modules/helper/manifests/script.pp
@@ -2,12 +2,10 @@ define helper::script ($content, $unless) {
 
 	$scriptName = md5($title)
 	$scriptDirname = "/tmp/${scriptName}"
-	$scriptFilename = "${scriptDirname}/${scriptName}"
-
 	$contentQuoted = shellquote($content)
 
 	exec {"exec ${title}":
-		command => "mkdir -p ${scriptDirname} && echo ${contentQuoted} > ${scriptFilename} && chmod +x ${scriptFilename} && cd ${scriptDirname} && ${scriptFilename}",
+		command => "mkdir -p ${scriptDirname} && cd ${scriptDirname} && echo ${contentQuoted} > ${scriptName} && chmod +x ${$scriptName} && ./${$scriptName}",
 		unless => $unless,
 		path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
 		logoutput => on_failure,
