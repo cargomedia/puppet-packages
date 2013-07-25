564894a5d7d85db6ff9e13b4d610582882f9b068
Tomasz Durka <tomasz@durka.pl>
2013-07-25 13:21:08 +0200

    Refactor recipe
    - Make default settings a template
    - Nest agent templates into folder
    - Curl over wget

--- modules/puppet/templates/install-apt-sources.sh
+++ modules/puppet/templates/install-apt-sources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-wget -q http://apt.puppetlabs.com/puppetlabs-release-squeeze.deb
+curl -Ls http://apt.puppetlabs.com/puppetlabs-release-squeeze.deb > puppetlabs-release-squeeze.deb
 dpkg -i puppetlabs-release-squeeze.deb
 apt-get update
