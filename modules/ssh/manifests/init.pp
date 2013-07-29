e5540a7cde87bcef612f3f2b167c5f830a84dd50
Tomasz Durka <tomasz@durka.pl>
2013-07-29 21:02:08 +0200

    Fix config file resource

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -14,7 +14,7 @@ class ssh {
 
 	file {'/etc/ssh/sshd_config':
 		ensure => present,
-		content => template('ssh/ssh_config'),
+		content => template('ssh/sshd_config'),
 		owner => '0',
 		group => '0',
 		mode => '0644',
