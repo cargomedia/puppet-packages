513c1cf102d297c8464f75602d235f52259a3603
Tomasz Durka <tomasz@durka.pl>
2013-07-30 14:43:42 +0200

    Refactor helper::script

--- modules/helper/manifests/script.pp
+++ modules/helper/manifests/script.pp
@@ -5,16 +5,17 @@ define helper::script ($content, $unless) {
 	$contentQuoted = shellquote($content)
 
 	exec {"exec ${title}":
+		provider => shell,
 		command => "mkdir -p ${scriptDirname} && cd ${scriptDirname} && echo ${contentQuoted} > ${scriptName} && chmod +x ${$scriptName} && ./${$scriptName}",
 		unless => $unless,
 		path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
 		logoutput => on_failure,
 	}
+	~>
 
 	exec {"cleanup ${title}":
 		command => "rm -rf ${scriptDirname}",
-		onlyif => "test -d ${scriptDirname}",
 		path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-		require => Exec["exec ${title}"],
+		refreshonly => true,
 	}
 }
