4fe6b8bdbe1199f79bd09a5dda780ae68a818790
Tomasz Durka <tomasz@durka.pl>
2013-07-30 12:07:45 +0200

    Refactor apache2 init ownmod

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -7,6 +7,8 @@ class apache2 {
 	file { '/etc/apache2/apache2.conf':
 		source => 'puppet:///modules/apache2/apache2.conf',
 		ensure => present,
-		owner => 0, group => 0, mode => 0644,
+		group => '0',
+		owner => '0',
+		mode => '0644',
 	}
 }
