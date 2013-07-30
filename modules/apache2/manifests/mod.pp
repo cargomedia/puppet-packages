62ecdffd00757a38c0047f1fa31c6f3a9e1ca991
Tomasz Durka <tomasz@durka.pl>
2013-07-30 12:56:23 +0200

    Require apache2

--- modules/apache2/manifests/mod.pp
+++ modules/apache2/manifests/mod.pp
@@ -1,8 +1,10 @@
 define apache2::mod ($enabled = true) {
 
-	file {"/etc/apache2/mods-enabled/${mod}.load":
+	require 'apache2'
+
+	file {"/etc/apache2/mods-enabled/${name}.load":
 		ensure => $enabled ? {true => link, false => absent},
-		target => "/etc/apache2/mods-available/${mod}.load",
+		target => "/etc/apache2/mods-available/${name}.load",
 		group => '0',
 		owner => '0',
 		mode => '0644',
