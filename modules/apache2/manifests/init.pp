2ce613ddb313a0d346affe7338f2ea56af446c81
Tomasz Durka <tomasz@durka.pl>
2013-07-30 09:46:07 +0200

    Add service for apache2
    - add apache2 service
    - refactor code so service can be notifiable
    - introduce dependencies

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -1,12 +1,26 @@
 class apache2 {
 
-	package { 'apache2':
-		ensure => present,
+	file {'/etc/apache2':
+		ensure => directory,
+		owner => '0',
+		group => '0',
+		mode => '0755',
 	}
+	->
 
-	file { '/etc/apache2/apache2.conf':
+	file {'/etc/apache2/apache2.conf':
 		source => 'puppet:///modules/apache2/apache2.conf',
 		ensure => present,
-		owner => 0, group => 0, mode => 0644,
+		owner => '0',
+		group => '0',
+		mode => '0644',
 	}
+	->
+
+	package {'apache2':
+		ensure => present,
+	}
+	->
+
+	service {'apache2':}
 }
--- modules/apache2/manifests/vhost.pp
+++ modules/apache2/manifests/vhost.pp
@@ -1,6 +1,6 @@
 define apache2::vhost ($content, $enabled = true) {
 
-	require 'apache2'
+	include 'apache2'
 
 	$vhostPath = "/etc/apache2/sites-available/${name}"
 
@@ -8,11 +8,14 @@ define apache2::vhost ($content, $enabled = true) {
 		content => $content,
 		ensure => present,
 		owner => 0, group => 0, mode => 0644,
+		notify => Service['apache2'],
 	}
+	->
 
 	file { "/etc/apache2/sites-enabled/${name}":
 		ensure => $enabled ? { true => link, false => absent},
 		target => $vhostPath,
 		owner => 0, group => 0, mode => 0644,
+		notify => Service['apache2'],
 	}
 }
