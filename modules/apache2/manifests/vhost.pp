3cecca0bcc6354dcb85cea7335a58a1284d54585
Tomasz Durka <tomasz@durka.pl>
2013-07-30 12:07:31 +0200

    Refactor vhost ownmod

--- modules/apache2/manifests/vhost.pp
+++ modules/apache2/manifests/vhost.pp
@@ -5,14 +5,18 @@ define apache2::vhost ($content, $enabled = true) {
 	$vhostPath = "/etc/apache2/sites-available/${name}"
 
 	file { $vhostPath:
-		content => $content,
 		ensure => present,
-		owner => 0, group => 0, mode => 0644,
+		content => $content,
+		group => '0',
+		owner => '0',
+		mode => '0644',
 	}
 
 	file { "/etc/apache2/sites-enabled/${name}":
 		ensure => $enabled ? { true => link, false => absent},
 		target => $vhostPath,
-		owner => 0, group => 0, mode => 0644,
+		group => '0',
+		owner => '0',
+		mode => '0644',
 	}
 }
