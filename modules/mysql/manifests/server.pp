4a5f83f581a684b1fccef1c6d755db12b519c631
Tomasz Durka <tomasz@durka.pl>
2013-08-08 20:12:05 +0200

    Refactor mysql::server to auto-change root password

--- modules/mysql/manifests/server.pp
+++ modules/mysql/manifests/server.pp
@@ -1,11 +1,55 @@
-class mysql::server {
+class mysql::server ($rootPassword = undef) {
+
+	include 'mysql::service'
+
+
+	file {'/root/.my.cnf':
+		ensure => $rootPassword ? {
+			undef => absent,
+			default => file,
+		},
+		content => template('mysql/client-config.cnf'),
+		owner => '0',
+		group => '0',
+		mode => '0600',
+	}
+
+	file {'/etc/mysql':
+		ensure => directory,
+		owner => '0',
+		group => '0',
+		mode => '0755',
+	}
+	->
+
+	file {'/etc/mysql/init.sql':
+		ensure => file,
+		content => template('mysql/init.sql'),
+		owner => 'root',
+		group => 'mysql',
+		mode => '0640',
+		notify => Service['mysql'],
+	}
+	->
+
+	file {'/etc/mysql/my.cnf':
+		ensure => file,
+		content => template('mysql/my.cnf'),
+		owner => 'root',
+		group => 'mysql',
+		mode => '0640',
+	}
+
+	->
 
 	package {'mysql-server':
 		ensure => present,
+		before => Service['mysql'],
 	}
+	->
 
 	monit::entry {'mysql':
-		content => template('mysql/monit'),
 		ensure => present,
+		content => template('mysql/monit'),
 	}
 }
