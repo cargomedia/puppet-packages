843a1992b7126b13e729447a24e97089b81b3441
Tomasz Durka <tomasz@durka.pl>
2013-08-09 23:16:06 +0200

    Adjust openx package

--- modules/openx/manifests/init.pp
+++ modules/openx/manifests/init.pp
@@ -1,13 +1,15 @@
 class openx (
-	$host = $name,
+	$host,
 	$certificatePem,
 	$certificateKey,
 	$certificateCa,
-	$version = '2.8.10'
+	$version = '2.8.11'
 ) {
 
 	require 'php5::apache2'
-	require 'php5::gd'
+	require 'php5::extension::mysql'
+	require 'php5::extension::gd'
+	require 'openssl'
 	require 'apache2::mod::ssl'
 	require 'mysql::server'
 
@@ -31,7 +33,6 @@ class openx (
 		mode => '0755',
 		require => Helper::Script['install openx'],
 	}
-
 
 	file {"/var/openx/ssl/${host}.pem":
 		ensure => present,
@@ -59,17 +60,16 @@ class openx (
 			owner => 'www-data',
 			mode => '0644',
 		}
-		~>
+		->
 
 		exec {"/var/openx/ssl-ca/ for ${host}":
 			provider => shell,
-			command => "ln -ls ${host} $(openssl x509 -noout -hash -in ${host}).0",
+			command => "ln -s ${host} $(openssl x509 -noout -hash -in ${host}).0",
+			unless => "test -L $(openssl x509 -noout -hash -in ${host}).0",
 			cwd => '/var/openx/ssl-ca/',
-			refreshonly => true,
 			before => Apache2::Vhost[$host],
 		}
 	}
-
 
 	file {'/var/openx/www/delivery/ajs-proxy.php':
 		ensure => present,
@@ -78,6 +78,14 @@ class openx (
 		owner => 'www-data',
 		mode => '0644',
 		require => Helper::Script['install openx'],
+	}
+
+	mysql::user {'openx@localhost':
+		password => $password,
+	}
+
+	mysql::database {'openx':
+		user => 'openx@localhost',
 	}
 
 	apache2::vhost {$host:
--- modules/openx/templates/install.sh
+++ modules/openx/templates/install.sh
@@ -3,5 +3,5 @@ set -e
 
 curl -sL http://download.openx.org/openx-<%= @version %>.tar.gz > openx.tar.gz
 tar -zxvf openx.tar.gz
-cp -R openx-<%= @version %> /var/openx
+rsync -a openx-<%= @version %>/ /var/openx
 chown -R www-data: /var/openx
