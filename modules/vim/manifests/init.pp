ab824bf0c409babb448880faaac1448803b43ab9
Tomasz Durka <tomasz@durka.pl>
2013-08-09 19:14:41 +0200

    Add basic vimrc with syntax higlighting and autoindent

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -1,5 +1,20 @@
 class vim {
 
+	file {'/etc/vim':
+		ensure => directory,
+		owner => '0',
+		group => '0',
+		mode => '755',
+	}
+
+	file {'/etc/vim/vimrc.local':
+		ensure => file,
+		content => template('vim/vimrc'),
+		owner => '0',
+		group => '0',
+		mode => '644',
+	}
+
 	package {'vim':
 		ensure => present,
 	}
