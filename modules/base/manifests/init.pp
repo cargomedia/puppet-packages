9788e03cb15d8895f1dd54044a106e51cb1f7f37
Tomasz Durka <tomasz@durka.pl>
2013-08-12 14:41:57 +0200

    Add monit system checks

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,7 +1,12 @@
-class base {
+class base ($alertOnLoad1 = 20, $alertOnLoad5 = 10) {
 
 	require 'bash'
 	require 'ssh'
 	require 'postfix'
 	require 'vim'
+	require 'monit'
+
+	monit::entry {'system':
+		content => template('base/monit'),
+	}
 }
