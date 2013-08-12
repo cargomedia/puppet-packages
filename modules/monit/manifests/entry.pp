0e1a0979e0a1d112d1c2cfdc44060c37775c96f4
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-08-12 13:03:47 +0200

    Make monit entry ensurable 'present' but entry's file 'file'

--- modules/monit/manifests/entry.pp
+++ modules/monit/manifests/entry.pp
@@ -1,10 +1,10 @@
-define monit::entry ($content, $ensure = 'file') {
+define monit::entry ($content, $ensure = present) {
 
 	include 'monit'
 
 	file { "/etc/monit/conf.d/${name}":
 		content => $content,
-		ensure => $ensure,
+		ensure => $ensure => present ? { true => file, false => absent},
 		group => 0, owner => 0, mode => 644,
 		require => Package['monit'],
 		notify => Service['monit'],
