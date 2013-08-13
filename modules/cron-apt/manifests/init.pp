37e1d39e5d1189d88c3cdb62ac0151217e95ddc4
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-08-13 16:59:10 +0200

    Added cron-apt class

---
+++ modules/cron-apt/manifests/init.pp
@@ -0,0 +1,22 @@
+class cron-apt {
+
+  package {'cron-apt':
+    ensure => present,
+  }
+
+  file {'/etc/cron-apt':
+    ensure => directory,
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+
+  file {'/etc/cron-apt/config':
+    ensure => file,
+    content => template('cron-apt/config'),
+    owner => '0',
+    group => '0',
+    mode => '0644',
+    require => File['/etc/cron-apt'],
+  }
+}
