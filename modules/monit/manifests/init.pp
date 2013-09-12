8a93c47c078cf708115016cae7571f11c2978aec
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-09-12 15:02:58 +0200

    Fromatting, added /etc/monit/conf.d

--- modules/monit/manifests/init.pp
+++ modules/monit/manifests/init.pp
@@ -1,37 +1,44 @@
 class monit ($emailTo = 'root@localhost', $emailFrom = 'root@localhost', $allowedHosts = []) {
 
-	include 'postfix'
-	include 'monit::service'
+  include 'monit::service'
 
-	file { '/etc/default/monit':
-		content => template('monit/default'),
-		ensure => file,
-		group => '0', owner => '0', mode => '0644',
-		before => Package['monit'],
-		notify => Service['monit'],
-	}
+  file { '/etc/default/monit':
+    content => template('monit/default'),
+    ensure => file,
+    group => '0',
+    owner => '0',
+    mode => '0644',
+    notify => Service['monit'],
+  }
+  ->
 
-	file { '/etc/monit':
-		ensure => directory,
-		group => '0', owner => '0', mode => '0755',
-	}
+  file { '/etc/monit':
+    ensure => directory,
+    group => '0',
+    owner => '0',
+    mode => '0755',
+  }
+  ->
 
-	file { '/etc/monit/monitrc':
-		content => template('monit/monitrc'),
-		ensure => file,
-		group => '0', owner => '0', mode => '0600',
-		notify => Service['monit'],
-		before => Package['monit'],
-	}
+  file { '/etc/monit/conf.d':
+    ensure => directory,
+    group => '0',
+    owner => '0',
+    mode => '0755',
+  }
+  ->
 
-	file { '/etc/monit/conf.d':
-		ensure => directory,
-		group => '0', owner => '0', mode => '0755',
-		before => Package['monit'],
-	}
+  file { '/etc/monit/monitrc':
+    content => template('monit/monitrc'),
+    ensure => file,
+    group => '0',
+    owner => '0',
+    mode => '0600',
+    notify => Service['monit'],
+  }
+  ->
 
-	package {'monit':
-		ensure => present,
-		require => Package['postfix'],
-	}
+  package {'monit':
+    ensure => present,
+  }
 }
