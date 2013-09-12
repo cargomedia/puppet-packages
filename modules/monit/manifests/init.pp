82a6b3c524e20efe0efaf242f2cf65c901b66aba
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-09-12 12:02:41 +0200

    Add conf.d dir and require postfix for monit package

--- modules/monit/manifests/init.pp
+++ modules/monit/manifests/init.pp
@@ -1,30 +1,37 @@
 class monit ($emailTo = 'root@localhost', $emailFrom = 'root@localhost', $allowedHosts = []) {
 
+	include 'postfix'
 	include 'monit::service'
 
 	file { '/etc/default/monit':
 		content => template('monit/default'),
 		ensure => file,
 		group => '0', owner => '0', mode => '0644',
+		before => Package['monit'],
 		notify => Service['monit'],
 	}
-	->
 
 	file { '/etc/monit':
 		ensure => directory,
 		group => '0', owner => '0', mode => '0755',
 	}
-	->
 
 	file { '/etc/monit/monitrc':
 		content => template('monit/monitrc'),
 		ensure => file,
 		group => '0', owner => '0', mode => '0600',
 		notify => Service['monit'],
+		before => Package['monit'],
 	}
-	->
+
+	file { '/etc/monit/conf.d':
+		ensure => directory,
+		group => '0', owner => '0', mode => '0755',
+		before => Package['monit'],
+	}
 
 	package {'monit':
 		ensure => present,
+		require => Package['postfix'],
 	}
 }
