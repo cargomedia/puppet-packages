c29d01dab885e90ca3d72da3a0860668aca7c9ab
Tomasz Durka <tomasz@durka.pl>
2013-09-16 13:12:32 +0200

    Add basic socket-redis package

---
+++ modules/socket-redis/manifests/init.pp
@@ -0,0 +1,90 @@
+class socket-redis (
+  $version = '0.1.1',
+  $redisHost = 'localhost',
+  $socketPorts = [8090],
+  $logDir = '/var/log/socket-redis',
+
+  $statusPort = '8085',
+  $sslKey = undef,
+  $sslCert = undef,
+  $sslPfx = undef,
+  $sslPassphrase = undef
+) {
+
+  require 'nodejs'
+
+  file {'/etc/socket-redis':
+    ensure => directory,
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+
+  file {'/etc/socket-redis/ssl':
+    ensure => directory,
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+
+  if $sslKey {
+    $sslKeyFile = '/etc/socket-redis/ssl/cert.key'
+    file {$sslKeyFile:
+      ensure => file,
+      content => $sslKey,
+      owner => '0',
+      group => '0',
+      mode => '0640',
+      before => File['/etc/init.d/socket-redis'],
+    }
+  }
+
+  if $sslCert {
+    $sslCertFile = '/etc/socket-redis/ssl/cert.pem'
+    file {$sslCertFile:
+      ensure => file,
+      content => $sslCert,
+      owner => '0',
+      group => '0',
+      mode => '0640',
+      before => File['/etc/init.d/socket-redis'],
+    }
+  }
+
+  if $sslPfx {
+    $sslPfxFile = '/etc/socket-redis/ssl/cert.pfx'
+    file {$sslPfxFile:
+      ensure => file,
+      content => $sslPfx,
+      owner => '0',
+      group => '0',
+      mode => '0640',
+      before => File['/etc/init.d/socket-redis'],
+    }
+  }
+
+  if $sslPassphrase {
+    $sslPassphraseFile = '/etc/socket-redis/ssl/passphrase'
+    file {$sslPassphraseFile:
+      ensure => file,
+      content => $sslPassphrase,
+      owner => '0',
+      group => '0',
+      mode => '0640',
+      before => File['/etc/init.d/socket-redis'],
+    }
+  }
+
+  file {'/etc/init.d/socket-redis':
+    ensure => file,
+    content => template('socket-redis/init.sh'),
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+
+  package {'socket-redis':
+    ensure => $version,
+    provider => 'npm',
+  }
+}
