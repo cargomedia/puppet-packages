f35c108052d3924b88ca02373d2afd0657809b88
Tomasz Durka <tomasz@durka.pl>
2013-09-17 01:07:23 +0200

    Add jetty config to specify custom listening host

--- modules/puppet/manifests/db.pp
+++ modules/puppet/manifests/db.pp
@@ -1,8 +1,17 @@
-class puppet::db {
+class puppet::db ($host) {
 
   file {'/etc/default/puppetdb':
     ensure => file,
     content => template('puppet/db/default'),
+    group => '0',
+    owner => '0',
+    mode => '0644',
+  }
+  ->
+
+  file {'/etc/puppetdb/conf.d/jetty.ini':
+    ensure => file,
+    content => template('puppet/db/conf.d/jetty.ini'),
     group => '0',
     owner => '0',
     mode => '0644',
