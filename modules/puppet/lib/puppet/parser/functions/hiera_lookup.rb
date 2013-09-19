b4cefef8d96e0b4f52df97266f168350577385de
Tomasz Durka <tomasz@durka.pl>
2013-09-19 12:48:01 +0200

    Format hiera_lookup

--- modules/puppet/lib/puppet/parser/functions/hiera_lookup.rb
+++ modules/puppet/lib/puppet/parser/functions/hiera_lookup.rb
@@ -5,7 +5,7 @@ module Puppet::Parser::Functions
     certname = args[0]
     key = args[1]
 
-   	uri = URI.parse('http://localhost:8080/v2/nodes/' + certname + '/facts')
+    uri = URI.parse('http://localhost:8080/v2/nodes/' + certname + '/facts')
     result = Net::HTTP.get(uri)
     puppetdbFacts = JSON.parse(result)
 
@@ -16,7 +16,7 @@ module Puppet::Parser::Functions
       facts[name] = value
     end
 
-	hiera = Hiera.new(:config => HieraPuppet.hiera_config)
+    hiera = Hiera.new(:config => HieraPuppet.hiera_config)
     hiera.lookup(key, 'default', facts)
   end
 end
