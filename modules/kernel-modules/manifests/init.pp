dca2316e8eba14b477414344288adafce37c9389
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-09-25 14:47:44 +0200

    RATR (Refactored According to Review)

--- modules/kernel-modules/manifests/init.pp
+++ modules/kernel-modules/manifests/init.pp
@@ -2,11 +2,9 @@ class kernel-modules($entries = nil) {
 
   file { '/etc/modules':
     ensure => file,
-    alias => 'modules file',
     owner => '0',
     group => '0',
     mode => '0644',
     content => template('kernel-modules/modules')
   }
-
 }
