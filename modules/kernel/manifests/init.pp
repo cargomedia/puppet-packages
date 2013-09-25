6bfb085bbff3bae19096ec7ed812d5c59f661136
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-09-25 16:22:09 +0200

    Renamed class to kernel, argument hash to modules, suppressed last blank line on template

--- modules/kernel/manifests/init.pp
+++ modules/kernel/manifests/init.pp
@@ -1,10 +1,10 @@
-class kernel-modules($entries = nil) {
+class kernel($modules = nil) {
 
   file { '/etc/modules':
     ensure => file,
     owner => '0',
     group => '0',
     mode => '0644',
-    content => template('kernel-modules/modules')
+    content => template('kernel/modules')
   }
 }
