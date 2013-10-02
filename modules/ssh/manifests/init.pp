29a66599e6e9727c3a08064d2a067e580d8aabe1
Tomasz Durka <tomasz@durka.pl>
2013-10-03 01:26:27 +0200

    Refactor ssh
    - Add dependencies hierarchy
    - Add ssh service, restart when config changes

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,15 +1,12 @@
 class ssh {
 
-  package {'ssh':
-    ensure => installed,
-  }
-
   file {'/etc/ssh/ssh_config':
     ensure => file,
     content => template('ssh/ssh_config'),
     owner => '0',
     group => '0',
     mode => '0644',
+    before => Package['ssh'],
   }
 
   file {'/etc/ssh/sshd_config':
@@ -18,5 +15,14 @@ class ssh {
     owner => '0',
     group => '0',
     mode => '0644',
+    before => Package['ssh'],
+    notify => Service['ssh'],
   }
+
+  package {'ssh':
+    ensure => installed,
+    before => Service['ssh'],
+  }
+
+  service {'sshd':}
 }
