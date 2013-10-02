2e1436d0e2e5cb3fd688abb9fe2fc1073908c55f
Tomasz Durka <tomasz@durka.pl>
2013-10-03 01:13:12 +0200

    Upgrade elasticsearch to 0.90.5

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -1,12 +1,4 @@
-class elasticsearch ($version = '0.90.3') {
-
-  file {'/etc/init.d/elasticsearch':
-    ensure => file,
-    content => template('elasticsearch/init.sh'),
-    owner => '0',
-    group => '0',
-    mode => '0755',
-  }
+class elasticsearch {
 
   file {'/etc/default/elasticsearch':
     ensure => file,
@@ -16,6 +8,7 @@ class elasticsearch ($version = '0.90.3') {
     mode => '0755',
   }
 
+  $version = '0.90.5'
   helper::script {'install elasticsearch':
     content => template('elasticsearch/install.sh'),
     unless => "test -x /usr/share/elasticsearch/bin/elasticsearch && /usr/share/elasticsearch/bin/elasticsearch -v | grep -q '${version}'"
