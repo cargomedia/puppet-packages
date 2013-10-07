830df0781631272413d0009916d55e4c1d112c9c
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-10-07 15:34:08 +0200

    Added upgrading; Added version param;

--- modules/puppet/manifests/master/module.pp
+++ modules/puppet/manifests/master/module.pp
@@ -1,7 +1,15 @@
-define puppet::master::module() {
+define puppet::master::module($version = '>0.0.0') {
+
+  exec {"puppet module upgrade $name":
+    command => "puppet module upgrade ${name}",
+    onlyif => "puppet module list | grep ${name}",
+    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  }
+  ->
 
   exec {"puppet module install $name":
-    command => "puppet module install ${name}",
+    command => "puppet module install -f -v '${version}' ${name}",
+    unless => "puppet module list | grep ${name}",
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
 }
