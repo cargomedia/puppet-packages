5cfbae0eb851f569091a886ec49995e546fc111a
Tomasz Durka <tomasz@durka.pl>
2013-10-15 15:33:48 +0200

    Remove debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,6 @@ RSpec.configure do |c|
       end
       actions.push('snapshot go default default-test-snapshot')
       actions.each do |action|
-        puts action
         `vagrant #{action}`
       end
 
