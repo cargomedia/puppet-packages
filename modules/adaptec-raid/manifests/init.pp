4ef4553964e8d928b66019040b8b355c7325ff4d
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-10-15 19:54:46 +0200

    Adapted Adaptec arcconf installation

--- modules/adaptec-raid/manifests/init.pp
+++ modules/adaptec-raid/manifests/init.pp
@@ -1,21 +1,25 @@
 class adaptec-raid {
 
-  helper::script {'install arcconf if hardware is present':
+
+  helper::script {'download and install arcconf':
     content => template('adaptec-raid/install-arcconf.sh'),
-    onlyif => 'lspci | grep -qi "adaptec.*raid"'
+    unless => 'which arcconf'
   }
-  ->
+
   helper::script {'set hard drive write cache off if adaptec raid':
-    content => template('adaptec-raid/install-arcconf.sh')
+    content => template('adaptec-raid/set-write-cache-off.sh')
+    unless => 'which arcconf'
   }
 
   file { '/usr/sbin/check-adaptec-raid-health.sh':
     content => template('adaptec-raid/check-adaptec-raid-health.sh')
   }
   ->
+
   cron {'raid-health-check':
     command => '/usr/sbin/check-adaptec-raid-health.sh',
     user => 'root',
-    minute  => '*/5'
+    minute  => '*//5',
   }
+
 }
--- modules/adaptec-raid/templates/install-arcconf.sh
+++ modules/adaptec-raid/templates/install-arcconf.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
-lspci | grep -qi 'adaptec.*raid' || exit 0
 
-if !(test -x /usr/local/bin/arcconf && /usr/local/bin/arcconf | grep -iq 'Version 7.30') then
-	apt-get -qy install libstdc++5
-	wget -q http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v7_30_18837.tgz
-	unp asm_linux_x64_v7_30_18837.tgz
-	mv cmdline/arcconf /usr/local/bin
-fi
+apt-get -qy install libstdc++5
+wget -q http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v7_30_18837.tgz
+tar -xzf asm_linux_x64_v7_30_18837.tgz
+mv cmdline/arcconf /usr/local/bin
