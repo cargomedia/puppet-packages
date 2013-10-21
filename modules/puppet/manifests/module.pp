8deeead9452bd936d2284c5fd0a323a26aeb4c86
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-10-21 12:28:13 +0200

    Optimized to install first and fore upgrade after

--- modules/puppet/manifests/module.pp
+++ modules/puppet/manifests/module.pp
@@ -1,21 +1,15 @@
-define puppet::module ($version = '') {
+define puppet::module ($version = '>v0.0.0') {
 
-  if $version == '' {
-    $version_install = '>v0.0.0'
-  } else {
-    $version_install = $version
-  }
-
-  exec {"puppet module upgrade $name":
-    command => "puppet module upgrade -f --version='${version_install}' ${name}",
-    onlyif => "puppet module list | grep ${name}",
+  exec {"puppet module install $name":
+    command => "puppet module install --force --version '${version}' ${name}",
+    unless => "puppet module list | grep ${name}",
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
   ->
 
-  exec {"puppet module install $name":
-    command => "puppet module install -f -v '${version_install}' ${name}",
-    unless => "puppet module list | grep ${name}.*${version}",
+  exec {"puppet module upgrade $name":
+    command => "puppet module upgrade --force --version='${version}' ${name}",
+    onlyif => "puppet module list | grep ${name}",
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
 }
