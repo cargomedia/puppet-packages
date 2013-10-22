89e1901b7daa0c276ccde377c1ea8546d55438ab
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-10-22 12:55:12 +0200

    Adjusted params to cacti agent

--- modules/cacti/manifests/params.pp
+++ modules/cacti/manifests/params.pp
@@ -1,72 +1,22 @@
 class cacti::params {
 
-  $host = $::host ? {
-    undef => 'localhost',
-    default => $::host,
-  }
-
-  $domain = $::domain  ? {
-    undef => 'localhost',
-    default => $::domain,
-  }
-
-  $ipPrivateNetwork = $::ipPrivateNetwork ? {
-    undef => '127.0.0.0/24',
-    default => $::ipPrivateNetwork,
-  }
-
-  $dbHost = $::dbHost ? {
-    undef => 'localhost',
-    default => $::dbHost,
-  }
-
-  $dbPort = $::dbPort ? {
-    undef => '3306',
-    default => $::dbPort,
-  }
-
-  $dbName = $::dbName ? {
+  $userName = $::userName ? {
     undef => 'cacti',
-    default => $::dbName,
+    default => $::userName,
   }
 
-  $dbUser = $::dbUser ? {
+  $userId = $::userId ? {
+    undef => 2001,
+    default => $::userId,
+  }
+
+  $groupName = $::groupName ? {
     undef => 'cacti',
-    default => $::dbUser,
+    default => $::groupName,
   }
 
-  $dbPassword = $::dbPassword ? {
-    undef => '',
-    default => $::dbPassword,
-  }
-
-  $dbSenseUser = $::dbSenseUser ? {
-    undef => '',
-    default => $::dbSenseUser,
-  }
-
-  $dbSensePassword = $::dbSensePassword ? {
-    undef => '',
-    default => $::dbSensePassword,
-  }
-
-  $sshPublicKey = $::sshPublicKey ? {
-    undef => 'No key defined',
-    default => $::sshPublicKey,
-  }
-
-  $storageLvmName = $::storageLvmName ? {
-    undef => '',
-    default => $::storageLvmName,
-  }
-
-  $username = $::username ? {
-    undef => 'cacti',
-    default => $::username,
-  }
-
-  $groupname = $::groupname ? {
-    undef => 'cacti',
-    default => $::groupname,
+  $groupId = $::groupId ? {
+    undef => 2001,
+    default => $::groupId,
   }
 }
