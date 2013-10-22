31186cc9ac02fd9b61576f3a0bf96768fc2615d1
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-10-22 15:39:14 +0200

    Add/refine linux md software raid module

--- modules/raid/manifests/linux-md.pp
+++ modules/raid/manifests/linux-md.pp
@@ -1,5 +1,27 @@
 class raid::linux-md {
-  notify { "$name":
-      message => 'linux-md'
+
+  file { '/etc/mdadm/mdadm.conf':
+    ensure => file,
+    content => template('raid/linux-md/mdadm.conf'),
+    group => '0',
+    owner => '0',
+    mode => '0644',
+    notify => Service['mdadm'],
+    before => Package['mdadm'],
   }
+
+  package { 'mdadm':
+    ensure => present
+  }
+  ->
+
+  service { 'mdadm':
+    hasstatus => false,
+  }
+  ->
+
+  monit::entry {'mdadm-status':
+    content => template('raid/linux-md/monit')
+  }
+
 }
