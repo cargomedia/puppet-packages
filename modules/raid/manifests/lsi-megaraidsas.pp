52820ee4a67975beec422222f256d0f70965c828
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-10-23 10:17:16 +0200

    bracketing indenting

--- modules/raid/manifests/lsi-megaraidsas.pp
+++ modules/raid/manifests/lsi-megaraidsas.pp
@@ -2,9 +2,9 @@ class raid::lsi-megaraidsas {
 
   apt::source {'hwraid_le-vert':
     entries => ['deb http://hwraid.le-vert.net/debian squeeze main'],
-      keys => { 'le-vert' => {
-        key     => '23B3D3B4',
-        key_url => 'http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key',
+    keys => {'le-vert' => {
+      key     => '23B3D3B4',
+      key_url => 'http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key',
       }
     }
   }
