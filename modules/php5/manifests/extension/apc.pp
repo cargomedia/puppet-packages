b3e76f543d0485419119418cdb4805e4594aa3b3
Tomasz Durka <tomasz@durka.pl>
2013-10-25 20:47:25 +0200

    Refactor php extensions (nesting), add shim size for apc as option, add spec for apc

--- modules/php5/manifests/extension/apc.pp
+++ modules/php5/manifests/extension/apc.pp
@@ -1,6 +1,7 @@
 class php5::extension::apc (
   $version = '3.1.13',
   $stat = true,
+  $shimSize = '4096M',
   $configureParams = '--enable-apc-mmap --enable-apc-pthreadmutex --disable-apc-debug --disable-apc-filehits --disable-apc-spinlocks'
 ) {
 
