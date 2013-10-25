c29b6a593bdd59cf8d86301daced57e618632608
Tomasz Durka <tomasz@durka.pl>
2013-10-25 15:02:12 +0200

    Update git-modules, allow passing version

--- modules/puppet/manifests/git-modules.pp
+++ modules/puppet/manifests/git-modules.pp
@@ -1,20 +1,30 @@
-define puppet::git-modules ($cloneUrl) {
+define puppet::git-modules ($source, $version = 'master') {
 
   include 'puppet::common'
   require 'git'
 
   $path = "/etc/puppet/repos/${name}"
+  $updateCommand = "/usr/local/bin/git-modules-update-${name}"
+
+  file {$updateCommand:
+    ensure => file,
+    content => template('puppet/git-modules/update.sh'),
+    owner => '0',
+    group => '0',
+    mode => '755',
+  }
+  ->
 
   exec {"puppet repo ${name}":
-    command => "git clone ${cloneUrl} ${path}",
+    command => "git clone ${source} ${path}; ${updateCommand}",
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     creates => $path,
     notify => Exec['/etc/puppet/conf.d/main-modulepath'],
   }
+  ->
 
-  $command = "cd '${path}' && git pull --quiet"
-  cron {"cron ${command}":
-    command => $command,
+  cron {"cron ${updateCommand}":
+    command => $updateCommand,
     user    => root,
   }
 }
