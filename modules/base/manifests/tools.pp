7f92181ba2c66719c87b9867b9552f796d5714e8
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-10-29 13:40:49 +0100

    Added base::tools

---
+++ modules/base/manifests/tools.pp
@@ -0,0 +1,16 @@
+class base::tools {
+
+  require 'vim'
+
+  package {'upn': }
+  package {'screen': }
+  package {'systat': }
+  package {'parallel': }
+  package {'systat': }
+  package {'zip': }
+  package {'htop': }
+  package {'iftop': }
+  package {'iotop': }
+  package {'mytop': }
+  package {'tree': }
+}
