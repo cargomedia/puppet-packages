968cc3b7816a94e1883b0dce365d8b5ca7275c08
Tomasz Durka <tomasz@durka.pl>
2013-10-29 14:56:46 +0100

    Use array for package names, add rsync

--- modules/base/manifests/tools.pp
+++ modules/base/manifests/tools.pp
@@ -2,13 +2,7 @@ class base::tools {
 
   require 'vim'
 
-  package {'unp': ensure => installed }
-  package {'screen': ensure => installed }
-  package {'parallel': ensure => installed }
-  package {'sysstat': ensure => installed }
-  package {'zip': ensure => installed }
-  package {'htop': ensure => installed }
-  package {'iftop': ensure => installed }
-  package {'iotop': ensure => installed }
-  package {'tree': ensure => installed }
+  package {['unp', 'screen', 'parallel', 'sysstat', 'zip', 'htop', 'iftop', 'iotop', 'tree', 'rsync']:
+    ensure => installed
+  }
 }
