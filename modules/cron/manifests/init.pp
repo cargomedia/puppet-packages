2d28633f25f45584fdae30d105da7ebd3bbb2b2d
Tomasz Durka <tomasz@durka.pl>
2013-10-30 18:27:42 +0100

    Actually service depends on package

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -3,11 +3,12 @@ class cron {
   package {'cron':
     ensure => present,
   }
+  ->
 
   service {'cron':}
+  ->
 
   monit::entry {'cron':
     content => template('cron/monit'),
-    require => [Package['cron'], Service['cron']],
   }
 }
