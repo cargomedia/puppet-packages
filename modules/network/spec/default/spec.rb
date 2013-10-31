6722bd65bcd6c6416fbfeb4d66ef6584eeb91f7d
Chris Stasiak <chris.stasiak@cargomedia.ch>
2013-10-31 18:15:15 +0100

    Updated tests

--- modules/network/spec/default/spec.rb
+++ modules/network/spec/default/spec.rb
@@ -6,11 +6,6 @@ describe file('/etc/network/interfaces') do
   it { should contain 'eth2' }
 end
 
-describe interface('eth1') do
-  it { should have_ipv4_address("10.10.20.10") }
-  it { should have_ipv4_address("10.10.20.10/16") }
-end
-
 describe file('/etc/hosts') do
   it { should be_file }
   it { should contain 'foo' }
@@ -20,12 +15,7 @@ describe host('foo') do
   it { should be_resolvable }
 end
 
-describe default_gateway do
-  its(:ipaddress) { should eq '10.10.10.1' }
-  its(:interface) { should eq 'eth1' }
-end
-
 describe file('/etc/resolv.conf') do
   it { should be_file }
-  it { should contain 'foo' }
+  it { should contain 'example.local' }
 end
