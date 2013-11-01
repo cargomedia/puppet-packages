47aeba40e0b0dfca47fa0993b308541462f888d5
Tomasz Durka <tomasz@durka.pl>
2013-11-01 14:05:56 +0100

    Fix manifest

--- modules/mysql/spec/user/manifest.pp
+++ modules/mysql/spec/user/manifest.pp
@@ -1,11 +1,12 @@
 node default {
 
-class {'apt::update':
-  before => Class['mysql::server'],
-}
+  class {'apt::update':
+    before => Class['mysql::server'],
+  }
 
-class {'mysql::server':}
+  class {'mysql::server':}
 
-mysql::user {'foo@localhost':
-  password => 'bar',
+  mysql::user {'foo@localhost':
+    password => 'bar',
+  }
 }
