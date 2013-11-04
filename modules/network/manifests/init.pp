80913f12586a65befc30c6e2b4e2005171e583a2
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-04 19:16:34 +0100

    Introduced draft of network class with autoload

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,1 +1,11 @@
-class network {}
+class network  {
+
+  $interfaces = hiera_hash('network::interface')
+  create_resources('network::interface::static', $interfaces)
+
+  $hosts = hiera_hash('network::hosts')
+  create_resources('network::host', $hosts)
+
+  $resolvs = hiera_hash('network::resolv')
+  create_resources('network::resolv', $resolvs)
+}
