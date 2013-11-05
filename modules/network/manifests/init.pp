82310afccebb408a4841613ecff883ca651ec711
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-05 20:32:00 +0100

    Added 'method' support

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -2,7 +2,7 @@ class network() {
 
   $config = hiera_hash('network', {})
 
-  create_resources('network::interface::static', $config['interfaces'])
+  create_resources('network::interface', $config['interfaces'])
   create_resources('network::host', $config['hosts'])
   create_resources('network::resolv', $config['resolv'])
 }
---
+++ modules/network/manifests/interface.pp
@@ -0,0 +1,55 @@
+define network::interface (
+  $device       = $name,
+  $method       = undef,
+  $ipaddr       = undef,
+  $netmask      = undef,
+  $gateway      = undef,
+  $hwaddr       = undef,
+  $network      = undef,
+  $slaves       = undef,
+  $mtu          = undef,
+  $bonding_opts = undef,
+  $route_opts   = undef,
+  $wpa_ssid     = undef,
+  $wpa_psk      = undef,
+  $applyconfig  = true,
+) {
+
+  case $method {
+
+    'dhcp': {
+      augeas {"main-$device" :
+        context => "/files/etc/network/interfaces",
+        changes => template('network/interface/dhcp'),
+      }
+    }
+
+    'static': {
+
+      if $ipaddr == undef {
+        fail('no ip')
+      }
+
+      if $netmask == undef {
+        fail('no netmask')
+      }
+
+      augeas {"main-$device" :
+        context => "/files/etc/network/interfaces",
+        changes => template('network/interface/static'),
+      }
+    }
+
+    default: {
+      fail("Unknown method ${method}")
+    }
+  }
+
+  if $applyconfig {
+    exec {"/sbin/ifup $device":
+      command => "/sbin/ifup $device",
+      unless  => "/sbin/ifconfig | grep $device",
+      path => ['/usr/local/bin', '/usr/bin', '/bin', '/sbin'],
+    }
+  }
+}
