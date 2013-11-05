d15f26c7665d41ecfbcd7fa5d134a7fe74758403
Tomasz Durka <tomasz@durka.pl>
2013-11-05 11:20:59 +0100

    Redo spec and its manifests

--- modules/puppet/spec/git-modules/manifest.pp
+++
@@ -1,7 +0,0 @@
-node default {
-
-  puppet::git-modules {'puppet-packages':
-    source => 'https://github.com/cargomedia/puppet-packages.git',
-    version => '6297f205d6e410c0d1b51d05af9f9f41394412be',
-  }
-}
--- modules/puppet/spec/git-modules/spec.rb
+++ modules/puppet/spec/git-modules/spec.rb
@@ -1,19 +1,11 @@
 require 'spec_helper'
 
-describe file('/etc/puppet/repos/puppet-packages/.git/HEAD') do
-  it { should contain '6297f205d6e410c0d1b51d05af9f9f41394412be' }
-end
-
-describe command('test -f /etc/puppet/repos/puppet-packages/modules/puppet/manifests/git-modules.pp') do
+describe command('cd /etc/puppet/repos/puppet-packages/ && test $(git rev-list origin/master -1) = $(git rev-list HEAD -1)') do
   it { should return_exit_status 0 }
 end
 
-describe command('test -f /etc/puppet/repos/puppet-packages/modules/puppet/spec/git-modules/spec.rb') do
-  it { should return_exit_status 1 }
-end
-
 describe cron do
-  it { should have_entry '* * * * * cd /etc/puppet/repos/puppet-packages && git fetch --quiet origin && git fetch --quiet --tags origin && git checkout --quiet $(git rev-list 6297f205d6e410c0d1b51d05af9f9f41394412be -1)' }
+  it { should have_entry '* * * * * cd /etc/puppet/repos/puppet-packages && git fetch --quiet origin && git fetch --quiet --tags origin && git checkout --quiet $(git rev-list origin/master -1 2>/dev/null || git rev-list master -1)' }
 end
 
 describe command('puppet apply --configprint modulepath') do
