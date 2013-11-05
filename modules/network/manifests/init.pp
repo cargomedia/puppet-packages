8e5593e7eb45011952c4f3a8c45b6021664b6bd1
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-05 15:28:18 +0100

    Removed 'config' param from network

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,8 +1,6 @@
-class network($config = undef) {
+class network() {
 
-  if !$config {
-    $config = hiera_hash('network', {})
-  }
+  $config = hiera_hash('network', {})
 
   create_resources('network::interface::static', $config['interfaces'])
   create_resources('network::host', $config['hosts'])
