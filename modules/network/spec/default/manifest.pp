24ec9c898aeeeee6d20fddd416ce2e081c5814af
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-07 12:30:17 +0100

    Updated tests

--- modules/network/spec/default/manifest.pp
+++ modules/network/spec/default/manifest.pp
@@ -28,7 +28,7 @@ node default {
     aliases => ['boo', 'moo']
   }
 
-  network::resolv {'resolvconf':
+  class {'network::resolv':
     search => ['example.local', 'example.com'],
     nameserver => ['172.168.1.2', '8.8.8.8'],
     domain     => "example.com",
