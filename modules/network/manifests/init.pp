8740ae3b2afc69ec4bece453cc53ac67cea3cc41
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-07 11:37:59 +0100

    Changed approach to network::resolv class

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,11 +1,10 @@
 class network() {
 
+  require 'network::resolv'
+
   $interfaces = hiera_hash('network::interfaces', {})
   create_resources('network::interface', $interfaces)
 
   $hosts = hiera_hash('network::hosts', {})
   create_resources('network::host', $hosts)
-
-  $resolvs = hiera_hash('network::resolvs', {})
-  create_resources('network::resolv', $resolvs)
 }
