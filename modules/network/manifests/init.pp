6c53bcdb31760af013e49992e6fea0beeb2dd7a7
Reto Kaiser <reto@cargomedia.ch>
2013-11-11 12:27:07 +0100

    Revert "Manage 127.0.0/1.1 entries in /etc/hosts, purge unmanaged entries"
    This reverts commit 52f41b918d8802174d570d5f1f71ed8672f45e75.

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -5,18 +5,4 @@ class network() {
 
   $hosts = hiera_hash('network::hosts', {})
   create_resources('network::host', $hosts)
-
-  network::host {'localhost':
-    ipaddr => '127.0.0.1',
-    aliases => [$::fqdn, $::hostname]
-  }
-
-  network::host {$::fqdn:
-    ipaddr => '127.0.1.1',
-    aliases => [$::hostname]
-  }
-
-  resources { 'host':
-    purge => true
-  }
 }
