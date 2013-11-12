72723eba0ecf6832bec67d343794744eaf076f2f
Tomasz Durka <tomasz@durka.pl>
2013-11-12 11:28:35 +0100

    Add versioning within raid::adaptec

--- modules/raid/templates/adaptec/install-arcconf.sh
+++ modules/raid/templates/adaptec/install-arcconf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
-
 apt-get -qy install libstdc++5
 wget http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v7_31_18856.tgz
-tar -xzf asm_linux_x64_v7_30_18837.tgz
+tar -xzf asm_linux_x64_v<%= @version.scan(/[\d]+/).join('_') %>.tgz
 mv cmdline/arcconf /usr/local/bin
