a9d9f6d3101dc44f867964af8ff82fdd5c5cc462
Reto Kaiser <reto@cargomedia.ch>
2013-11-12 18:22:37 +0100

    Don't raise warning if no raid controller

--- modules/raid/manifests/init.pp
+++ modules/raid/manifests/init.pp
@@ -2,8 +2,4 @@ class raid {
 
   $raid_list = split($::raid, ',')
   include $raid_list
-
-  unless size($raid_list) > 0 {
-    warning('Should not use `raid` class if no raid hardware present')
-  }
 }
