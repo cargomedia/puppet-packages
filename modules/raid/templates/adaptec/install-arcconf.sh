ec3da4e8ba3353513fcfb0bc8c625b85554dcab0
Tomasz Durka <tomasz@durka.pl>
2013-11-12 10:56:16 +0100

    Upgrade arcconf to 7.31

--- modules/raid/templates/adaptec/install-arcconf.sh
+++ modules/raid/templates/adaptec/install-arcconf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 apt-get -qy install libstdc++5
-wget -q http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v7_30_18837.tgz
+wget http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v7_31_18856.tgz
 tar -xzf asm_linux_x64_v7_30_18837.tgz
 mv cmdline/arcconf /usr/local/bin
