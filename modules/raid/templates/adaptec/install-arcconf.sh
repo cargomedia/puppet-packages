122daede1c41b63cea53d7f71c28be5ba4bd8653
Tomasz Durka <tomasz@durka.pl>
2013-11-12 11:31:30 +0100

    Fix install script

--- modules/raid/templates/adaptec/install-arcconf.sh
+++ modules/raid/templates/adaptec/install-arcconf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 apt-get -qy install libstdc++5
-wget http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v7_31_18856.tgz
-tar -xzf asm_linux_x64_v<%= @version.scan(/[\d]+/).join('_') %>.tgz
+curl -L http://download.adaptec.com/raid/storage_manager/asm_linux_x64_v<%= @version.scan(/[\d]+/).join('_') %>.tgz | tar -xzf -
 mv cmdline/arcconf /usr/local/bin
