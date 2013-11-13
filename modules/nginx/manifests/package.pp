200f12d57b9955bcbc826dfed10543fbf639a88a
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-13 10:00:02 +0100

    Adapted puppetlabs-nginx v0.0.1; Removed first approach;

--- modules/nginx/manifests/package.pp
+++ modules/nginx/manifests/package.pp
@@ -1,3 +1,46 @@
+# Class: nginx::package
+#
+# This module manages NGINX package installation
+#
+# Parameters:
+#
+# There are no default parameters for this class.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# This class file is not called directly
 class nginx::package {
+  anchor { 'nginx::package::begin': }
+  anchor { 'nginx::package::end': }
 
+  case $::operatingsystem {
+    centos,fedora,rhel,redhat,scientific: {
+      class { 'nginx::package::redhat':
+        require => Anchor['nginx::package::begin'],
+        before  => Anchor['nginx::package::end'],
+      }
+    }
+    debian,ubuntu: {
+
+      # add apt-get sources.d
+
+      class { 'nginx::package::debian': 
+        require => Anchor['nginx::package::begin'],
+        before  => Anchor['nginx::package::end'],
+      }
+
+      # monit::entry
+
+    }
+    opensuse,suse: {
+      class { 'nginx::package::suse':
+        require => Anchor['nginx::package::begin'],
+        before  => Anchor['nginx::package::end'],
+      }
+    }
+  }
 }
