681945c67a93ec2ce33717a82072ae647717cd4a
Tomasz Durka <tomasz@durka.pl>
2013-11-13 02:33:58 +0100

    Adjust set-write-cache-off script, copy get-write-cache-on-devices script into disk, use it within script and unless

--- modules/raid/templates/adaptec/set-write-cache-off.sh
+++ modules/raid/templates/adaptec/set-write-cache-off.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -e
-
-for i in $(seq $(arcconf getversion | awk '/^Controllers found:.*$/{print $3}'));
-do
-	DEVICES=$(arcconf getconfig $i PD | perl -e '
-	foreach (join("", <STDIN>) =~ /(Device #\d+.+?MaxCache Assigned[^\n]*)/sg) {
-	  if ($_ !~ /Write Cache\s*:\s*Disabled/ && $_ =~ /Reported Channel,Device\(T:L\)\s*:\s*(\d+),(\d+)\(\d+:\d+\)/) {
-	  print "$1 $2\n";
-	  }
-	}')
-    for DEVICE in $DEVICES; do
-        CHANNEL=$(echo $DEVICE | awk "{print $1}")
-        ID=$(echo $DEVICE | awk "{print $2}")
-        set +e
-        OUT=$(arcconf SETCACHE $i DEVICE ${CHANNEL} ${ID} wt)
-        STATUS=$?
-        set -e
-        if [ $STATUS -gt 0 ] && [ -z "$(echo $OUT | grep 'same as the old')" ]; then
-            echo $OUT
-            exit $STATUS
-        fi
-    done
+DEVICES=$(/usr/local/sbin/arcconf-write-cache-on-devices.pl)
+for DEVICE in $DEVICES; do
+	CONTROLLER=$(echo ${DEVICE} | awk "{print $1}")
+	CHANNEL=$(echo ${DEVICE} | awk "{print $2}")
+	ID=$(echo ${DEVICE} | awk "{print $3}")
+	set +e
+	OUT=$(arcconf SETCACHE ${CONTROLLER} DEVICE ${CHANNEL} ${ID} wt)
+	STATUS=$?
+	set -e
+	if [ $STATUS -gt 0 ] && [ -z "$(echo $OUT | grep 'same as the old')" ]; then
+		echo $OUT
+		exit $STATUS
+	fi
 done
