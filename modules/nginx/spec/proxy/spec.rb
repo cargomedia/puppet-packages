fddad1ac2e98bdcbcbccb9c4bbe2735bd1327442
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-13 17:39:40 +0100

    Added more tests

--- modules/nginx/spec/proxy/spec.rb
+++ modules/nginx/spec/proxy/spec.rb
@@ -3,3 +3,23 @@ require 'spec_helper'
 describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
 end
+
+describe file('/etc/nginx/conf.d/proxy.conf') do
+  it { should be_file }
+end
+
+describe file('/etc/nginx/conf.d/backend-socketredis-upstream.conf') do
+  it { should be_file }
+end
+
+describe file('/etc/nginx/conf.d/vhost_autogen.conf') do
+  it { should be_file }
+end
+
+describe command('monit summary | grep nginx.*running') do
+  it { should return_exit_status 0 }
+end
+
+describe port(8090) do
+  it { should be_listening }
+end
