f1e6c442954cdbe0317c30ec99a6f8928d2c9c66
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-14 11:51:12 +0100

    Added deeper testing

--- modules/nginx/spec/proxy/spec.rb
+++ modules/nginx/spec/proxy/spec.rb
@@ -2,18 +2,30 @@ require 'spec_helper'
 
 describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
+  its(:content) { should match /worker_processes(.*)6/ }
+  its(:content) { should match /worker_rlimit_nofile(.*)20000/ }
+  its(:content) { should match /worker_connections(.*)10000/ }
+  its(:content) { should match /keepalive_timeout(.*)30/ }
+  its(:content) { should match /access_log(.*)off/ }
 end
 
 describe file('/etc/nginx/conf.d/proxy.conf') do
   it { should be_file }
 end
 
-describe file('/etc/nginx/conf.d/backend-socketredis-upstream.conf') do
-  it { should be_file }
-end
-
 describe file('/etc/nginx/conf.d/vhost_autogen.conf') do
   it { should be_file }
+  its(:content) { should match /listen(.*)8090/ }
+  its(:content) { should match /proxy_pass(.*)backend-socketredis/ }
+  its(:content) { should match /ssl(.*)on/ }
+  its(:content) { should match /proxy_buffering(.*)off/ }
+end
+
+describe file('/etc/nginx/conf.d/backend-socketredis-upstream.conf') do
+  it { should be_file }
+  its(:content) { should match /upstream(.*)backend-socketredis/ }
+  its(:content) { should match /ip_hash/ }
+  its(:content) { should match /server(.*)localhost:8096/ }
 end
 
 describe command('monit summary | grep nginx') do
