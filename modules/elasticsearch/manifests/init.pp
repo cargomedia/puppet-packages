cf10caf9cdbe9a9dc688ed0c5617f5c5a83e5b84
Tomasz Durka <tomasz@durka.pl>
2013-11-18 20:29:42 +0100

    Extend elasticsearch package
    - add monit entry
    - add basic configuration

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -1,4 +1,8 @@
-class elasticsearch {
+class elasticsearch ($publish_host, $heap_size = '256m', $cluster_name = undef) {
+
+  require 'java'
+
+  $version = '0.90.5'
 
   file {'/etc/default/elasticsearch':
     ensure => file,
@@ -6,11 +10,32 @@ class elasticsearch {
     owner => '0',
     group => '0',
     mode => '0755',
+    before => Helper::Script['install elasticsearch'],
   }
 
-  $version = '0.90.5'
+  file {'/etc/elasticsearch':
+    ensure => directory,
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+
+  file {'/etc/elasticsearch/elasticsearch.yml':
+    ensure => file,
+    content =>template('elasticsearch/elasticsearch.yml'),
+    owner => '0',
+    group => '0',
+    mode => '0755',
+    before => Helper::Script['install elasticsearch'],
+  }
+
   helper::script {'install elasticsearch':
     content => template('elasticsearch/install.sh'),
     unless => "test -x /usr/share/elasticsearch/bin/elasticsearch && /usr/share/elasticsearch/bin/elasticsearch -v | grep -q '${version}'"
+  }
+
+  @monit::entry {'elasticsearch':
+    content => template('elasticsearch/monit'),
+    require => Helper::Script['install elasticsearch'],
   }
 }
