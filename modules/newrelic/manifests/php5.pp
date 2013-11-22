40865db92b581519d14597a5a85fa537bd9282d3
Tomasz Durka <tomasz@durka.pl>
2013-11-22 12:18:30 +0100

    Refactor newrelic module

--- modules/newrelic/manifests/php5.pp
+++ modules/newrelic/manifests/php5.pp
@@ -1,43 +1,34 @@
-class newrelic::php5(
-  $licenseKey                 = undef,
-  $appname                    = undef,
-  $enabled                    = false,
-  $browser_monitoring_enabled = false
-) {
+class newrelic::php5 ($license_key, $appname = undef, $enabled = false, $browser_monitoring_enabled = false) {
 
-  if !($licenseKey) { fail("You need to provide an installation key to New Relic") }
+  include 'php5'
 
-  apt::source { 'new-relic':
+  apt::source {'new-relic':
     entries => ['deb http://apt.newrelic.com/debian/ newrelic non-free'],
-    keys => { 'newrelic' => {
+    keys => {'newrelic' => {
         key     => '548C16BF',
         key_url => 'http://download.newrelic.com/548C16BF.gpg',
       }
     }
   }
   ->
-  package { 'newrelic-php5':
+
+  package {'newrelic-php5':
     ensure => present
   }
   ->
-  exec { 'postinstall':
+
+  exec {'new-relic postinstall':
     command => 'bash -c "NR_INSTALL_SILENT=yes, NR_INSTALL_KEY=$licenseKey newrelic-install install"',
     path => ['/usr/bin', '/bin'],
   }
   ->
-  file { ['/etc/php5', '/etc/php5/conf.d/']:
-    ensure => directory,
-    owner   => '0',
-    group   => '0',
-    mode    => '0755',
-  }
-  ->
+
   file { '/etc/php5/conf.d/newrelic.ini':
     ensure  => file,
+    content => template('newrelic/php/config'),
     owner   => '0',
     group   => '0',
     mode    => '0644',
-    content => template("${module_name}/newrelic-ini"),
+    before  => Package['php5-common'],
   }
-
 }
