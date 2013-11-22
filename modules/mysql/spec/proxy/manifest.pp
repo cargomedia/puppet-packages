de96923e643d0c2a557d9ec6a1986027ab9d9ba8
Tomasz Durka <tomasz@durka.pl>
2013-11-22 02:51:28 +0100

    Add failover lua script, add missing directory, adjust spec

--- modules/mysql/spec/proxy/manifest.pp
+++ modules/mysql/spec/proxy/manifest.pp
@@ -2,5 +2,7 @@ node default {
 
   require 'monit'
 
-  class {'mysql::proxy':}
+  class {'mysql::proxy':
+    backend_addresses => ['10.10.10.12:3306', '10.10.10.13:3306'],
+  }
 }
--- modules/mysql/spec/proxy/spec.rb
+++ modules/mysql/spec/proxy/spec.rb
@@ -7,3 +7,8 @@ end
 describe command('monit summary') do
   its(:stdout) { should match /Process 'mysql-proxy'/ }
 end
+
+describe file('/etc/default/mysql-proxy') do
+  its(:content) { should match '--proxy-backend-addresses=10.10.10.12:3306' }
+  its(:content) { should match '--proxy-backend-addresses=10.10.10.13:3306' }
+end
