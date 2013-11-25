728f7f64b8e1c4d715e88794ede4897bd2750bc1
Chris Stasiak <me@krzysztof-stasiak.com>
2013-11-25 11:46:29 +0100

    Removed username/groupname param.

--- modules/cacti/manifests/agent/ssh.pp
+++ modules/cacti/manifests/agent/ssh.pp
@@ -1,14 +1,13 @@
 class cacti::agent::ssh (
-  $userName   = $cacti::params::userName,
   $userId     = $cacti::params::userId,
-  $groupName  = $cacti::params::groupName,
   $groupId    = $cacti::params::groupId
 ) inherits cacti::params {
 
+  $userName = 'cacti'
+  $groupName = 'cacti'
+
   class {'cacti':
-    userName  => $userName,
     userId    => $userId,
-    groupName => $groupName,
     groupId   => $groupId,
   }
   ->
@@ -31,7 +30,7 @@ class cacti::agent::ssh (
 
   file {"/home/${userName}/.ssh/authorized_keys":
     ensure  => file,
-    content => template("${module_name}/agent/authorized_keys"),
+    content => template("cacti/agent/authorized_keys"),
     group   => $groupName,
     owner   => $userName,
     mode    => '0750',
--- modules/mysql/manifests/database.pp
+++ modules/mysql/manifests/database.pp
@@ -2,13 +2,11 @@ define mysql::database ($user = undef) {
 
   require 'mysql::server'
 
-
   database {$name:
     ensure => present,
     provider => mysql,
     require => Service['mysql'],
   }
-
 
   if $user {
     database_grant {"${user}/${name}":
