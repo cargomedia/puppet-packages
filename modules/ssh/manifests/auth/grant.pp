fe736fc794bcf37f565ea1b3bb2a0a07bdb32b91
Tomasz Durka <tomasz@durka.pl>
2013-11-27 18:11:43 +0100

    Add id param for user::auth::grant, remove hosts and knownhosts from grant/id

--- modules/ssh/manifests/auth/grant.pp
+++ modules/ssh/manifests/auth/grant.pp
@@ -1,11 +1,6 @@
-define ssh::auth::grant ($user, $hosts = []) {
+define ssh::auth::grant ($id, $user) {
 
-  Ssh::Authorized_key <<| title == $title |>> {
+  Ssh::Authorized_key <<| title == $id |>> {
     user => $user,
-  }
-
-  @@ssh::knownhost {$hosts:
-    key => $sshrsakey,
-    tag => md5("ssh::auth ${name}"),
   }
 }
--- modules/ssh/manifests/auth/id.pp
+++ modules/ssh/manifests/auth/id.pp
@@ -6,7 +6,4 @@ define ssh::auth::id ($user) {
 
   Ssh::Key <<| title == $title |>>
   Ssh::Key <<| title == "${title}.pub" |>>
-
-  $knownhost_tag = md5("ssh::auth ${name}")
-  Ssh::Knownhost <<| tag == $knownhost_tag |>>
 }
