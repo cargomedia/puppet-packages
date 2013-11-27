d81a12c747d840950e6b92c09a58680468819936
Tomasz Durka <tomasz@durka.pl>
2013-11-27 02:47:09 +0100

    Add $hosts param for ssh::auth::grant to allow setting exported hosts, realized (added to knowhosts) on ssh::auth::id machine (using md5 as tag for valid puppet)

--- modules/ssh/manifests/auth/grant.pp
+++ modules/ssh/manifests/auth/grant.pp
@@ -1,6 +1,11 @@
-define ssh::auth::grant ($user) {
+define ssh::auth::grant ($user, $hosts = []) {
 
   Ssh::Authorized_key <<| title == $title |>> {
     user => $user,
+  }
+
+  @@ssh::knownhost {$hosts:
+    key => $sshrsakey,
+    tag => md5("ssh::auth ${name}"),
   }
 }
--- modules/ssh/manifests/auth/id.pp
+++ modules/ssh/manifests/auth/id.pp
@@ -7,4 +7,7 @@ define ssh::auth::id ($user, $ssh_dir) {
   }
 
   Ssh::Key <<| title == $title |>>
+
+  $knownhost_tag = md5("ssh::auth ${name}")
+  Ssh::Knownhost <<| tag == $knownhost_tag |>>
 }
