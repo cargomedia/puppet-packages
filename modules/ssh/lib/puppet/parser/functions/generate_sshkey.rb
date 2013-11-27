b7b520cc19e2249dfd7997a72686feb20837c5e3
Tomasz Durka <tomasz@durka.pl>
2013-11-27 12:05:09 +0100

    shell escape keyname

--- modules/ssh/lib/puppet/parser/functions/generate_sshkey.rb
+++ modules/ssh/lib/puppet/parser/functions/generate_sshkey.rb
@@ -1,11 +1,12 @@
 require 'fileutils'
+require 'shellwords'
 
 module Puppet::Parser::Functions
   newfunction(:generate_sshkey, :type => :rvalue) do |args|
     key_file = args[0]
     unless File.exists? key_file
       FileUtils.mkdir_p File.dirname key_file
-      `/usr/bin/ssh-keygen -t ssh-rsa -f #{key_file}`
+      `/usr/bin/ssh-keygen -t ssh-rsa -f #{Shellwords.escape(key_file)}`
     end
     private = File.read(key_file)
     public = File.read(key_file + '.pub').match(/ssh-rsa ([^\s]+)/)[1]
