36d2864ee9c240499c6227a926cef55cfb70bce5
Reto Kaiser <reto@cargomedia.ch>
2013-11-27 16:48:24 +0100

    Fix bash loop and cutting

--- modules/raid/templates/adaptec/set-write-cache-off.sh
+++ modules/raid/templates/adaptec/set-write-cache-off.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 
-DEVICES=$(/usr/local/sbin/arcconf-write-cache-on-devices.pl)
-for DEVICE in $DEVICES; do
-	CONTROLLER=$(echo ${DEVICE} | awk "{print $1}")
-	CHANNEL=$(echo ${DEVICE} | awk "{print $2}")
-	ID=$(echo ${DEVICE} | awk "{print $3}")
+/usr/local/sbin/arcconf-write-cache-on-devices.pl | while read DEVICE; do
+	CONTROLLER=$(echo "${DEVICE}" | cut -f 1)
+	CHANNEL=$(echo "${DEVICE}" | cut -f 2)
+	ID=$(echo "${DEVICE}" | cut -f 3)
 	set +e
 	OUT=$(arcconf SETCACHE ${CONTROLLER} DEVICE ${CHANNEL} ${ID} wt)
 	STATUS=$?
