5ee5c6fc7f7d13412e0dfb4039231f9b8efdd88f
Reto Kaiser <reto@cargomedia.ch>
2013-12-01 18:41:07 +0100

    Add network::hostname

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,22 +1,14 @@
-class network() {
+class network(
+  $fqdn
+) {
+
+  class{'network::hostname':
+    fqdn => $fqdn,
+  }
 
   $interfaces = hiera_hash('network::interfaces', {})
   create_resources('network::interface', $interfaces)
 
   $hosts = hiera_hash('network::hosts', {})
   create_resources('network::host', $hosts)
-
-  network::host {$::fqdn:
-    ipaddr => '127.0.0.1',
-    aliases => [$::hostname, 'localhost']
-  }
-
-  network::host {$::hostname:
-    ipaddr => '127.0.1.1',
-    aliases => [$::fqdn]
-  }
-
-  resources {'host':
-    purge => true
-  }
 }
