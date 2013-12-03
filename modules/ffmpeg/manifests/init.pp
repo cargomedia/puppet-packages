1d5ab1dcf612aaabf6c35cf6a1156d03f760c393
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-03 11:10:23 +0100

    service pack

--- modules/ffmpeg/manifests/init.pp
+++ modules/ffmpeg/manifests/init.pp
@@ -1,16 +1,32 @@
 class ffmpeg ($version = '1.0') {
 
   require 'deb-multimedia'
+  require 'yasm'
+  require 'x264'
 
-  package {['liba52-dev', 'libdts-dev', 'libgsm1-dev', 'libfaac-dev', 'libsdl1.2-dev', 'libspeex-dev', 'mjpegtools', 'libgpac-dev',
-            'libxfixes-dev', 'zlib1g-dev', 'libvpx-dev', 'libvdpau-dev', 'libopencore-amrnb-dev', 'libopencore-amrwb-dev']:
+  package {[
+      'liba52-0.7.4-dev',
+      'libdts-dev',
+      'libgsm1-dev',
+      'libfaac-dev',
+      'libsdl1.2-dev',
+      'libspeex-dev',
+      'mjpegtools',
+      'libgpac-dev',
+      'libxfixes-dev',
+      'zlib1g-dev',
+      'libvpx-dev',
+      'libvdpau-dev',
+      'libopencore-amrnb-dev',
+      'libopencore-amrwb-dev'
+    ]:
     ensure => present,
   }
   ->
 
   helper::script {'install ffmpeg':
     content => template('ffmpeg/install.sh'),
-    unless => "test -x /usr/local/bin/ffmpeg && (ffmpeg -version 2>/dev/null | grep -q '^ffmpeg version ${version}$')",
+    unless => "ffmpeg -version | grep -q '^ffmpeg version ${version}$'",
     timeout => 900,
   }
 
