a481b2728fe5cfae0d66d022a355144c9b9dd53a
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-03 10:31:53 +0100

    Added 'build' package; check for version

--- modules/yasm/manifests/init.pp
+++ modules/yasm/manifests/init.pp
@@ -1,8 +1,10 @@
 class yasm ($version = '1.2.0'){
 
+  require 'build'
+
   helper::script {'install yasm':
     content => template('yasm/install.sh'),
-    unless => 'test -x /usr/local/bin/yasm',
+    unless => "yasm --version 2>/dev/null | grep -q 'yasm ${version}'",
   }
 
 }
