347face03e9645f77fcca3f8c1735257c23bfbfa
Reto Kaiser <reto@cargomedia.ch>
2013-12-03 13:52:10 +0100

    Inline bash variables

--- modules/backup/templates/agent/rdiff/lvm.sh
+++ modules/backup/templates/agent/rdiff/lvm.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -e
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-HOST="<%= @host %>"
-ORIG_VOLUME="<%= @source %>"
 KEEP=2
 BACKUP_MNT=/raid-backup
-RDIFF_BACKUP_OPTS='<%= @options %>'
-RDIFF_DEST=<%= @destination %>
-VG_NAME=$(expr "$ORIG_VOLUME" : "^/.*/\(.*\)/.*$")
-LV_NAME=$(expr "$ORIG_VOLUME" : "^/.*/\(.*\)$")
+VG_NAME=$(expr "<%= @source %>" : "^/.*/\(.*\)/.*$")
+LV_NAME=$(expr "<%= @source %>" : "^/.*/\(.*\)$")
 SNAPSHOTS=$(lvs | perl -ne 'print "$1\n" if /'${LV_NAME}'-snap(\d+)/' | sort -r)
 
 for i in $SNAPSHOTS; do
@@ -28,7 +24,7 @@ lvcreate -s ${ORIG_VOLUME} -l $((10/$KEEP))%VG --name ${ORIG_VOLUME}-snap1 >/dev
 
 mkdir -p ${BACKUP_MNT}
 mount -o nouuid,inode64,nobarrier,ro ${ORIG_VOLUME}-snap1 ${BACKUP_MNT}
-ssh root@$HOST "mkdir -p ${RDIFF_DEST}"
-rdiff-backup ${RDIFF_BACKUP_OPTS} ${BACKUP_MNT} root@$HOST::${RDIFF_DEST} >/dev/null
-rdiff-backup --remove-older-than 4W --force root@$HOST::${RDIFF_DEST} >/dev/null
+ssh root@<%= @host %> "mkdir -p <%= @destination %>"
+rdiff-backup <%= @options %> ${BACKUP_MNT} root@<%= @host %>::<%= @destination %> >/dev/null
+rdiff-backup --remove-older-than 4W --force root@<%= @host %>::<%= @destination %> >/dev/null
 umount ${BACKUP_MNT}
