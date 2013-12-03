547da7d5acef5ba88f6a52b6055a912035c5fd1d
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-03 14:22:51 +0100

    Removed stderr redirection

--- modules/ffmpeg/templates/install.sh
+++ modules/ffmpeg/templates/install.sh
@@ -4,6 +4,6 @@ curl -sL http://ffmpeg.org/releases/ffmpeg-<%= @version %>.tar.gz >ffmpeg-<%= @v
 tar -xvf ffmpeg-<%= @version %>.tar.gz
 cd ffmpeg-<%= @version %>
 ./configure --enable-libspeex --enable-libx264 --enable-libfaac --enable-gpl --enable-nonfree --enable-shared --enable-version3 --enable-runtime-cpudetect
-make 2>/dev/null # Without dumping STDERR the calling ssh process will strangely hang indefinitely
+make
 make install
 ldconfig
