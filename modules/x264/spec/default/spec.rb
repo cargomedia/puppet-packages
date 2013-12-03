4e807840c16cec63b2fb0a78d34e717458dcd5f0
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-03 10:46:40 +0100

    Removed x264 module

--- modules/x264/spec/default/spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe file('/usr/local/bin/x264') do
-  it { should be_executable }
-end
--- modules/x264/templates/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-git clone git://git.videolan.org/x264
-cd x264
-./configure --enable-shared
-make
-make install
