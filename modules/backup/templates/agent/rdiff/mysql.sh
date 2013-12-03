8d6b616993522f5c77f00a349157f09fa28d3f75
Reto Kaiser <reto@cargomedia.ch>
2013-12-03 13:56:51 +0100

    Revert "Inline bash variables"
    This reverts commit 347face03e9645f77fcca3f8c1735257c23bfbfa.

--- modules/backup/templates/agent/rdiff/mysql.sh
+++ modules/backup/templates/agent/rdiff/mysql.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
-rm -rf /tmp/backup-db
+HOST="<%= @host %>"
+RDIFF_BACKUP_OPTS='<%= @options %>'
 
+rm -rf /tmp/backup-db
 /usr/sbin/monit unmonitor mysql
 /etc/init.d/mysql stop >/dev/null
 cp -a <%= @source %> /tmp/backup-db
 /etc/init.d/mysql start >/dev/null
 /usr/sbin/monit monitor mysql
 
-ssh root@<%= @host %> "mkdir -p <%= @destination %>"
-rdiff-backup <%= @options %> /tmp/backup-db root@<%= @host %>::<%= @destination %> >/dev/null
-rdiff-backup --remove-older-than 4W --force root@<%= @host %>::<%= @destination %> >/dev/null
+ssh root@$HOST "mkdir -p <%= @destination %>"
+rdiff-backup ${RDIFF_BACKUP_OPTS} /tmp/backup-db root@$HOST::<%= @destination %> >/dev/null
+rdiff-backup --remove-older-than 4W --force root@$HOST::<%= @destination %> >/dev/null
 
 rm -rf /tmp/backup-db
