35c18d631e5be055fa18ac3c994e3ce4f4436f7d
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-03 10:45:58 +0100

    removed pipe; adjusted condition...

--- modules/yasm/manifests/init.pp
+++ modules/yasm/manifests/init.pp
@@ -4,7 +4,7 @@ class yasm ($version = '1.2.0'){
 
   helper::script {'install yasm':
     content => template('yasm/install.sh'),
-    unless => "yasm --version 2>/dev/null | grep -q 'yasm ${version}'",
+    unless => "yasm --version | grep -q '^yasm ${version}$'",
   }
 
 }
