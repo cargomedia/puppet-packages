46e4a18066562288f5d7bbae3f114f1d08cfaee8
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-04 15:02:24 +0100

    Updated test - removed refresh only

--- modules/wowza/spec/cm/manifest.pp
+++ modules/wowza/spec/cm/manifest.pp
@@ -7,8 +7,6 @@ node default {
     command => '/etc/init.d/wowza start',
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     unless => '/etc/init.d/wowza status',
-    refreshonly => true,
-
   }
 
 }
