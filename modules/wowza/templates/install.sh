7ffb37b19b31f6e0be4400e39a0cf562003125a5
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-05 10:22:15 +0100

    Droped 'bash -e' in install wowza script

--- modules/wowza/templates/install.sh
+++ modules/wowza/templates/install.sh
@@ -2,7 +2,7 @@
 
 curl -sL http://www.wowza.com/downloads/WowzaMediaServer-$(echo '<%= @version %>' | tr '.' '-')/WowzaMediaServer-<%= @version %>.deb.bin > WowzaMediaServer.deb.bin
 chmod +x WowzaMediaServer.deb.bin
-echo yes | bash -e WowzaMediaServer.deb.bin
+echo yes | ./WowzaMediaServer.deb.bin
 
 echo '<%= @license %>' > /usr/local/WowzaMediaServer/conf/Server.license
 
