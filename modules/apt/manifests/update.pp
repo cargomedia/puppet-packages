ed55ac2ac9be9367dbc63c7d5274db877f2890c4
Reto Kaiser <reto@cargomedia.ch>
2013-12-05 16:33:16 +0100

    Disable proxy for apt-get update in tests

--- modules/apt/manifests/update.pp
+++ modules/apt/manifests/update.pp
@@ -1,10 +1,14 @@
-class apt::update {
+class apt::update(
+  $options = []
+) {
 
   require 'apt'
 
+  $arguments = join(prefix($options, '-o '), ' ')
+
   exec { 'apt_update':
     path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    command     => "apt-get update",
+    command     => "apt-get ${arguments} update",
     logoutput   => 'on_failure',
     refreshonly => true,
   }
