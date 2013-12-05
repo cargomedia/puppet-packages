ba83db34a9593bc3742dfd91b9b4106f8b0d1680
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-05 10:48:23 +0100

    Fixed version variable

--- modules/wowza/templates/jar/cm-wowza.sh
+++ modules/wowza/templates/jar/cm-wowza.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -sL https://github.com/cargomedia/CM-wowza/releases/download/v<% @version %>/ch.cargomedia.wms.jar > /usr/local/WowzaMediaServer/lib/ch.cargomedia.wms-<%= @version %>.jar
+curl -sL https://github.com/cargomedia/CM-wowza/releases/download/v<%= @version %>/ch.cargomedia.wms.jar > /usr/local/WowzaMediaServer/lib/ch.cargomedia.wms-<%= @version %>.jar
