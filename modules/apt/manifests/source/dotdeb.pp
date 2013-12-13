c6e4f5f3abc60e70b31cd36ef6851455d2b4f320
Reto Kaiser <reto@cargomedia.ch>
2013-12-13 10:44:23 +0100

    Add dotdeb key

--- modules/apt/manifests/source/dotdeb.pp
+++ modules/apt/manifests/source/dotdeb.pp
@@ -5,5 +5,11 @@ class apt::source::dotdeb {
     'deb http://packages.dotdeb.org squeeze all',
     'deb-src http://packages.dotdeb.org squeeze all',
     ],
+    keys => {
+      'dotdeb' => {
+        'key' => '89DF5277',
+        'key_url' => 'http://www.dotdeb.org/dotdeb.gpg',
+      }
+    },
   }
 }
