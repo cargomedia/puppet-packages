2e2195bda6c3c3eb597ffe1220944fbe31e45b13
Reto Kaiser <reto@cargomedia.ch>
2013-12-13 11:35:53 +0100

    Configure php5::fpm

--- modules/php5/manifests/fpm.pp
+++ modules/php5/manifests/fpm.pp
@@ -3,6 +3,45 @@ class php5::fpm {
   require 'php5'
   require 'apt::source::dotdeb'
 
+  file {'/etc/php5/fpm':
+    ensure => directory,
+    owner => '0',
+    group => '0',
+    mode => '0644',
+  }
+
+  file {'/etc/php5/fpm/pool.d':
+    ensure => directory,
+    owner => '0',
+    group => '0',
+    mode => '0644',
+  }
+
+  file {'/etc/php5/fpm/php-fpm.conf':
+    ensure => file,
+    content => template('php5/fpm/php-fpm.conf'),
+    owner => '0',
+    group => '0',
+    mode => '0644',
+    before => Package['php5-fpm'],
+    notify => Service['php5-fpm'],
+  }
+
+  file {'/etc/php5/fpm/pool.d/www.conf':
+    ensure => file,
+    content => template('php5/fpm/www.conf'),
+    owner => '0',
+    group => '0',
+    mode => '0644',
+    before => Package['php5-fpm'],
+    notify => Service['php5-fpm'],
+  }
+
+  php5::config {'/etc/php5/fpm/php.ini':
+    before => Package['php5-fpm'],
+    notify => Service['php5-fpm'],
+  }
+
   package {'php5-fpm':
     ensure => present,
   }
