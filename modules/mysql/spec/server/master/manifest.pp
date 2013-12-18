19e9dd6d5b8dd40e2cba04377c0b3a3c73497846
Tomasz Durka <tomasz@durka.pl>
2013-12-18 16:23:36 +0100

    Add cluster_name for master/slave tests

--- modules/mysql/spec/server/master/manifest.pp
+++ modules/mysql/spec/server/master/manifest.pp
@@ -1,6 +1,7 @@
 node default {
 
   class {'mysql::server::master':
+    cluster_name => 'foo',
     server_id => '123',
   }
 }
--- modules/mysql/spec/server/slave/manifest.pp
+++ modules/mysql/spec/server/slave/manifest.pp
@@ -1,6 +1,7 @@
 node default {
 
   class {'mysql::server::slave':
+    cluster_name => 'foo',
     server_id => '321',
   }
 }
