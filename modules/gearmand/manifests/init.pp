230512fae71687a6fce47721166fa9c9a0ad7b49
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-20 11:31:30 +0100

    Fixed unless test

--- modules/gearmand/manifests/init.pp
+++ modules/gearmand/manifests/init.pp
@@ -7,7 +7,7 @@ class gearmand ($version = '1.1.2', $series = '1.2') {
 
   helper::script {'install gearman':
     content => template('gearmand/install.sh'),
-    unless => "test -x /usr/local/sbin/gearmand  && /usr/local/sbin/gearmand --version | grep -q '${version}')",
+    unless => "test -x /usr/local/sbin/gearmand && /usr/local/sbin/gearmand --version | grep -q '${version}'",
   }
 
 }
