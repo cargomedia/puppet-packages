57c83a6c2e992878a2fc0318e6b0f7791e0b5571
Chris Stasiak <me@krzysztof-stasiak.com>
2013-12-20 11:10:46 +0100

    Fix name

--- modules/gearmand/manifests/init.pp
+++ modules/gearmand/manifests/init.pp
@@ -6,7 +6,7 @@ class gearmand ($server_version = '1.1.2', $server_series = '1.2') {
   ->
 
   helper::script {'install gearman':
-    content => template('gearman/install.sh'),
+    content => template('gearmand/install.sh'),
     unless => "test -x /usr/local/sbin/gearmand  && /usr/local/sbin/gearmand --version | grep -q '${server_version}')",
   }
 
