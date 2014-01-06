ebba919fa47e1437dd36b337ba5fa25388304710
Philipp <philipp.gysin@cargomedia.ch>
2014-01-06 18:17:37 +0100

    Refactored module composer

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -4,13 +4,12 @@ class composer($version = '1.0.0-alpha7') {
 
   $phar = '/usr/local/lib/composer.phar'
   $binary = '/usr/local/bin/composer'
-  $config = '/etc/php5/conf.d/composer.ini'
 
   exec {"curl ${phar}":
     command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > ${phar}",
     path => ['/usr/local/bin', '/usr/bin', '/bin'],
     unless => "test -f ${phar} && ${binary} --version | grep -w '${version}'",
-    require => [File[$binary], File[$config], Class['php5']],
+    require => [File[$binary], Php5::Extension::Config['suhosin'], Class['php5']],
   }
 
   file {$binary:
@@ -19,8 +18,10 @@ class composer($version = '1.0.0-alpha7') {
     mode => '0755',
   }
 
-  file {$config:
-    ensure => file,
-    source => 'puppet:///modules/composer/composer.ini',
+  php5::extension::config {'suhosin':
+    settings => {
+      'suhosin.executor.include.whitelist' => 'phar'
+    }
   }
+
 }
