26a24480259e700e0a2bb4b2e52068bbe98c982b
ppp0 <philipp.gysin@cargomedia.ch>
2014-02-05 18:10:24 +0100

    Change permissions defaults, modify sequence to be able to set permissions

--- modules/nfs/manifests/server/export.pp
+++ modules/nfs/manifests/server/export.pp
@@ -1,4 +1,4 @@
-define nfs::server::export($publicPath = $name, $localPath, $configuration, $owner = 'nobody', $group = 'nogroup', $permissions = '777') {
+define nfs::server::export($publicPath = $name, $localPath, $configuration, $owner = 'root', $group = 'root', $permissions = '640') {
 
   include 'nfs::server'
 
@@ -7,26 +7,19 @@ define nfs::server::export($publicPath = $name, $localPath, $configuration, $own
 
   $filename = md5($name)
 
-  exec {$path:
-    command => "mkdir -p ${path} && chown ${owner}:${group} ${path} && chmod ${permissions} ${path}",
-    creates => $path,
-    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  }
-  ->
-
-  exec {$lpath:
-    command => "mkdir -p ${lpath}",
-    creates => $lpath,
-    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  }
-  ->
-
   mount::entry {$path:
     source => $localPath,
     options => 'bind',
     mount => true,
   }
   ->
+
+  exec {$path:
+    command => "chown -R ${owner}:${group} ${path} && chmod -R ${permissions} ${path}",
+    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  }
+  ->
+
 
   file {"/etc/exports.d/${filename}":
     ensure => file,
