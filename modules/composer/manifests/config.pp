6f6ae6da705fb449b71aede09d11e2004ca80b46
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-05 14:49:26 +0100

    Added composer::config

---
+++ modules/composer/manifests/config.pp
@@ -0,0 +1,21 @@
+define composer::config(
+  $user = 'root',
+  $user_home = '/root',
+  $github_oauth = undef
+) {
+
+  require 'composer'
+
+  file {"${user_home}/.composer":
+    ensure => directory,
+    owner => $user,
+    mode => '0644',
+  }
+
+  file {"${user_home}/.composer/config.json":
+    ensure => file,
+    content => template('composer/config.json'),
+    owner => $user,
+    mode => '0644',
+  }
+}
