2a8fa82fb4490c1b7e1f0a16108336ccd6efd609
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-05 17:46:21 +0100

    Added group for file resource

--- modules/composer/manifests/config.pp
+++ modules/composer/manifests/config.pp
@@ -9,13 +9,15 @@ define composer::config(
   file {"${user_home}/.composer":
     ensure => directory,
     owner => $user,
+    group => $user,
     mode => '0644',
   }
 
   file {"${user_home}/.composer/config.json":
     ensure => file,
-    content => template('composer/config.json'),
+    content => template('composer/config'),
     owner => $user,
+    group => $user,
     mode => '0644',
   }
 }
