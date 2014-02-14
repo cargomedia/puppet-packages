42d716a78116d0c36d85b45bf47ae774d19879c3
ppp0 <philipp.gysin@cargomedia.ch>
2014-02-14 16:23:08 +0100

    put start as part of init.pp, adapted test

--- modules/wowza/spec/cm/manifest.pp
+++ modules/wowza/spec/cm/manifest.pp
@@ -6,11 +6,4 @@ node default {
     archive_dir => '/tmp/archive',
     rpc_url => 'http://example.com/rpc',
   }
-
-  exec {'start wowza':
-    command => '/etc/init.d/wowza start',
-    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    unless => '/etc/init.d/wowza status',
-  }
-
 }
