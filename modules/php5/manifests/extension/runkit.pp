aa19c7d8779fb8970fadec7e009b1b7832e06017
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-17 10:29:54 +0100

    Moved $commit to params

--- modules/php5/manifests/extension/runkit.pp
+++ modules/php5/manifests/extension/runkit.pp
@@ -1,11 +1,10 @@
 class php5::extension::runkit (
-  $version = '1.0.4'
+  $version = '1.0.4',
+  $commit = '5e179e978af79444d3c877d5681ea91d15134a01'
 ) {
 
   require 'build'
   require 'php5'
-
-  $commit = '5e179e978af79444d3c877d5681ea91d15134a01'
 
   helper::script {'install php5::extension::runkit':
     content => template('php5/extension/runkit/install.sh'),
