8c6247fc5d09d34f5117c1ade4285b77a4805f30
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-17 10:21:51 +0100

    Updated version to 1.0.4; updated install script

--- modules/php5/manifests/extension/runkit.pp
+++ modules/php5/manifests/extension/runkit.pp
@@ -1,9 +1,11 @@
 class php5::extension::runkit (
-  $version = '1.0.3'
+  $version = '1.0.4'
 ) {
 
   require 'build'
   require 'php5'
+
+  $commit = '5e179e978af79444d3c877d5681ea91d15134a01'
 
   helper::script {'install php5::extension::runkit':
     content => template('php5/extension/runkit/install.sh'),
