b59e5cb77a775b5e50b3cfd3881d8a5cef51f8a2
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-18 16:59:24 +0100

    Adjusted test part 1

--- modules/php5/spec/extension/apc/spec.rb
+++ modules/php5/spec/extension/apc/spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
-describe command('php --re apc') do
+describe command('php --ri apc') do
   it { should return_exit_status 0 }
+  its(:stdout) { should_not match /Warning.*apc.*loaded/ }
 end
--- modules/php5/spec/extension/gearman/spec.rb
+++ modules/php5/spec/extension/gearman/spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
-describe command('php --re gearman') do
+describe command('php --ri gearman') do
   it { should return_exit_status 0 }
+  its(:stdout) { should_not match /Warning.*gearman.*loaded/ }
 end
