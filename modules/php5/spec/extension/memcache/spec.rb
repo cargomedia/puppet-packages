81b2b515ce67b89942c229e0d53e59962d65687f
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-18 16:59:33 +0100

    Adjusted test part 2

--- modules/php5/spec/extension/memcache/spec.rb
+++ modules/php5/spec/extension/memcache/spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe command('php -m') do
-  its(:stdout) { should match /memcache/ }
+describe command('php --ri memcache') do
+  it { should return_exit_status 0 }
   its(:stdout) { should_not match /Warning.*memcache.*loaded/ }
 end
--- modules/php5/spec/extension/redis/spec.rb
+++ modules/php5/spec/extension/redis/spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
-describe command('php --re redis') do
+describe command('php --ri redis') do
   it { should return_exit_status 0 }
+  its(:stdout) { should_not match /Warning.*redis.*loaded/ }
 end
