afa6eceb6610dc4bb9032f97528b928f47db26c5
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-18 16:59:39 +0100

    Adjusted test part 3

--- modules/php5/spec/extension/xdebug/spec.rb
+++ modules/php5/spec/extension/xdebug/spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
-describe command('php --re xdebug') do
+describe command('php --ri xdebug') do
   it { should return_exit_status 0 }
+  its(:stdout) { should_not match /Warning.*xdebug.*loaded/ }
 end
