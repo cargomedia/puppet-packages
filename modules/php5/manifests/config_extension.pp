4ce42da416c12acde7c8922940e529d12509ab2c
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-18 14:30:23 +0100

    Changes test condition for squeeze

--- modules/php5/manifests/config_extension.pp
+++ modules/php5/manifests/config_extension.pp
@@ -2,10 +2,10 @@ define php5::config_extension (
   $extension = $name
 ) {
 
-  if $::lsbdistcodename == 'wheezy' {
-    $conf_dir = 'mods-available'
-  } else {
+  if $::lsbdistcodename == 'squeeze' {
     $conf_dir = 'conf.d'
+  } else {
+    $conf_dir = 'mods-available'
   }
 
   file {"/etc/php5/${conf_dir}/${extension}.ini":
@@ -17,10 +17,12 @@ define php5::config_extension (
     before => Exec['exec php5enmod']
   }
 
-  if $::lsbdistcodename == 'wheezy' {
+  if $::lsbdistcodename != 'squeeze' {
     exec {'exec php5enmod':
       command => "php5enmod ${extension}",
       path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+      subscribe => File["/etc/php5/${conf_dir}/${extension}.ini"],
+      refreshonly => true,
     }
   }
 }
