6bc29c4d18c62c1402a875fbe47f45ad859f8876
ppp0 <philipp.gysin@cargomedia.ch>
2014-02-25 09:50:04 +0100

    Changed dependency type

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -40,7 +40,7 @@ class redis {
     group => '0',
     mode => '0755',
   }
-  ->
+  ~>
 
   exec {'update-rc.d redis defaults  && /etc/init.d/redis start':
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
