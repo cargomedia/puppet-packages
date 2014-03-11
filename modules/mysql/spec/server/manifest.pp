4d1ad6dc1a1dc2a2dd164882ea5d66147b07d9d2
Reto Kaiser <reto@cargomedia.ch>
2014-03-11 10:17:27 +0100

    Remove superfluous apt-get update

--- modules/mysql/spec/server/manifest.pp
+++ modules/mysql/spec/server/manifest.pp
@@ -1,9 +1,5 @@
 node default {
 
-  class {'apt::update':
-    before => Class['mysql::server'],
-  }
-
   class {'mysql::server':
     root_password => 'foo',
     debian_sys_maint_password => 'bar',
