1558b54c4bef1b4354a5c12291c0888fb7dc1b82
Reto Kaiser <reto@cargomedia.ch>
2014-03-11 13:08:58 +0100

    Allow empty mysql password

--- modules/database/lib/puppet/type/database_user.rb
+++ modules/database/lib/puppet/type/database_user.rb
@@ -18,7 +18,7 @@ Puppet::Type.newtype(:database_user) do
 
   newproperty(:password_hash) do
     desc 'The password hash of the user. Use mysql_password() for creating such a hash.'
-    newvalue(/\w+/)
+    newvalue(/\w*/)
   end
 
   newproperty(:max_user_connections) do
--- modules/mysql/lib/puppet/parser/functions/mysql_password.rb
+++ modules/mysql/lib/puppet/parser/functions/mysql_password.rb
@@ -3,12 +3,18 @@ require 'digest/sha1'
 module Puppet::Parser::Functions
   newfunction(:mysql_password, :type => :rvalue, :doc => <<-EOS
     Returns the mysql password hash from the clear text password.
-    EOS
+  EOS
   ) do |args|
 
-    raise(Puppet::ParseError, 'mysql_password(): Wrong number of arguments ' +
-      "given (#{args.size} for 1)") if args.size != 1
+    raise(Puppet::ParseError, "mysql_password(): Wrong number of arguments given (#{args.size} for 1)") if args.size != 1
 
-    '*' + Digest::SHA1.hexdigest(Digest::SHA1.digest(args[0])).upcase
+    cleartext = args[0]
+    hash = ''
+
+    if cleartext.length > 0
+      hash = '*' + Digest::SHA1.hexdigest(Digest::SHA1.digest(cleartext)).upcase
+    end
+
+    hash
   end
 end
