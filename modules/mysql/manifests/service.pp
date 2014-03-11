3085b2264e6f0c7fba047ddf80cb1f16e7243022
Reto Kaiser <reto@cargomedia.ch>
2014-03-11 14:27:29 +0100

    Kill mysqld in case the init-script cannot shut it down properly (debian bug workaround)

--- modules/mysql/manifests/service.pp
+++ modules/mysql/manifests/service.pp
@@ -4,5 +4,10 @@ class mysql::service {
 
   service {'mysql':
     require => Package['mysql-server'],
+    hasrestart => false,
+    start => '/etc/init.d/mysql start',
+    stop => '/etc/init.d/mysql stop \
+      || (killall -SIGTERM mysqld && timeout 10 sh -c "while (pgrep mysqld); do sleep 0.1; done")', # Workaround for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=741286
+    loglevel => debug,
   }
 }
