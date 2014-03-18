99c5a1442cf64f0d682bf6f89623beed24721108
Chris Stasiak <me@krzysztof-stasiak.com>
2014-03-18 16:06:18 +0100

    Adjusted apache2::mod usage

--- modules/passenger/manifests/init.pp
+++ modules/passenger/manifests/init.pp
@@ -4,7 +4,6 @@ class passenger (
   $gem_home = '/var/lib/gems/1.9.1'
 ){
 
-  require 'ruby'
   require 'build'
   require 'apache2'
   require 'apache2::dev'
@@ -12,18 +11,9 @@ class passenger (
   $passenger_root = "${gem_home}/gems/passenger-${version}"
   $mod_passenger_location = "${gem_home}/gems/passenger-${version}/buildout/apache2/mod_passenger.so"
 
-  file {'/etc/apache2/mods-available/passenger.load':
-    ensure  => present,
-    content => template('passenger/passenger-load'),
-    owner   => '0',
-    group   => '0',
-    mode    => '0644',
-    notify  => Service['apache2'],
-    require => Exec['compile-passenger'],
-  }
-
   apache2::mod {'passenger':
     configuration => template('passenger/passenger-enabled'),
+    load_configuration => template('passenger/passenger-load'),
     notify  => Service['apache2'],
     require => Exec['compile-passenger']
   }
