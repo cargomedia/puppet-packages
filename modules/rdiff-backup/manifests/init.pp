c3fc97caaf267999573414461ae4e0db4fe690ee
Reto Kaiser <reto@cargomedia.ch>
2014-03-18 14:21:58 +0100

    Remove squeeze hack for rdiff-backup

--- modules/rdiff-backup/manifests/init.pp
+++ modules/rdiff-backup/manifests/init.pp
@@ -1,28 +1,5 @@
 class rdiff-backup {
 
-  if $::lsbdistid == 'Debian' and $::lsbmajdistrelease <= 6 {
-    file {'/etc/python2.6':
-      ensure => directory,
-      owner => '0',
-      group => '0',
-      mode => '755',
-    }
-    ->
-
-    file {'/etc/python2.6/sitecustomize.py':
-      ensure => file,
-      content => template('rdiff-backup/python-sitecustomize.py'),
-      owner => '0',
-      group => '0',
-      mode => '644',
-    }
-    ->
-
-    package {'python':
-      ensure => present,
-    }
-  }
-
   package {'rdiff-backup':
     ensure => installed,
   }
