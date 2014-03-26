2088882ffdd71dbecc919b40bfd85a925aa3c23b
Chris Stasiak <me@krzysztof-stasiak.com>
2014-03-26 12:03:08 +0100

    Change output to sql

--- modules/backup/templates/agent/rdiff/create.sh
+++ modules/backup/templates/agent/rdiff/create.sh
@@ -51,14 +51,13 @@ case "${TYPE}" in
 
     ;;
 "mysql-dump")
-    rm -rf /tmp/backup-db-dump
-    chown mysql:mysql /tmp/backup-db-dump
-    mysqldump --tab=/tmp/backup-db-dump ${SOURCE_PATH}
+    rm -rf /tmp/backup-db
+    mysqldump ${SOURCE_PATH} > /tmp/backup-db/sql-dump
 
     ssh root@${HOST} "mkdir -p ${DEST_PATH}"
-    rdiff-backup ${RDIFF_OPTIONS} /tmp/backup-db-dump root@${HOST}::${DEST_PATH} >/dev/null
+    rdiff-backup ${RDIFF_OPTIONS} /tmp/backup-db/sql-dump root@${HOST}::${DEST_PATH} >/dev/null
 
-    rm -rf /tmp/backup-db-dump
+    rm -rf /tmp/backup-db
 
     ;;
 "lvm")
