d0f08056620c5e9da29da785e5d7ab7cf56c3343
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-09 13:56:41 +0200

    Init class with apt source + key

---
+++ modules/mongo/manifests/init.pp
@@ -0,0 +1,15 @@
+class mongo {
+
+  apt::source {'mongodb':
+    entries => [
+      "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen",
+    ],
+    keys => {
+      'mongodb' => {
+        'key' => '7F0CEB10',
+        'key_server' => 'keyserver.ubuntu.com',
+      }
+    },
+  }
+
+}
