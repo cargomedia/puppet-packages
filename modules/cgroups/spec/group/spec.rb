fd282d201effd33e893ea6e574ab752cd87d35a0
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-16 19:07:41 +0200

    Updated test

--- modules/cgroups/spec/group/spec.rb
+++ modules/cgroups/spec/group/spec.rb
@@ -17,3 +17,17 @@ describe command('cgexec -g cpu:vagrant uname -ar') do
   it { should return_exit_status 0 }
   its(:stdout) { should match /Debian/ }
 end
+
+describe command('cgget puppet --all') do
+  it { should return_exit_status 0 }
+  its(:stdout) { should match /cpu.shares: 1024/ }
+  its(:stdout) { should match /cpuset.mems: 0/ }
+  its(:stdout) { should match /cpuset.cpus: 0/ }
+end
+
+describe command('cgget vagrant --all') do
+  it { should return_exit_status 0 }
+  its(:stdout) { should match /cpu.shares: 200/ }
+  its(:stdout) { should match /cpuset.mems: 0/ }
+  its(:stdout) { should match /cpuset.cpus: 0/ }
+end
