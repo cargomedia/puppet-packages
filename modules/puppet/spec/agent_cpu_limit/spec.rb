11f1d2d15fd65d795b23400d2e9af67d63c81c07
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-24 10:56:02 +0200

    Added assert to check if puppet daemon PID is handled by cgroups

--- modules/puppet/spec/agent_cpu_limit/spec.rb
+++ modules/puppet/spec/agent_cpu_limit/spec.rb
@@ -4,3 +4,7 @@ describe command('cgget puppet-agent-daemon --all') do
   it { should return_exit_status 0 }
   its(:stdout) { should match /cpu.shares: 50/ }
 end
+
+describe command('grep -q $(pgrep --full "/usr/bin/ruby /usr/bin/puppet agent") /sys/fs/cgroup/puppet-agent-daemon/tasks') do
+  it { should return_exit_status 0 }
+end
