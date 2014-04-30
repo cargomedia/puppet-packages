11a8f2972c152d4713246c71d25795a7de748317
tomaszdurka <tomasz@durka.pl>
2014-04-30 23:59:44 +0200

    Adjust to standards

--- modules/php5/manifests/extension/mongo.pp
+++ modules/php5/manifests/extension/mongo.pp
@@ -5,8 +5,7 @@ class php5::extension::mongo($version = '1.5.1') {
 
   helper::script {'install php5::mongo':
     content => template('php5/extension/mongo/install.sh'),
-    unless => "php --re mongo | grep 'version' | grep ' ${version} '",
-    require => Class['php5'],
+    unless => "php --re mongo | grep 'mongo version' | grep ' ${version} '",
   }
   ->
 
