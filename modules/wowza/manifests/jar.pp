215c4a0cf083ae8d555b0bbd432fedc9e874004a
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-09 15:07:06 +0200

    Updated vars name

--- modules/wowza/manifests/jar.pp
+++ modules/wowza/manifests/jar.pp
@@ -3,18 +3,18 @@ define wowza::jar (
   $version
 ) {
 
-  $source = "/usr/local/WowzaStreamingEngine/lib/lib-versions/${name}-${version}.jar"
-  $destination = "/usr/local/WowzaStreamingEngine/lib/${name}.jar"
+  $path = "/usr/local/WowzaStreamingEngine/lib/lib-versions/${name}-${version}.jar"
+  $symlink = "/usr/local/WowzaStreamingEngine/lib/${name}.jar"
 
   exec {"install jar: ${name}":
-    command => "${get_command} > ${source}",
-    unless => "test -h ${destination} && test $(readlink ${destination}) = ${source}",
+    command => "${get_command} > ${path}",
+    unless => "test -h ${symlink} && test $(readlink ${symlink}) = ${path}",
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
   ~>
 
-  file {$destination:
-    ensure => $source,
+  file {$symlink:
+    ensure => $path,
   }
 
 }
