d4675e228882a936fb6fb08c8c27c25653531d2a
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-09 15:02:23 +0200

    Removed old installation scripts

--- modules/wowza/templates/jar/cm-wowza.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-VERSION_PATH=/usr/local/WowzaStreamingEngine/lib/lib-versions/ch.cargomedia.wms-<%= @version %>.jar
-LIB_PATH=/usr/local/WowzaStreamingEngine/lib/ch.cargomedia.wms.jar
-curl -sL https://github.com/cargomedia/CM-wowza/releases/download/v<%= @version %>/ch.cargomedia.wms.jar > $VERSION_PATH
-
-rm -f $LIB_PATH
-ln -s $VERSION_PATH $LIB_PATH
--- modules/wowza/templates/jar/simple-json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-VERSION_PATH=/usr/local/WowzaStreamingEngine/lib/lib-versions/json-simple-<%= @version %>.jar
-LIB_PATH=/usr/local/WowzaStreamingEngine/lib/json-simple.jar
-curl -sL http://json-simple.googlecode.com/files/json-simple-<%= @version %>.jar > $VERSION_PATH
-
-rm -f $LIB_PATH
-ln -s $VERSION_PATH $LIB_PATH
