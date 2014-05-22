6cd5346e5dc03ad3b84e5727fee9082fea55b9f7
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-22 12:24:14 +0200

    Added core mongos

---
+++ modules/mongodb/manifests/core/mongos.pp
@@ -0,0 +1,50 @@
+define mongodb::core::mongos (
+  $config_servers,
+  $port = 27017,
+  $bind_ip = undef,
+  $fork = false,
+  $log_dir = '/var/log/mongodb',
+  $log_append = true,
+  $options = []
+) {
+
+  require 'mongodb'
+
+  $daemon = 'mongos'
+  $instance_name = "${daemon}_${name}"
+
+  file {
+    "/etc/mongodb/${instance_name}.conf":
+      ensure  => file,
+      content => template('mongodb/mongos/conf'),
+      mode    => '0644',
+      owner   => 'mongodb',
+      group   => 'mongodb',
+      notify  => Service[$instance_name];
+
+    "/etc/init.d/${instance_name}":
+      ensure  => file,
+      content => template('mongodb/init'),
+      mode    => '0755',
+      owner   => 'mongodb',
+      group   => 'mongodb',
+      notify  => Service[$instance_name];
+  }
+  ~>
+
+  exec {"/etc/init.d/${instance_name} start":
+    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    refreshonly => true,
+  }
+
+  service {$instance_name:
+    enable => true,
+    hasrestart => false,
+  }
+
+  @monit::entry {$instance_name:
+    content => template('mongodb/monit'),
+    require => Service[$instance_name],
+  }
+
+}
