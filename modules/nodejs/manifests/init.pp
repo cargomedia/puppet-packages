32fecdad7c6d72c9c5c8f480b8f341f291691d56
Reto Kaiser <reto@cargomedia.ch>
2014-05-23 16:14:03 +0200

    Stick to specific npm version

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,4 +1,6 @@
-class nodejs {
+class nodejs(
+  $npm_version = '1.4.10'
+) {
 
   package {'nodejs':
     ensure => present,
@@ -8,7 +10,7 @@ class nodejs {
   }
 
   exec {'install npm':
-    command => 'curl https://www.npmjs.org/install.sh | clean=yes sh',
+    command => "curl https://www.npmjs.org/install.sh | clean=yes npm_install=${npm_version} sh",
     unless => 'test -x /usr/bin/npm && /usr/bin/npm -v',
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     require => Package['nodejs-legacy'],
