e9202966bf3a6991ffbab1cd62ea9718008814f6
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-23 16:03:28 +0200

    Improved mongodb_replset custom function

--- modules/mongodb/lib/puppet/provider/mongodb_replset/mongo.rb
+++ modules/mongodb/lib/puppet/provider/mongodb_replset/mongo.rb
@@ -10,15 +10,31 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo) do
 
   commands :mongo => 'mongo'
 
+  def block_until_mongodb(tries = 10, host = '')
+    begin
+      mongo('--quiet', '--host', host, '--eval', 'db.getMongo()')
+    rescue
+      debug('MongoDB server not ready, retrying')
+      sleep 2
+      retry unless (tries -= 1) <= 0
+    end
+  end
+
   def create
-    alive_members = members_present
-    hostsconf = alive_members.each_with_index.map do |host, id|
-      "{ _id: #{id}, host: \"#{host}\" }"
-    end.join(',')
-    conf = "{ _id: \"#{@resource[:name]}\", members: [ #{hostsconf} ] }"
-    output = self.rs_initiate(conf, alive_members[0])
-    if output['ok'] == 0
-      raise Puppet::Error, "rs.initiate() failed for replicaset #{@resource[:name]}: #{output['errmsg']}"
+    if @resource[:arbiter]
+      if master = master_host()
+        self.rs_addArb(@resource[:arbiter], master)
+      end
+    else
+      alive_members = members_present
+      hostsconf = alive_members.each_with_index.map do |host, id|
+        "{ _id: #{id}, host: \"#{host}\" }"
+      end.join(',')
+      conf = "{ _id: \"#{@resource[:name]}\", members: [ #{hostsconf} ] }"
+      output = self.rs_initiate(conf, alive_members[0])
+      if output['ok'] == 0
+        raise Puppet::Error, "rs.initiate() failed for replicaset #{@resource[:name]}: #{output['errmsg']}"
+      end
     end
   end
 
@@ -30,10 +46,11 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo) do
     is_configured = false
     @resource[:members].each do |host|
       begin
+        block_until_mongodb(@resource[:tries], host)
         debug "Checking replicaset member #{host} ..."
         status = self.rs_status(host)
         if status.has_key?('errmsg') and status['errmsg'] == 'not running with --replSet'
-            raise Puppet::Error, "Can't configure replicaset #{@resource[:name]}, host #{host} is not supposed to be part of a replicaset."
+          raise Puppet::Error, "Can't configure replicaset #{@resource[:name]}, host #{host} is not supposed to be part of a replicaset."
         end
         if status.has_key?('set')
           if status['set'] != @resource[:name]
@@ -47,9 +64,26 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo) do
       end
     end
 
+    if @resource[:arbiter]
+      is_configured = false
+      if master = master_host()
+        status = self.rs_status(master)
+        if status.has_key?('members')
+          status['members'].each do |host|
+            if host['stateStr'] == 'ARBITER' and host['name'] == @resource[:arbiter]
+              is_configured = true
+            elsif host['name'] == @resource[:arbiter]
+              self.rs_remove(@resource[:arbiter], master)
+            end
+          end
+        end
+      end
+    end
+
     if failcount == @resource[:members].length
       raise Puppet::Error, "Can't connect to any member of replicaset #{@resource[:name]}."
     end
+
     return is_configured
   end
 
@@ -110,9 +144,13 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo) do
 
   def master_host
     @resource[:members].each do |host|
-      status = self.db_ismaster(host)
-      if status.has_key?('primary')
-        return status['primary']
+      begin
+        status = self.db_ismaster(host)
+        if status.has_key?('primary')
+          return status['primary']
+        end
+      rescue
+        # do nothing
       end
     end
     false
@@ -123,8 +161,7 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo) do
   end
 
   def rs_initiate(conf, host)
-    return self.mongo_command("rs.initiate(#{conf})", @resource[:members][0])
-
+    return self.mongo_command("rs.initiate(#{conf})", host)
   end
 
   def rs_status(host)
@@ -133,6 +170,14 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo) do
 
   def rs_add(host, master)
     self.mongo_command("rs.add(\"#{host}\")", master)
+  end
+
+  def rs_addArb(host, master)
+    self.mongo_command("rs.addArb(\"#{host}\")", master)
+  end
+
+  def rs_remove(host, master)
+    self.mongo_command("rs.remove(\"#{host}\")", master)
   end
 
 end
--- modules/mongodb/lib/puppet/type/mongodb_replset.rb
+++ modules/mongodb/lib/puppet/type/mongodb_replset.rb
@@ -17,12 +17,30 @@ Puppet::Type.newtype(:mongodb_replset) do
     desc "The name of the replicaSet"
   end
 
+  newparam(:tries) do
+    desc "The maximum amount of two second tries to wait MongoDB startup."
+    defaultto 2
+    newvalues(/^\d+$/)
+    munge do |value|
+      Integer(value)
+    end
+  end
+
   newproperty(:members, :array_matching => :all) do
     desc "The replicaSet members"
 
     def insync?(is)
       is.sort == should.sort
     end
+  end
+
+  newparam(:member) do
+    desc "Host:port of member which run this command"
+  end
+
+  newparam(:arbiter) do
+    desc "Set if host is an arbiter of replicaset"
+    defaultto false
   end
 
   autorequire(:package) do
