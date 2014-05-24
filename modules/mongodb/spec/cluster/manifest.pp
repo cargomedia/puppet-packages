6db58b63b83d08c76a3ac526be8a22445ab9f82c
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-24 19:17:17 +0200

    Updated cluster test

--- modules/mongodb/spec/cluster/manifest.pp
+++ modules/mongodb/spec/cluster/manifest.pp
@@ -6,49 +6,55 @@ node default {
     config_server => true,
     port => 28000,
   }
+  ->
 
   mongodb::core::mongod {'config2':
     config_server => true,
     port => 28001,
   }
+  ->
 
   mongodb::core::mongod {'config3':
     config_server => true,
     port => 28002,
   }
+  ->
 
   mongodb::core::mongod {'shard1':
     port => 27000,
     shard_server => true,
   }
+  ->
 
   mongodb::core::mongod {'shard2':
     port => 27001,
     shard_server => true,
   }
+  ->
 
   mongodb::core::mongos {'router1':
     config_servers => ['127.0.0.1:28000', '127.0.0.1:28001', '127.0.0.1:28002'],
   }
   ->
 
-  mongodb_replicaset {'rs':
+  mongodb_database {'testdb':
     ensure => present,
-    members => ['localhost:27000', 'localhost:27001']
+    router => 'localhost:27017',
   }
   ->
 
-  mongodb_database {'testdb':
-    ensure   => present,
-    tries    => 10,
+  mongodb_user {'testuser':
+    database => 'testdb',
+    password_hash => 'password',
+    router => 'localhost:27017',
   }
+  ->
 
   mongodb_shard {'testdb':
     ensure => present,
+    collections => '*',
+    rules => {},
     router => 'localhost:27017',
-    collections => {
-      '*' => '_id'
-    },
   }
 
 }
