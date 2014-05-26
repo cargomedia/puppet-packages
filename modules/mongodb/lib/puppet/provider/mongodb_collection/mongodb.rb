a4a3603b6e4a3a7da24f1b125b1d6b2bbc9d74f0
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-26 17:40:26 +0200

    Add collection sharding by key

--- modules/mongodb/lib/puppet/provider/mongodb_collection/mongodb.rb
+++ modules/mongodb/lib/puppet/provider/mongodb_collection/mongodb.rb
@@ -6,30 +6,36 @@ Puppet::Type.type(:mongodb_collection).provide(:mongodb) do
 
   commands :mongo => 'mongo'
 
-  def block_until_mongodb(tries = 10)
-    begin
-      mongo('--quiet', '--eval', 'db.getMongo()')
-    rescue
-      debug('MongoDB server not ready, retrying')
-      sleep 2
-      retry unless (tries -= 1) <= 0
+  def create
+    if @resource[:name] == '__all__'
+      if @resource[:shard_enabled]
+        # dont create collection
+        # get list of collections
+        # loop over collection and enable sharding if needed
+      end
+    else
+      mongo(@resource[:database], '--quiet', '--eval', "db.createCollection('#{@resource[:name]}')")
+      if @resource[:shard_enabled]
+        if !sh_issharded(@resource[:name], @resource[:database], @resource[:router])
+          sh_shard(@resource[:name], @resource[:database], @resource[:shard_key], @resource[:router])
+        end
+      end
     end
   end
 
-  def create
-    collection_name = @resource[:name]
-    mongo(@resource[:database], '--quiet', '--eval', "db.createCollection(#{collection_name})")
-  end
-
   def destroy
-    collection_name = @resource[:name]
-    mongo(@resource[:database], '--quiet', '--eval', "db.#{collection_name}.drop()")
+    mongo(@resource[:database], '--quiet', '--eval', "db.#{@resource[:name]}.drop()")
   end
 
   def exists?
-    block_until_mongodb(@resource[:tries])
-    db_name = @resource[:database]
-    mongo("--quiet", "--eval", "db.getMongo().getDB('#{db_name}').getCollectionNames()").split(",").include?(@resource[:name])
+    if @resource[:name] == '__all__'
+      return false
+    end
+    col_exists = mongo("--quiet", "--eval", "db.getMongo().getDB('#{@resource[:database]}').getCollectionNames()").split(",").include?(@resource[:name])
+    if @resource[:shard_enabled] and col_exists
+      return sh_issharded(@resource[:name], @resource[:database], @resource[:router])
+    end
+    col_exists
   end
 
   def mongo_command(command, host, retries=4)
@@ -52,28 +58,25 @@ Puppet::Type.type(:mongodb_collection).provide(:mongodb) do
     # Dirty hack to remove JavaScript objects
     output.gsub!(/ISODate\((.+?)\)/, '\1 ')
     output.gsub!(/Timestamp\((.+?)\)/, '[\1]')
+    output.gsub!(/ObjectId\((.+?)\)/, '1')
+
     JSON.parse(output)
   end
 
-  def rules
-    {}
+  def db_collections
+    self.mongo_command("db.getMongo().getDB('#{@resource[:database]}').getCollectionNames()", master)
   end
 
-  def rules(settings)
-    if settings.empty? then
-      # apply default sharding for all
-    end
+  def sh_shard(collection, dbname, key, master)
+    self.mongo_command("sh.shardCollection(\"#{dbname}.#{collection}\", {'#{key}': 1})", master)
   end
 
-  def sh_shard_col(collection, dbname, key, unique, master)
-    self.mongo_command("sh.shardCollection(\"#{dbname}.#{collection}\", \"#{key}\", \"#{unique}\")", master)
+  def sh_status(collection, dbname, master)
+    self.mongo_command("db.getMongo().getDB('#{dbname}').getCollection('#{collection}').stats()", master)
   end
 
   def sh_issharded(collection, dbname, master)
-  end
-
-  def sh_status_col(collection, dbname, master)
-    self.mongo_command("db.#{collection}.stats()", master)
+    self.sh_status(collection, dbname, master)['sharded']
   end
 
 end
