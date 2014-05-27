81ed409e81b84a62c8f4d61791528122060004a9
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-27 13:44:21 +0200

    Implemented db create, remove, shard enable

--- modules/mongodb/lib/puppet/provider/mongodb_database/mongodb.rb
+++ modules/mongodb/lib/puppet/provider/mongodb_database/mongodb.rb
@@ -18,15 +18,22 @@ Puppet::Type.type(:mongodb_database).provide(:mongodb) do
 
   def create
     mongo(@resource[:name], '--quiet', '--eval', "db.dummyData.insert({\"created_by_puppet\": 1})")
+    if @resource[:shard] and !self.sh_issharded(@resource[:name], @resource[:router])
+      self.sh_enable(@resource[:name], @resource[:router])
+    end
   end
 
   def destroy
-    mongo(@resource[:name], '--quiet', '--eval', 'db.dropDatabase()')
+    mongo(@resource[:name], '--quiet', '--host', @resource[:router], '--eval', 'db.dropDatabase()')
   end
 
   def exists?
     block_until_mongodb(@resource[:tries])
-    mongo("--quiet", "--eval", 'db.getMongo().getDBNames()').split(",").include?(@resource[:name])
+    db_exists = mongo("--quiet", "--eval", 'db.getMongo().getDBNames()').split(",").include?(@resource[:name])
+    if @resource[:ensure].to_s != 'absent' and @resource[:shard] and db_exists
+      return self.sh_issharded(@resource[:name], @resource[:router])
+    end
+    db_exists
   end
 
   def mongo_command(command, host, retries=4)
@@ -53,10 +60,12 @@ Puppet::Type.type(:mongodb_database).provide(:mongodb) do
   end
 
   def sh_enable(dbname, master)
-    self.mongo_command("sh.enableSharding(#{dbname})", master)
+    self.mongo_command("sh.enableSharding('#{dbname}')", master)
   end
 
-  def sh_status_db(dbname, master)
+  def sh_issharded(dbname, master)
+    output = self.mongo('config', '--quiet', '--host', master, '--eval', "printjson(db.databases.find({\"_id\": \"#{dbname}\", \"partitioned\": true}).count())")
+    1 == output.to_i
   end
 
 end
