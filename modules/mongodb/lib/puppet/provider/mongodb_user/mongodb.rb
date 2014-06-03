d5fe77a1b3439db088bc791c4c0909cf8b239f6d
Chris Stasiak <me@krzysztof-stasiak.com>
2014-06-03 12:44:32 +0200

    mongodb_user added isprimary() ismaster()

--- modules/mongodb/lib/puppet/provider/mongodb_user/mongodb.rb
+++ modules/mongodb/lib/puppet/provider/mongodb_user/mongodb.rb
@@ -26,10 +26,17 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb) do
 
   def exists?
     block_until_mongodb(@resource[:tries])
+    if self.db_isprimary(@resource[:router]) == false
+      warn ('Cannot add user on not primary/master member!')
+      return true
+    end
     mongo(@resource[:database], '--quiet', '--host', @resource[:router], '--eval', "db.system.users.find({user:\"#{@resource[:name]}\"}).count()").strip.eql?('1')
   end
 
   def password_hash
+    if !self.db_isprimary(@resource[:router])
+      return @resource[:password_hash]
+    end
     mongo(@resource[:database], '--quiet', '--host', @resource[:router], '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"pwd\"]").strip
   end
 
@@ -38,11 +45,47 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb) do
   end
 
   def roles
+    if !self.db_isprimary(@resource[:router])
+      return @resource[:roles]
+    end
     mongo(@resource[:database], '--quiet', '--host', @resource[:router], '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"roles\"]").strip.split(",").sort
   end
 
   def roles=(value)
     mongo(@resource[:database], '--quiet', '--host', @resource[:router], '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {roles: #{@resource[:roles].inspect}}})")
+  end
+
+  def mongo_command(command, host, retries=4)
+    # Allow waiting for mongod to become ready
+    # Wait for 2 seconds initially and double the delay at each retry
+    wait = 2
+    begin
+      output = self.mongo('--quiet', '--host', host, '--eval', "printjson(#{command})")
+    rescue Puppet::ExecutionFailure => e
+      if e =~ /Error: couldn't connect to server/ and wait <= 2**max_wait
+        info("Waiting #{wait} seconds for mongod to become available")
+        sleep wait
+        wait *= 2
+        retry
+      else
+        raise
+      end
+    end
+
+    # Dirty hack to remove JavaScript objects
+    output.gsub!(/ISODate\((.+?)\)/, '\1 ')
+    output.gsub!(/Timestamp\((.+?)\)/, '[\1]')
+    JSON.parse(output)
+  end
+
+  def db_ismaster(host)
+    status = self.mongo_command("db.isMaster()", host)
+    status['ismaster'] == true
+  end
+
+  def db_isprimary(host)
+    status = self.mongo_command("db.isMaster()", host)
+    status['ismaster'] == true and status['secondary'] == false
   end
 
 end
