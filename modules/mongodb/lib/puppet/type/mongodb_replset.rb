4b3f7f3c100a6f19990260481c87630a4508c791
Chris Stasiak <me@krzysztof-stasiak.com>
2014-06-10 12:45:46 +0200

    DOnt validate members/arbiter of replicaset

--- modules/mongodb/lib/puppet/type/mongodb_replset.rb
+++ modules/mongodb/lib/puppet/type/mongodb_replset.rb
@@ -7,24 +7,17 @@ Puppet::Type.newtype(:mongodb_replset) do
     desc 'The name of the replicaSet'
   end
 
+  newparam(:arbiter) do
+    desc 'Hostname of arbiter'
+    defaultto ''
+  end
+
   newproperty(:members, :array_matching => :all) do
     desc 'Hostnames of members'
-
-    validate do |value|
-      arbiter = @resources[:arbiter]
-      if !arbiter.nil? and value.include?(arbiter)
-        raise Puppet::Error, 'Members shouldnt contain arbiter'
-      end
-    end
 
     def insync?(is)
       is.sort == should.sort
     end
-  end
-
-  newparam(:arbiter) do
-    desc 'Hostname of arbiter'
-    defaultto nil
   end
 
   autorequire(:package) do
