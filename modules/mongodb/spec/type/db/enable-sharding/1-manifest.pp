f91d20cf5d0f9b5b80185ba9deacd869607ad65a
Chris Stasiak <me@krzysztof-stasiak.com>
2014-06-12 15:01:59 +0200

    Added test for mongodb_database: create, enable-sharding

---
+++ modules/mongodb/spec/type/db/enable-sharding/1-manifest.pp
@@ -0,0 +1,33 @@
+node default {
+
+  mongodb::core::mongod {'config':
+    config_server => true,
+    port => 27019
+  }
+  ->
+
+  exec {'wait for config server up':
+    command => 'while ! (mongo --quiet --host localhost:27019 --eval \'db.getMongo()\'); do sleep 0.5; done',
+    provider => shell,
+    timeout => 30,
+  }
+  ->
+
+  mongodb::core::mongos {'router':
+    port => 27017,
+    config_servers => ['127.0.0.1:27019'],
+  }
+  ->
+
+  class {'mongodb::role::shard':
+    port => 27018,
+    router => 'localhost:27017'
+  }
+  ->
+
+  mongodb_database {'testdb':
+    ensure => present,
+    router => 'localhost:27017',
+  }
+
+}
