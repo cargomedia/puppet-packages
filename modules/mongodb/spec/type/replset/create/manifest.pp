0590664c7356decad34375dc624d0d2be84661e8
Chris Stasiak <me@krzysztof-stasiak.com>
2014-06-12 16:19:03 +0200

    Wait for secondary in replset-create test

--- modules/mongodb/spec/type/replset/create/manifest.pp
+++ modules/mongodb/spec/type/replset/create/manifest.pp
@@ -27,5 +27,12 @@ node default {
     members => ['localhost:27001', 'localhost:27002'],
     arbiters => ['localhost:27003'],
   }
+  ->
+
+  exec {'wait for secondary':
+    command => 'while ! (mongo --quiet --host localhost:27001 --eval "printjson(rs.status())" | grep "SECONDARY"); do sleep 0.5; done',
+    provider => shell,
+    timeout => 30,
+  }
 
 }
