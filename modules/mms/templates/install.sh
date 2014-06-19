5d1fe1adf7d6c1516660c6ab910932290e8b5dba
Chris Stasiak <me@krzysztof-stasiak.com>
2014-06-19 12:03:03 +0200

    Fixed service name and download url

--- modules/mms/templates/install.sh
+++ modules/mms/templates/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-curl -Ls https://mms.mongodb.com/download/agent/monitoring/<%= @agent_name %>_<%= @version %>-1_amd64.deb > <%= @agent_name %>.deb
+curl -Ls https://mms.mongodb.com/download/agent/monitoring/mongodb-<%= @agent_name %>-agent_<%= @version %>-1_amd64.deb > <%= @agent_name %>.deb
 sudo dpkg -i <%= @agent_name %>.deb
