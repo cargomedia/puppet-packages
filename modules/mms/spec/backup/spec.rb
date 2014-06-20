420d23fba0461685e7f4f2cda3355c33271bae39
Chris Stasiak <me@krzysztof-stasiak.com>
2014-06-20 11:07:56 +0200

    Updated tests - checking for monit entry

--- modules/mms/spec/backup/spec.rb
+++ modules/mms/spec/backup/spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe service('mms-backup') do
   it { should be_enabled }
   it { should be_running }
-  it { should be_monitored_by('monit') }
 end
 
 describe file ('/etc/mongodb-mms/backup-agent.config') do
   its(:content) { should match /mmsApiKey=test-key/ }
+end
+
+describe command('monit summary') do
+  its(:stdout) { should match /Process 'mms-backup'/ }
 end
--- modules/mms/spec/monitoring/spec.rb
+++ modules/mms/spec/monitoring/spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe service('mms-monitoring') do
   it { should be_enabled }
   it { should be_running }
-  it { should be_monitored_by('monit') }
 end
 
 describe file ('/etc/mongodb-mms/monitoring-agent.config') do
   its(:content) { should match /mmsApiKey=test-key/ }
   its(:content) { should match /globalAuthUsername=mms/ }
   its(:content) { should match /globalAuthPassword=mms/ }
+end
+
+describe command('monit summary') do
+  its(:stdout) { should match /Process 'mms-monitoring'/ }
 end
