c3f6be5182460471323d39c492e82b9a3e1b4083
ppp0 <philipp.gysin@cargomedia.ch>
2014-06-23 13:07:24 +0200

    Add bipbip plugin to monit service/module

--- modules/monit/manifests/service.pp
+++ modules/monit/manifests/service.pp
@@ -3,5 +3,15 @@ class monit::service {
   require 'monit'
 
   service {'monit':
+    hasrestart => true,
+  }
+
+  @bipbip::entry {'monit':
+    plugin => 'monit',
+    options => {
+      'host' => 'localhost',
+      'auth' => 'false'
+    },
+    require => Service['monit'],
   }
 }
