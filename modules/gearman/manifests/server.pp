7dcc19071488faf27f3fc191cefbe73952bf53ed
ppp0 <philipp.gysin@cargomedia.ch>
2014-06-24 14:54:12 +0200

    Reintroduce parameters to new class gearman, use /etc/default

--- modules/gearman/manifests/server.pp
+++ modules/gearman/manifests/server.pp
@@ -1,6 +1,15 @@
-class gearman::server {
+class gearman::server(
+  $persistence = 'none',
+  $jobretries = 0,
+) {
 
   require 'apt::source::cargomedia'
+
+  case $persistence {
+    none:    { $daemon_args = [] }
+    sqlite3: { $daemon_args = ['-q libsqlite3', "--libsqlite3-db=/var/log/gearman-job-server/gearman-persist.sqlite3"] }
+    default: { fail('Only sqlite3-based persistent queues supported right now') }
+  }
 
   package {'gearman-job-server':
     ensure => present,
@@ -8,6 +17,7 @@ class gearman::server {
 
   service {'gearman-job-server':
     hasrestart => true,
+    require => Package['gearman-job-server'],
   }
 
   @monit::entry {'gearman-job-server':
@@ -22,6 +32,16 @@ class gearman::server {
       'port' => '4730',
     },
     require => Service['gearman-job-server'],
+  }
+
+  file {'/etc/default/gearman-job-server':
+    ensure => file,
+    content => template('gearman/default'),
+    owner => '0',
+    group => '0',
+    mode => '0644',
+    before => Package['gearman-job-server'],
+    notify => Service['gearman-job-server'],
   }
 
 }
