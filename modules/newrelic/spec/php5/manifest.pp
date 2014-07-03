048e80611562647a6348f8dd38321c99ac32259a
ppp0 <philipp.gysin@cargomedia.ch>
2014-07-03 20:36:29 +0200

    Delete newrelic::php5

--- modules/newrelic/spec/php5/manifest.pp
+++
@@ -1,9 +0,0 @@
-node default {
-
-  class {'newrelic::php5':
-    license_key => 'xxxxx',
-    appname => 'bar',
-    enabled => true,
-    browser_monitoring_enabled => true,
-  }
-}
--- modules/newrelic/spec/php5/spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe package('newrelic-php5') do
-  it { should be_installed }
-end
-
-describe command('php --re newrelic') do
-  it { should return_exit_status 0 }
-end
