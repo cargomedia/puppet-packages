8cae3313b5a80f12dfaa13b58af1cbb281abb339
ppp0 <philipp.gysin@cargomedia.ch>
2014-07-09 18:34:16 +0200

    Manifest change and test specs

--- modules/monit/spec/alerting/spec.rb
+++ modules/monit/spec/alerting/spec.rb
@@ -24,6 +24,20 @@ describe file('/etc/monit/conf.d/alert') do
   its(:content) { should match /set alert/ }
 end
 
+describe command('monit-silent foo bar') do
+  it { should return_exit_status 1 }
+end
+
+describe command('monit-silent unmonitor root') do
+  it { should return_exit_status 0 }
+end
+
+describe command('echo q | mail -u vagrant | grep monit | wc -l ') do
+  it { should return_exit_status 0 }
+  its(:stdout) { should match /1\n/ }
+end
+
+
 describe process('monit') do
   it { should be_running }
 end
