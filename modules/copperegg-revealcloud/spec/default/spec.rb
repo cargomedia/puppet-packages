4680529d3f7c7888fc4cbc5f9d9904d3952c285c
ppp0 <philipp.gysin@cargomedia.ch>
2014-07-10 13:12:01 +0200

    Adapt tests to consider stderr and accomodate weird 3rd party daemons

--- modules/copperegg-revealcloud/spec/default/spec.rb
+++ modules/copperegg-revealcloud/spec/default/spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe command('/usr/local/revealcloud/revealcloud -V') do
-  it(:stdout) { should match /v3\.3-9-g06271da/ }
+  it { should return_exit_status 0 }
+  it(:stderr) { should match /v3\.3-9-g06271da/ }
 end
 
