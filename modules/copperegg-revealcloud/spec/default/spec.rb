bb6e96a03d23261206fa4903037630ba477c5616
ppp0 <philipp.gysin@cargomedia.ch>
2014-07-10 13:30:32 +0200

    Correct and adapt tests

--- modules/copperegg-revealcloud/spec/default/spec.rb
+++ modules/copperegg-revealcloud/spec/default/spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe command('/usr/local/revealcloud/revealcloud -V') do
   it { should return_exit_status 0 }
-  it(:stderr) { should match /v3\.3-9-g06271da/ }
+  its(:stderr) { should match /v3\.3-9-g06271da/ }
 end
 
+describe service('revealcloud') do
+  it { should be_enabled }
+  it { should be_running }
+end
