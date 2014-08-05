b18d3c9995daf3d5364c5448fd425ba924d78a7f
ppp0 <philipp.gysin@cargomedia.ch>
2014-08-05 12:18:49 +0200

    Adapt tests

--- modules/puppet/spec/master_puppetfile/spec.rb
+++ modules/puppet/spec/master_puppetfile/spec.rb
@@ -4,9 +4,13 @@ describe file('/etc/puppet/modules/mysql/Modulefile') do
   its(:content) { should match /name 'puppetlabs-mysql'/ }
 end
 
+describe file('/foobar') do
+  it { should be_directory }
+end
+
 describe file('/usr/local/bin/sync_hiera.sh') do
   it { should be_executable }
-  its(:content) { should match /foobar+.*\/etc\/puppet\/hiera\/data\/+.*/ }
+  its(:content) { should match /foobar+.*\/etc\/puppet\/data\/+.*/ }
 end
 
 describe cron do
