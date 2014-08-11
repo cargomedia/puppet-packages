ddf860f51c6bccba952896b645cdfcba7629e805
ppp0 <philipp.gysin@cargomedia.ch>
2014-08-11 13:24:53 +0200

    refactor deb-multimedia

--- modules/deb_multimedia/manifests/init.pp
+++ modules/deb_multimedia/manifests/init.pp
@@ -1,11 +1,11 @@
-class deb-multimedia {
+class deb_multimedia {
 
   apt::source {'deb-multimedia':
     entries => [ "deb http://www.deb-multimedia.org ${::lsbdistcodename} main non-free" ],
     keys => {
       'debian-multimedia' => {
         key => '1F41B907',
-        key_content => template('deb-multimedia/debian-multimedia-keyring.gpg'),
+        key_content => template('deb_multimedia/debian-multimedia-keyring.gpg'),
       }
     }
   }
