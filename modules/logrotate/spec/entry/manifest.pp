4789078eea1e4b58a0b05d60f28c786f1ad539b0
ppp0 <philipp.gysin@cargomedia.ch>
2014-09-29 16:06:18 +0200

    Made tests more understandable - introduce test for log rotation and compression (not only existence and content of conf)

--- modules/logrotate/spec/entry/manifest.pp
+++ modules/logrotate/spec/entry/manifest.pp
@@ -1,40 +1,29 @@
+$logrotate_conf = '
+/var/log/foo_bar.log
+/var/log/foo/*.log {
+  create
+  rotate 7
+  daily
+  missingok
+  delaycompress
+  compress
+  postrotate
+    echo "*" >> /tmp/test
+  endscript
+}'
+
 node default {
 
-  user {'bar':}
-
-  file {['/var/log/bar', '/var/log/foo']:
+  file {'/var/log/foo':
     ensure => directory,
   }
-  ->
 
-  file {['/var/log/foo_bar.log', '/var/log/foo/foo.log', '/var/log/bar_foo.log', '/var/log/bar/bar.log']:
+  file {'/var/log/foo/bar.log':
     ensure => file,
   }
   ->
 
   logrotate::entry{'foo':
-    file_paths => ['/var/log/foo/*.log', '/var/log/foo_bar.log'],
-    commands => [
-      'create 640',
-      'rotate 14',
-      'compress',
-      'daily',
-      'delaycompress',
-      'notifyempty'
-    ]
-  }
-
-  logrotate::entry{'bar':
-    file_paths => ['/var/log/bar/*.log', '/var/log/bar_foo.log'],
-    commands => [
-      'create 640 bar bar',
-      'rotate 4',
-      'compress',
-      'monthly',
-    ]
-  }
-
-  logrotate::entry{'baz':
-    content => template('logrotate/spec/baz'),
+    content => $logrotate_conf,
   }
 }
--- modules/logrotate/spec/entry/spec.rb
+++ modules/logrotate/spec/entry/spec.rb
@@ -1,54 +1,45 @@
 require 'spec_helper'
 
-tests = [
-    {:title => 'foo',
-     :file_paths => ['/var/log/foo/*.log', '/var/log/foo_bar.log'],
-     :commands => [
-         'create 640',
-         'rotate 14',
-         'compress',
-         'daily',
-         'delaycompress',
-         'notifyempty'
-     ],
-    },
-    {:title => 'bar',
-     :file_paths => ['/var/log/bar/*.log', '/var/log/bar_foo.log'],
-     :commands => [
-         'create 640 bar bar',
-         'rotate 4',
-         'compress',
-         'monthly',
-     ],
-    },
-    {:title => 'baz',
-     :file_paths => ['/var/log/messages', '/var/log/syslog'],
-     :commands => [],
-    }
-]
+spec = {file_paths: ['/var/log/foo/*.log', '/var/log/foo_bar.log'],
+        commands: [
+            'rotate 7',
+            'compress',
+            'daily',
+            'create',
+            'delaycompress',
+            'missingok']}
 
-tests.each do |test|
-  path_to_file = '/etc/logrotate.d/' + test[:title]
+logrotate_file = '/etc/logrotate.d/foo'
 
-  describe file(path_to_file) do
-    it { should be_file }
+
+describe file(logrotate_file) do
+  spec[:file_paths].each do |path|
+    it { should contain path }
   end
-
-  test[:file_paths].each do |path|
-    describe file(path_to_file) do
-      it { should contain path }
-    end
+  spec[:commands].each do |c|
+    it { should contain c }
   end
+end
 
-  test[:commands].each do |command|
-    describe file(path_to_file) do
-      it { should contain command }
-    end
-  end
+describe command("logrotate -d " + logrotate_file) do
+  it { should return_exit_status 0 }
+end
 
-  describe command("logrotate -d " + path_to_file) do
+2.times do
+  describe command("logrotate -f " + logrotate_file) do
     it { should return_exit_status 0 }
   end
 end
 
+describe file(logrotate_file) do
+  it { should be_file }
+end
+
+describe file('/var/log/foo/bar.log.2.gz') do
+  it { should be_file }
+end
+
+describe command('cat /tmp/test | wc -l') do
+  its(:stdout) { should == '3'}
+end
 
