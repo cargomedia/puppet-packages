cbbbabcfb34edd01ff9b0228e186a5fe9985899d
Chris Stasiak <me@krzysztof-stasiak.com>
2014-10-06 14:10:47 +0200

    Adjusted monit with adjust_oom()

--- modules/monit/spec/default/spec.rb
+++ modules/monit/spec/default/spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe command('cat /proc/$(cat /var/run/monit.pid)/oom_adj') do
+describe command('cat /proc/$(cat /var/run/monit.pid)/oom_score_adj') do
   it { should return_exit_status 0 }
-  its(:stdout) { should match '-17' }
+  its(:stdout) { should match '-1000' }
 end
 
 describe process('monit') do
