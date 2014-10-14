4e8676e7c2fe76c1f8a4b4948ecb8d85c1c1d18b
ppp0 <philipp.gysin@cargomedia.ch>
2014-10-14 16:00:18 +0200

    Adapt spec testing to validate YAML file

--- modules/bipbip/spec/default/spec.rb
+++ modules/bipbip/spec/default/spec.rb
@@ -20,20 +20,31 @@ describe file('/etc/init.d/bipbip') do
   it { should be_file }
 end
 
-describe file('/etc/bipbip/config.yml') do
-  it { should be_file }
-  its(:content) { should match /include:.*services.d/ }
-end
-
 describe file('/etc/bipbip/services.d') do
   it { should be_directory }
 end
 
-describe file('/etc/bipbip/services.d/memcache.yml') do
-  it { should be_file }
+yaml_files = ['/etc/bipbip/config.yml', '/etc/bipbip/services.d/memcache.yml', '/etc/bipbip/services.d/logparser.yml']
+
+yaml_files.each do |file|
+  describe command("ruby -e \"require 'psych';Psych.load_file('#{file}')\"") do
+    it { should return_exit_status 0 }
+  end
+end
+
+describe file(yaml_files.shift) do
+  its(:content) { should match /include:.*services.d/ }
+end
+
+describe file(yaml_files.shift) do
   its(:content) { should match /plugin:.*memcached/ }
   its(:content) { should match /hostname:.*localhost/ }
   its(:content) { should match /port:.*6379/ }
+end
+
+describe file(yaml_files.shift) do
+  its(:content) { should match /plugin:.*log-parser/ }
+  its(:content) { should match /name:.*oom_killer/ }
 end
 
 describe command('/etc/init.d/bipbip status') do
