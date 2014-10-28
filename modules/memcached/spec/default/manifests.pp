112d3cc123ef1b596f2107a657c607a5fe4159fd
ppp0 <philipp.gysin@cargomedia.ch>
2014-10-28 19:35:30 +0100

    Make tests run, add bipbip test

--- modules/memcached/spec/default/manifests.pp
+++ modules/memcached/spec/default/manifests.pp
@@ -2,8 +2,18 @@ node default {
 
   require 'monit'
 
+  class {'bipbip':
+    api_key => 'foo'
+  }
+
   class {'memcached':
     max_connections => 99,
-    log_verbosity => 1024
+    log_verbosity => -1024
+  }
+  ->
+
+  exec {'empty log and restart':
+    command => 'rm -f /var/log/memcached.log && /etc/init.d/memcached restart',
+    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
 }
--- modules/memcached/spec/default/spec.rb
+++ modules/memcached/spec/default/spec.rb
@@ -4,16 +4,16 @@ describe port(11211) do
   it { should be_listening }
 end
 
-describe command('monit summary') do
-  its(:stdout) { should match /memcached/ }
+['/etc/monit/conf.d/memcached', '/etc/bipbip/services.d/memcached.yml'].each do |file|
+  describe file(file) do
+    it { should be_file }
+  end
 end
 
 describe file('/etc/memcached.conf') do
   it { should contain '-c 99' }
   it { should contain '-vvv' }
 end
-
-describe command('>/var/log/memcached.log && monit restart memcached')
 
 describe command('ruby -e \'require "date";p Date.parse(File.open("/var/log/memcached.log").first)\'') do
   it { should return_exit_status 0 }
