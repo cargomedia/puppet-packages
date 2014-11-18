0a5fe687811c8e330d27d908be96d0cce9d04254
ppp0 <philipp.gysin@cargomedia.ch>
2014-11-18 17:17:33 +0100

    refine build script

--- modules/truecrypt/templates/install.sh
+++ modules/truecrypt/templates/install.sh
@@ -1,8 +1,11 @@
-RESPONSES="1\\nG\\nyes\\n\\n"
+#!/bin/sh
 
-curl http://cznic.dl.sourceforge.net/project/truecrypt/TrueCrypt/Other/TrueCrypt-<%= @version %>-Linux-console-x64.tar.gz > TrueCrypt-<%= @version %>-Linux-console-x64.tar.gz
-unp TrueCrypt-<%= @version %>-Linux-console-x64.tar.gz
-echo $RESPONSES | ./truecrypt-<%= @version %>-setup-console-x64
+curl http://cznic.dl.sourceforge.net/project/truecrypt/TrueCrypt/Other/TrueCrypt-<%= @version %>-source-unix.tar.gz > TrueCrypt-<%= @version %>-source-unix.tar.gz
+tar -xzf TrueCrypt-<%= @version %>-source-unix.tar.gz
+cd truecrypt-<%= @version %>-source
+sleep 5
+#patching according to http://polywogsys.livejournal.com/296576.html (and others)
+sed -E 's/(TC_TOKEN_ERR) (\(CKR_NEW_PIN_MODE\)|\(CKR_NEXT_OTP\))/\/\/ \1 \2/g' Common/SecurityToken.cpp > t && mv t Common/SecurityToken.cpp
 
-
-PKCS11_INC=/usr/include/opencryptoki/ make NOGUI=1 WXSTATIC=1
+export PKCS11_INC=/usr/include/pkcs11-helper-1.0/ make NOGUI=1
+mv Main/truecrypt /usr/local/sbin/
