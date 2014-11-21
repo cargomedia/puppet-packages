e9c80d3d30cfff66397f83a0f3717dd9156f7d01
Reto Kaiser <reto@cargomedia.ch>
2014-11-21 12:09:44 +0100

    Disable opcache for CLI

--- modules/php5/manifests/extension/opcache.pp
+++ modules/php5/manifests/extension/opcache.pp
@@ -2,7 +2,7 @@ class php5::extension::opcache (
   $version = '7.0.3',
   $version_output = '7.0.3FE',
   $enable = true,
-  $enable_cli = true,
+  $enable_cli = false,
   $memory_consumption = 256, # in Mbytes
   $interned_strings_buffer = 8, # in Mbytes
   $max_accelerated_files = 4000,
