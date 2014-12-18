859741a7896eb3d4f19ce8f828ac262ec489e07a
tomaszdurka <tomasz@durka.pl>
2014-12-18 11:37:50 +0100

    Fix npm installation (follow links)

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -10,7 +10,7 @@ class nodejs(
   }
 
   exec {'install npm':
-    command => "curl https://www.npmjs.org/install.sh | clean=yes npm_install=${npm_version} sh",
+    command => "curl -L https://www.npmjs.org/install.sh | clean=yes npm_install=${npm_version} sh",
     unless => 'test -x /usr/bin/npm && /usr/bin/npm -v',
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     require => Package['nodejs-legacy'],
