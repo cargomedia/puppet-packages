ab70756247a913fc12dc1531fe78e62daad30833
Reto Kaiser <reto@cargomedia.ch>
2015-01-09 20:02:41 +0100

    Add tests with multiple mysql users

---
+++ modules/mysql/spec/user-multiple/spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe command('mysql -u root --execute "select User from mysql.user;"') do
+  its(:stdout) { should match 'foo' }
+  its(:stdout) { should match 'bar' }
+end
