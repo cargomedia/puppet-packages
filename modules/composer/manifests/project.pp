2d74c97d31eb4933e0fceceb9d497b4da9c2d5e6
tomaszdurka <tomasz@durka.pl>
2015-01-16 10:32:33 +0100

    Add composer project

---
+++ modules/composer/manifests/project.pp
@@ -0,0 +1,29 @@
+define composer::project (
+  $source = $name,
+  $target = "/usr/local/composer/${source}",
+  $user = 'root',
+  $home = '/root',
+) {
+
+  require 'composer'
+
+  exec {"install ${name}":
+    command => "composer --no-interaction create-project composer/satis --stability=dev --keep-vcs ${target}",
+    creates => $target,
+    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    user => $user,
+    environment => ["HOME=${home}"],
+    require => Class['composer'],
+  }
+  ->
+
+  exec {'upgrade satis':
+    command => "git fetch && git checkout ${version} && composer --no-interaction --no-dev install",
+    cwd => $target,
+    unless => "test $(git rev-parse HEAD) = $(git rev-parse ${version})",
+    path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    user => $user,
+    environment => ["HOME=${home}"],
+    require => Class['composer'],
+  }
+}
