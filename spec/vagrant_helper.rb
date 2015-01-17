43ab34a43376569e1b57083c65d11f0487ddf58f
Reto Kaiser <reto@cargomedia.ch>
2015-01-17 20:00:29 +0100

    Always pass box identifier to "vagrant"

--- spec/vagrant_helper.rb
+++ spec/vagrant_helper.rb
@@ -22,7 +22,7 @@ class VagrantHelper
       execute_local("vagrant destroy -f #{@box}")
       execute_local("vagrant up --no-provision #{@box}", {'DISABLE_PROXY' => 'true'})
       execute_local("vagrant provision #{@box}", {'DISABLE_PROXY' => 'true'})
-      execute_local('vagrant provision')
+      execute_local("vagrant provision #{@box}")
       execute_local("vagrant snapshot take #{@box} default")
     end
     unless is_running
