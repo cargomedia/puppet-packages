e97c472c9ec007f3e293b851837cf1d4be2d1767
tomaszdurka <tomasz@durka.pl>
2015-01-18 21:07:39 +0100

    Formatting

--- modules/satis/manifests/init.pp
+++ modules/satis/manifests/init.pp
@@ -5,39 +5,39 @@ class satis($hostname) {
 
   $version = 'b20fd944ec40ad65c1e54bb0860fe844f4efd56e' # 1.0.0-alpha1
 
-  file {'/etc/satis':
+  file { '/etc/satis':
     ensure => 'directory',
-    owner => '0',
-    group => '0',
-    mode => '0755',
+    owner  => '0',
+    group  => '0',
+    mode   => '0755',
   }
 
-  user {'satis':
-    ensure => present,
-    system => true,
+  user { 'satis':
+    ensure     => present,
+    system     => true,
     managehome => true,
-    home => '/var/lib/satis',
+    home       => '/var/lib/satis',
   }
   ->
 
-  composer::project {'composer/satis':
-    target => '/var/lib/satis/satis',
-    version => $version,
+  composer::project { 'composer/satis':
+    target    => '/var/lib/satis/satis',
+    version   => $version,
     stability => 'dev',
-    user => 'satis',
-    home => '/var/lib/satis'
+    user      => 'satis',
+    home      => '/var/lib/satis'
   }
   ->
 
-  file {'/var/lib/satis/public':
+  file { '/var/lib/satis/public':
     ensure => 'directory',
-    owner => 'satis',
-    group => 'satis',
-    mode => '0755',
+    owner  => 'satis',
+    group  => 'satis',
+    mode   => '0755',
   }
   ->
 
-  apache2::vhost{$hostname:
+  apache2::vhost{ $hostname:
     content => template("${module_name}/vhost"),
   }
 }
