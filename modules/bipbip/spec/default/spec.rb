9bedbe07e73b1c833eb919a2a0ed55a5b99ddd20
Chris Stasiak <me@krzysztof-stasiak.com>
2015-01-19 10:34:23 +0100

    Updated test

--- modules/bipbip/spec/default/spec.rb
+++ modules/bipbip/spec/default/spec.rb
@@ -24,6 +24,10 @@ describe file('/etc/bipbip/services.d') do
   it { should be_directory }
 end
 
+describe file('/home/bipbip') do
+  it { should be_directory }
+end
+
 yaml_files = ['/etc/bipbip/config.yml', '/etc/bipbip/services.d/memcache.yml', '/etc/bipbip/services.d/logparser.yml']
 
 yaml_files.each do |file|
