c4d68cbf4a6e9affcab55dce09d8882c8e26c0f5
ppp0 <philipp.gysin@cargomedia.ch>
2015-01-27 17:40:00 +0100

    de-linting

--- modules/helper/spec/service/manifest.pp
+++ modules/helper/spec/service/manifest.pp
@@ -2,7 +2,7 @@ node default {
 
   file{'/tmp/bar':
     ensure => file,
-    content => template("helper/spec/service/bar"),
+    content => template('helper/spec/service/bar'),
     owner => '0',
     group => '0',
     mode => '0755',
@@ -10,6 +10,6 @@ node default {
   ->
 
   helper::service{'foo':
-    init_file_content => template("helper/spec/service/foo"),
+    init_file_content => template('helper/spec/service/foo'),
   }
 }
