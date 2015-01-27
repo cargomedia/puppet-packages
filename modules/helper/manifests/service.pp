50be350d5aa2dbf9753670840d7d68fa0ec63fb4
ppp0 <philipp.gysin@cargomedia.ch>
2015-01-27 12:18:15 +0100

    refactor helper::service to install init script in a self-contained fashion

--- modules/helper/manifests/service.pp
+++ modules/helper/manifests/service.pp
@@ -1,8 +1,19 @@
-define helper::service {
+define helper::service(
+  $init_file_content,
+) {
+
+  file {"/etc/init.d/${name}":
+    ensure => file,
+    content => $init_file_content,
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+  ~>
 
   exec {"update-rc.d ${name} defaults":
     path => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    unless => "ls /etc/rc3.d/ | grep -- '^S..${name}$'",
+    refreshonly => true,
   }
   ~>
 
