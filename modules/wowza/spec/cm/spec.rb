7dd3f426d487eef66f7aeee9d2687d2cfbb21fee
ppp0 <philipp.gysin@cargomedia.ch>
2015-01-27 12:58:36 +0100

    Adapt spec test for wowza service

--- modules/wowza/spec/cm/spec.rb
+++ modules/wowza/spec/cm/spec.rb
@@ -4,6 +4,11 @@ describe package('wowzastreamingengine-4.0.3') do
   it { should be_installed }
 end
 
+describe service('wowza') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
 describe file('/usr/local/WowzaStreamingEngine/lib/json-simple.jar') do
   it { should be_file }
   it { should be_linked_to '/usr/local/WowzaStreamingEngine/lib/lib-versions/json-simple-1.1.1.jar' }
@@ -31,13 +36,13 @@ describe command('sleep 10') do
   it { should return_exit_status 0 }
 end
 
-[1935,8083,8086].each do |port|
+[1935, 8083, 8086].each do |port|
   describe port(port) do
     it { should be_listening }
   end
 end
 
-[8084,8085].each do |port|
+[8084, 8085].each do |port|
   describe port(port) do
     it { should_not be_listening }
   end
