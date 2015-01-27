eeea1da10fb0ba44bc9ce81452bbc541ab2d9b46
ppp0 <philipp.gysin@cargomedia.ch>
2015-01-27 12:18:26 +0100

    Add tests

---
+++ modules/helper/spec/service/manifest.pp
@@ -0,0 +1,15 @@
+node default {
+
+  file{'/tmp/bar':
+    ensure => file,
+    content => template("helper/spec/service/bar"),
+    owner => '0',
+    group => '0',
+    mode => '0755',
+  }
+  ->
+
+  helper::service{'foo':
+    init_file_content => template("helper/spec/service/foo"),
+  }
+}
