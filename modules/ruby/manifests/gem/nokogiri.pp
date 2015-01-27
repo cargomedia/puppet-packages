f8c2f12ad0df7a91b1fae07fabb3c07ce34fa8d2
Chris Stasiak <me@krzysztof-stasiak.com>
2015-01-27 18:27:38 +0100

    Updated usage of zlib1g at nokogiri

--- modules/ruby/manifests/gem/nokogiri.pp
+++ modules/ruby/manifests/gem/nokogiri.pp
@@ -1,13 +1,14 @@
 class ruby::gem::nokogiri ($version = 'present') {
 
   require 'build'
+  require 'build::dev::zlib1g'
 
-  package {['libxslt1-dev', 'libxml2-dev', 'zlib1g-dev']:
+  package { ['libxslt1-dev', 'libxml2-dev']:
     ensure => present
   }
 
-  ruby::gem {'nokogiri':
-    ensure => $version,
+  ruby::gem { 'nokogiri':
+    ensure  => $version,
     require => Package['libxslt1-dev', 'libxml2-dev'],
   }
 }
