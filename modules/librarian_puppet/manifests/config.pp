5740b3c9952524a60a65919704ca6a4b33b44e75
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-26 16:24:26 +0100

    Add librarian_puppet::config + specs

---
+++ modules/librarian_puppet/manifests/config.pp
@@ -0,0 +1,28 @@
+define librarian_puppet::config (
+  $value,
+  $path = undef,
+  $user = 'root',
+  $environment = ['HOME=/root']
+) {
+
+  $command_prefix = "librarian-puppet config ${name}"
+  $unless_sufix = "${command_prefix} | grep ${name}"
+
+  if ($path == undef) {
+    $command = "${command_prefix} ${value} --global"
+    $unless = $unless_sufix
+  } else {
+    $command = "cd ${path} && ${command_prefix} ${value} --local"
+    $unless = "cd ${path} && ${unless_sufix}"
+  }
+
+  exec { "set config ${name} to ${value}":
+    command     => $command,
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    provider    => shell,
+    user        => $user,
+    environment => $environment,
+    unless      => $unless
+  }
+
+}
---
+++ modules/librarian_puppet/spec/config/manifest.pp
@@ -0,0 +1,24 @@
+node default {
+
+  require 'librarian_puppet'
+
+  librarian_puppet::config { 'master-local':
+    path => '/tmp',
+    value => '3'
+  }
+
+  librarian_puppet::config { 'master-global':
+    value => false
+  }
+
+  librarian_puppet::config { 'slave-local':
+    path => '/tmp',
+    value => 'true'
+  }
+
+  librarian_puppet::config { 'slave-global':
+    value => 88
+  }
+
+}
+
