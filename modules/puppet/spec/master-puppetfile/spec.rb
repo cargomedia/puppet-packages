e8aeacec7f254742d7f76ad221a633658cf801c5
Chris Stasiak <me@krzysztof-stasiak.com>
2015-02-26 13:10:13 +0100

    Added spec

--- modules/puppet/spec/master-puppetfile/spec.rb
+++ modules/puppet/spec/master-puppetfile/spec.rb
@@ -15,6 +15,10 @@ describe 'puppet::master puppetfile' do
     its(:content) { should match /foobar+.*\/etc\/puppet\/data\/+.*/ }
   end
 
+  describe file('/etc/puppet/.librarian/puppet/config') do
+    its(:content) { should match /LIBRARIAN_PUPPET_RSYNC: 'true'/ }
+  end
+
   describe cron do
     it { should have_entry '* * * * * cd /etc/puppet && librarian-puppet update && /usr/local/bin/sync_hiera.sh' }
   end
