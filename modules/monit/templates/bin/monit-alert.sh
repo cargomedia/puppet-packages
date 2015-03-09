a6652fc36cd519e9dbf4a75703e4b1a422429226
ppp0 <philipp.gysin@cargomedia.ch>
2015-03-09 14:59:39 +0100

    refactored function and its call, introduce argument timeout in seconds

--- modules/monit/templates/bin/monit-alert.sh
+++ modules/monit/templates/bin/monit-alert.sh
@@ -17,16 +17,16 @@ ln -s ${SOURCE} ${DEST}
 function monitGetStateFileAccess { stat -c "%Y" "/root/.monit.state"; }
 export -f monitGetStateFileAccess
 export MONIT_STATE_FILE_ACCESS=$(monitGetStateFileAccess)
-function monitHasStateChanged { test "$(monitGetStateFileAccess)" != "${MONIT_STATE_FILE_ACCESS}"; }
-export -f monitHasStateChanged
+function waitForMonitStateChange { timeout --signal=9 $1 bash -c 'while ! (test "$(monitGetStateFileAccess)" != "${MONIT_STATE_FILE_ACCESS}"); do sleep 0.05; done'; }
+export -f waitForMonitStateChange
 
 monit reload
 
-if ! (timeout --signal=9 5 bash -c "while ! (monitHasStateChanged); do sleep 0.05; done"); then
+if ! (waitForMonitStateChange 5); then
   # Hard-restart monit in case it cannot reload
   /etc/init.d/monit restart
 
-  if ! (timeout --signal=9 10 bash -c "while ! (monitHasStateChanged); do sleep 0.05; done"); then
+  if ! (waitForMonitStateChange 10); then
     exit 1
   fi
 fi
