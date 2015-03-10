51c7a486844650e5901369251ab2843da845dac7
Chris Stasiak <me@krzysztof-stasiak.com>
2015-03-10 16:39:21 +0100

    Refresh apt cache if older then 5 days by default

--- modules/apt/manifests/update.pp
+++ modules/apt/manifests/update.pp
@@ -1,16 +1,19 @@
 class apt::update(
-  $options = []
+  $options = [],
+  $max_cache_age = 5,
 ) {
 
   require 'apt'
 
   $arguments = parse_apt_opts($options)
 
+  $refreshonly = (apt_cache_age() < $max_cache_age)
+
   exec { 'apt_update':
     path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     command     => "apt-get ${arguments} update",
     logoutput   => 'on_failure',
-    refreshonly => true,
+    refreshonly => $refreshonly,
   }
 
   Exec['apt_update'] -> Package <| |>
