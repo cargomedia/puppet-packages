523a0f2cdcef3fc1dd855dfa543112e29cde872c
Reto Kaiser <reto@cargomedia.ch>
2015-04-30 11:09:02 +0200

    Extract puppet helper function for "apply"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,10 @@ RSpec.configure do |configuration|
     Specinfra.configuration.ssh_options = vagrant_box.ssh_options
     Specinfra.configuration.ssh = vagrant_box.ssh_start
 
-    puppet_spec = PuppetSpec.new(vagrant_box, self)
+    puppet_spec = PuppetSpec.new(vagrant_box, self, debug)
 
     puppet_spec.apply_facts
     puppet_spec.configure_hiera
-    puppet_spec.apply_manifests(debug)
+    puppet_spec.apply_manifests
   end
 end
