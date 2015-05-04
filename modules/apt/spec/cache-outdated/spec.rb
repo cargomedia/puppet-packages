2cc0b8b331cea95a6dd6838ae7a5148a2cf8fea3
Reto Kaiser <reto@cargomedia.ch>
2015-05-04 12:58:23 +0200

    Make sure we're testing a "max_cache_age" below the one in "before_spec"

--- modules/apt/spec/cache-outdated/spec.rb
+++ modules/apt/spec/cache-outdated/spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'apt::update' do
 
   describe command('stat -c "%Y" /var/lib/apt/periodic/update-success-stamp') do
-    its(:stdout) { should_not match /12345/ }
+    let(:update_stamp) { subject.stdout.to_i }
+
+    it 'should update indices' do
+      expect(update_stamp).to be_within(10).of(Time.now.to_i)
+    end
   end
 
 end
