7d87b145207c8359ccbad216b057565f92bfb9d8
ppp0 <philipp.gysin@cargomedia.ch>
2015-06-25 11:59:56 +0200

    Add forcing existing config file at installation

--- modules/mms/templates/install.sh
+++ modules/mms/templates/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 curl -Ls https://cloud.mongodb.com/download/agent/monitoring/mongodb-<%= @agent_name %>-agent_<%= @version %>-1_amd64.deb > <%= @agent_name %>.deb
-sudo dpkg -i <%= @agent_name %>.deb
+sudo dpkg -i --force-confold <%= @agent_name %>.deb
