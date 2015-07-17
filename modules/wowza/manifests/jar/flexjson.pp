fdf6da9886ed84f634477d710d271e2a0b9641eb
Reto Kaiser <reto@cargomedia.ch>
2015-07-17 10:37:29 +0200

    Change download url for flexjson.jar, because sourceforge is down

--- modules/wowza/manifests/jar/flexjson.pp
+++ modules/wowza/manifests/jar/flexjson.pp
@@ -3,7 +3,7 @@ class wowza::jar::flexjson($version = '2.1') {
   require 'wowza'
 
   wowza::jar { 'flexjson':
-    get_command => "curl -sL http://downloads.sourceforge.net/project/flexjson/flexjson/flexjson%20${version}/flexjson-${version}.tar.gz | tar -xzvO flexjson-${version}/flexjson-${version}.jar",
+    get_command => "curl -sL http://central.maven.org/maven2/net/sf/flexjson/flexjson/${version}/flexjson-${version}.jar",
     version     => $version,
   }
 }
