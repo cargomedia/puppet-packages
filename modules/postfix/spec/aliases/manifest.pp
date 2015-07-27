357a450dc97644b187bc85b18cb55b1e16a32c24
ppp0 <philipp.gysin@cargomedia.ch>
2015-07-27 18:38:01 +0200

    De-linting

--- modules/postfix/spec/aliases/manifest.pp
+++ modules/postfix/spec/aliases/manifest.pp
@@ -11,7 +11,7 @@ node default {
     },
   }
 
-  exec { "send mail to user foo":
+  exec { 'send mail to user foo':
     provider    => shell,
     command     => 'echo "test" | mail -s "Test" foo',
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
