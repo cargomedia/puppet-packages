4a4d16709e35f97c5cf1a6aa8209e261d512ae3f
ppp0 <philipp.gysin@cargomedia.ch>
2015-07-27 18:33:02 +0200

    Change template fro header_checks, repeat the tests as a relay config

---
+++ modules/postfix/spec/relay/manifest.pp
@@ -0,0 +1,34 @@
+node default {
+
+  class { 'postfix':
+    transports => [
+      {
+        'protocol' => 'smtp',
+        'host' => '127.0.1.1',
+        'port' => '2525',
+        'credentials' => 'foo:bar',
+      }
+    ],
+  }
+
+  exec { 'smtp-sink instance':
+    provider    => shell,
+    command     => '(sudo smtp-sink -vu postfix 127.0.1.1:2525 10 2>&1 | tee /tmp/2525)&',
+    path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    require     => Class ['postfix'],
+  }
+
+  exec { 'send test mail #1':
+    provider    => shell,
+    command     => 'echo "test" | mail -s "test" filter_2525@example.com',
+    path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    require     => Exec ['smtp-sink instance'],
+  }
+
+  exec { 'send test mail #2':
+    provider    => shell,
+      command     => 'echo "test" | mail -s "test" filter_2828@example.com',
+    path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    require     => Exec ['smtp-sink instance'],
+  }
+}
---
+++ modules/postfix/spec/relay/spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'base64'
+
+describe 'postfix' do
+
+  describe package('postfix') do
+    it { should be_installed }
+  end
+
+  describe service('postfix') do
+    it { should be_running }
+  end
+
+  describe file('/etc/postfix/header_checks') do
+    it 'is a file' do
+      expect(subject).to be_file
+    end
+    it 'contains a catch-all // regexp' do
+      expect(subject).to contain('//').before(/FILTER/)
+    end
+  end
+
+  credentials1 = Base64.strict_encode64("\0foo\0bar")
+
+  describe file('/tmp/2525') do
+    it 'is a file' do
+      expect(subject).to be_file
+    end
+    it 'contains credentials foo:bar' do
+      expect(subject).to contain(credentials1).after(/AUTH PLAIN/)
+    end
+    it 'is contains test mail #1' do
+      expect(subject).to contain('<filter_2525@example.com>').after(/RCPT TO/)
+    end
+    it 'is contains test mail #2' do
+      expect(subject).to contain('<filter_2828@example.com>').after(/RCPT TO/)
+    end
+  end
+end
