23cb49439f8c44dfd497f3c57b845c3170e7c3c8
ppp0 <philipp.gysin@cargomedia.ch>
2015-08-10 18:11:17 +0200

    wait for bipbip having started up

--- modules/mysql/spec/server-with-bipbip/spec.rb
+++ modules/mysql/spec/server-with-bipbip/spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe 'mysql::server' do
 
+
+  # Wait for bipbip to start up
+  describe command('timeout --signal=9 30 bash -c "while ! (/etc/init.d/bipbip status); do sleep 0.5; done"') do
+    its(:exit_status) { should eq 0 }
+  end
+
   describe service('bipbip') do
     it { should be_running }
   end
