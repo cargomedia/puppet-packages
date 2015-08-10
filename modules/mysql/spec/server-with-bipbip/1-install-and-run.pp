892304489cbedd30a2f2c986e97dfb318e81b508
ppp0 <philipp.gysin@cargomedia.ch>
2015-08-10 15:08:57 +0200

    Set up tests for chmod and bipbip

---
+++ modules/mysql/spec/server-with-bipbip/1-install-and-run.pp
@@ -0,0 +1,14 @@
+node default {
+
+  require 'bipbip'
+
+  class { 'mysql::server':
+  }
+
+  exec { 'chmod error log without restarting mysql-server':
+    provider    => shell,
+    command     => 'chmod 600 /var/log/mysql.err',
+    path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    require     => Class['mysql::server'],
+  }
+}
