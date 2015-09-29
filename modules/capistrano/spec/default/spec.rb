2d6416dff634d0b2ac8ef59a42c0f20ea8358136
ppp0 <philipp.gysin@cargomedia.ch>
2015-09-29 13:09:26 +0200

    Add test to check for complete install

--- modules/capistrano/spec/default/spec.rb
+++ modules/capistrano/spec/default/spec.rb
@@ -5,4 +5,9 @@ describe 'capistrano' do
   describe command('gem list') do
     its(:stdout) { should match 'capistrano' }
   end
+
+  describe command('cap --version') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match 'Capistrano' }
+  end
 end
