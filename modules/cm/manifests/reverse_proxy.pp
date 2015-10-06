cda9b7686dc43922efbf7ee2fbf5b89646189f55
Chris Stasiak <me@krzysztof-stasiak.com>
2015-10-06 14:10:56 +0100

    Fixed reverse-proxy-upstream define name

--- modules/cm/manifests/reverse_proxy.pp
+++ modules/cm/manifests/reverse_proxy.pp
@@ -3,15 +3,14 @@ define cm::reverse_proxy(
   $ssl_cert = undef,
   $ssl_key = undef,
   $aliases = [],
-  $redirects = undef,
-  $debug = false
+  $redirects = undef
 ) {
 
   include 'nginx'
 
   $upstream_members = suffix($members, ' max_fails=999 fail_timeout=1')
 
-  nginx::resource::upstream { 'reverse-proxy-backend':
+  nginx::resource::upstream { "reverse-proxy-backend-${name}":
     ensure              => present,
     members             => $upstream_members,
     upstream_cfg_append => [
@@ -20,7 +19,6 @@ define cm::reverse_proxy(
   }
 
   $hostnames = concat([$name], $aliases)
-  $debug_int = $debug ? { true => 1, false => 0 }
   $ssl = ($ssl_cert != undef) or ($ssl_key != undef)
   $port = $ssl ? { true => 443, false => 80 }
 
@@ -54,7 +52,7 @@ define cm::reverse_proxy(
     location_cfg_append => [
       "proxy_set_header Host '${name}';",
       'proxy_set_header X-Real-IP $remote_addr;',
-      'proxy_pass https://reverse-proxy-backend;',
+      "proxy_pass https://reverse-proxy-backend-${name};",
       'proxy_next_upstream error timeout http_502;'
     ],
   }
