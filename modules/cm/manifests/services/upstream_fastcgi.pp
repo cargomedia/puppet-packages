0529a30611a4eb10b2183aa490f84b99e93b6698
Chris Stasiak <me@krzysztof-stasiak.com>
2015-10-06 14:55:42 +0100

    Refactoring

---
+++ modules/cm/manifests/services/upstream_fastcgi.pp
@@ -0,0 +1,17 @@
+class cm::services::upstream_fastcgi(
+  $members = ['localhost:9000']
+) {
+
+  include 'cm::services::webserver'
+
+  $upstream_members = suffix($members, ' max_fails=3 fail_timeout=3')
+
+  nginx::resource::upstream { $name:
+    ensure              => present,
+    members             => $upstream_members,
+    upstream_cfg_append => [
+      'keepalive 400;',
+    ],
+  }
+
+}
