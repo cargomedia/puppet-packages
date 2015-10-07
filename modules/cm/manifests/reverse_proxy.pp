b852863253aaf9f2303843ccfabfd0eb9527662b
Chris Stasiak <me@krzysztof-stasiak.com>
2015-10-07 11:36:57 +0100

    Create upstream for vhost if not name not passed

--- modules/cm/manifests/reverse_proxy.pp
+++ modules/cm/manifests/reverse_proxy.pp
@@ -3,10 +3,17 @@ define cm::reverse_proxy(
   $ssl_key = undef,
   $aliases = [],
   $redirects = undef,
-  $upstream_name = 'reverse-proxy-backend'
+  $upstream_name = undef
 ) {
 
   include 'cm::services::webserver'
+
+  $upstream = $upstream_name ? { default => $upstream_name, undef => 'reverse-proxy-backend' }
+
+  if ($upstream_name == undef and defined(Cm::Upstream::Fastcgi['reverse-proxy-backend']) == false) {
+    cm::upstream::proxy { $upstream:
+    }
+  }
 
   $hostnames = concat([$name], $aliases)
   $ssl = ($ssl_cert != undef) or ($ssl_key != undef)
@@ -42,7 +49,7 @@ define cm::reverse_proxy(
     location_cfg_append => [
       "proxy_set_header Host '${name}';",
       'proxy_set_header X-Real-IP $remote_addr;',
-      "proxy_pass https://${$upstream_name};",
+      "proxy_pass https://${$upstream};",
       'proxy_next_upstream error timeout http_502;'
     ],
   }
