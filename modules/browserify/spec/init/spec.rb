427c47cb943c8306070ea93243b6c291f9ea7dfa
Michel Meyer <michel.meyer@cargomedia.ch>
2015-10-23 14:50:20 +0200

    Fix browserify module specification

--- modules/browserify/spec/init/spec.rb
+++ modules/browserify/spec/init/spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe 'browserify' do
 
-  describe package('browserify-js') do
+  describe package('browserify') do
     it { should be_installed.by('npm') }
+  end
+
+  describe command('browserify --version') do
+    its(:exit_status) { should eq 0 }
   end
 end
