15d9ca1b3d193884418d106f2297a9e42363f7e5
Reto Kaiser <reto@cargomedia.ch>
2015-10-23 14:57:33 +0200

    Update mongodb php driver to use "legacy" url

--- modules/php5/templates/extension/mongo/install.sh
+++ modules/php5/templates/extension/mongo/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-curl -sL https://github.com/mongodb/mongo-php-driver/archive/<%= @version %>.tar.gz | tar -xzf -
-cd mongo-php-driver-<%= @version %>/
+curl -sL https://github.com/mongodb/mongo-php-driver-legacy/archive/<%= @version %>.tar.gz | tar -xzf -
+cd mongo-php-driver-legacy-<%= @version %>/
 phpize
 ./configure
 make
