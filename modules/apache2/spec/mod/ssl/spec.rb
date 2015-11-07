b66b6d0a3f296ed5ed511a67b8b72817dc99c39c
tomaszdurka <tomasz@durka.pl>
2015-11-07 01:18:53 +0100

    Intentional change on module to trigger successful CI

--- modules/apache2/spec/mod/ssl/spec.rb
+++ modules/apache2/spec/mod/ssl/spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'apache::mod::ssl' do
 
-  describe command('apachectls -M') do
-    its(:stdout) { should match 'ssl_module' }
+  describe command('apachectl -M') do
+    its(:stdout) { should match 'ssl_module'  }
   end
 end
