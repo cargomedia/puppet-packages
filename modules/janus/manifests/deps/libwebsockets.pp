33806b55081c7a8cc1900491e789e4f42f67c1d3
ppp0 <philipp.gysin@cargomedia.ch>
2015-11-09 15:46:10 +0100

    Dependency libwebsocket

---
+++ modules/janus/manifests/deps/libwebsockets.pp
@@ -0,0 +1,13 @@
+class janus::deps::libwebsockets(
+  $version = '1.5',
+) {
+
+  require 'git'
+  package { ['cmake', 'pkg-config']: }
+
+  helper::script { 'install libwebsockets':
+    content => template("${module_name}/deps/libwebsockets_install.sh"),
+    unless  => "pkg-config --modversion libwebsockets | grep -qE '^${version}$'",
+    timeout => 900,
+  }
+}
