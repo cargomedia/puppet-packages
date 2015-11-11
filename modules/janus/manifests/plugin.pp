d02b60984922ea73f455e2cdcc4a93081726df12
ppp0 <philipp.gysin@cargomedia.ch>
2015-11-11 17:25:57 +0100

    Simplify plugin install

--- modules/janus/manifests/plugin.pp
+++ modules/janus/manifests/plugin.pp
@@ -1,11 +1,18 @@
 define janus::plugin {
 
+  require 'build::autoconf'
   include 'janus::version'
   include 'janus::service'
+  include 'janus'
 
-  janus::deps::plugin::install { $name:
+  $janus_version = $janus::version::number
+
+  helper::script { "install janus plugin ${name}":
+    content => template("${module_name}/plugin_install.sh"),
+    unless  => "ls /opt/janus/lib/janus/plugins/libjanus_${name}.so",
+    timeout => 900,
+    require => Helper::Script['install janus'],
     notify  => Service['janus'],
-    janus_version => $janus::version::number,
   }
 
 }
