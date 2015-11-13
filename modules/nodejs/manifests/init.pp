e5b17e560a28fa1c17ecc9424dd900e83b14aca8
tomaszdurka <tomasz@durka.pl>
2015-11-13 13:55:50 +0100

    Move apt nodesource into node package

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,6 +1,18 @@
 class nodejs {
 
-  require 'apt::source::nodesource'
+  apt::source { 'nodesource':
+    entries => [
+      "deb https://deb.nodesource.com/node_0.12 ${::lsbdistcodename} main",
+      "deb-src https://deb.nodesource.com/node_0.12 ${::lsbdistcodename} main",
+    ],
+    keys    => {
+      'nodesource' => {
+        key     => '68576280',
+        key_url => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+      }
+    }
+  }
+  ->
 
   package { 'nodejs':
     ensure => present,
