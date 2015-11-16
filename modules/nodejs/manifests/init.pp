f42f092ab95e5249389cb328623a268f61b7f20f
Philipp Pfaff <ppp0@users.noreply.github.com>
2015-11-16 11:31:39 +0100

    Revert "nodejs v0.12"

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,21 +1,21 @@
-class nodejs {
+class nodejs(
+  $npm_version = '1.4.10'
+) {
 
-  apt::source { 'nodesource':
-    entries => [
-      "deb https://deb.nodesource.com/node_0.12 ${::lsbdistcodename} main",
-      "deb-src https://deb.nodesource.com/node_0.12 ${::lsbdistcodename} main",
-    ],
-    keys    => {
-      'nodesource' => {
-        key     => '68576280',
-        key_url => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
-      }
-    }
-  }
-  ->
+  require 'apt::source::backports'
 
   package { 'nodejs':
     ensure => present,
+  }
+  package { 'nodejs-legacy':
+    ensure => present
+  }
+
+  exec { 'install npm':
+    command => "curl -sL https://www.npmjs.com/install.sh | clean=yes npm_install=${npm_version} sh",
+    unless  => 'test -x /usr/bin/npm && /usr/bin/npm -v',
+    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    require => Package['nodejs-legacy'],
   }
 
 }
--- modules/nodejs/spec/init/spec.rb
+++ modules/nodejs/spec/init/spec.rb
@@ -2,13 +2,8 @@ require 'spec_helper'
 
 describe 'nodejs' do
 
-  describe file('/etc/apt/sources.list.d/nodesource.list') do
-    it { should exist }
-  end
-
   describe command('nodejs -v') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /v0\.12\./}
   end
 
   describe command('node -v') do
