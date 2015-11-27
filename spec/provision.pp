f9eda8f8d041830dd79198d795145393606e4805
tomaszdurka <tomasz@durka.pl>
2015-11-27 11:23:28 +0100

    Do not run apt_update as it's already run in puppet::common, polipo

--- spec/provision.pp
+++ spec/provision.pp
@@ -2,14 +2,6 @@ node default {
 
   require 'puppet::common'
 
-  exec { 'apt_update':
-    provider    => shell,
-    path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    command     => 'apt-get update',
-    logoutput   => 'on_failure',
-  }
-  ->
-
   class { 'polipo':
     diskCacheRoot => '/tmp/proxy-cache',
     idleTime      => 0,
