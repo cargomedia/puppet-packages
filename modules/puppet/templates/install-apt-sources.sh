b46c3f24964effd56ea922130ff9a1724c447fe9
ppp0 <philipp.gysin@cargomedia.ch>
2015-12-01 16:48:30 +0100

    Inverse logic

--- modules/puppet/templates/install-apt-sources.sh
+++ modules/puppet/templates/install-apt-sources.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-if (echo 'wheezy trusty precise utopic' | grep -q <%= @lsbdistcodename %>); then
-	PUPPETLABS_REPO='puppetlabs-release'
-else
+if [ <%= @lsbdistcodename %> -eq 'vivid' ]; then
 	# 15.04 is only available as a PC1 (p...-candidate?) dist right now
 	PUPPETLABS_REPO='puppetlabs-release-pc1'
+else
+	PUPPETLABS_REPO='puppetlabs-release'
 fi
 curl -Ls http://apt.puppetlabs.com/$PUPPETLABS_REPO-<%= @lsbdistcodename %>.deb > $PUPPETLABS_REPO-<%= @lsbdistcodename %>.deb
 dpkg -i $PUPPETLABS_REPO-<%= @lsbdistcodename %>.deb
