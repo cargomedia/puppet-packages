f5fbf64d5f5f5e76aa016f9a316c6853ca6cdc3f
ppp0 <philipp.gysin@cargomedia.ch>
2015-12-01 15:36:52 +0100

    Adapt puppetlabs apt sources and default apt sources list

--- modules/puppet/templates/install-apt-sources.sh
+++ modules/puppet/templates/install-apt-sources.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
-curl -Ls http://apt.puppetlabs.com/puppetlabs-release-<%= @lsbdistcodename %>.deb > puppetlabs-release-<%= @lsbdistcodename %>.deb
-dpkg -i puppetlabs-release-<%= @lsbdistcodename %>.deb
+if (echo 'wheezy trusty precise utopic' | grep -q <%= @lsbdistcodename %>); then
+	PUPPETLABS_REPO='puppetlabs-release'
+else
+	# 15.04 is only available as a PC1 (p...-candidate?) dist right now
+	PUPPETLABS_REPO='puppetlabs-release-pc1'
+fi
+curl -Ls http://apt.puppetlabs.com/$PUPPETLABS_REPO-<%= @lsbdistcodename %>.deb > $PUPPETLABS_REPO-<%= @lsbdistcodename %>.deb
+dpkg -i $PUPPETLABS_REPO-<%= @lsbdistcodename %>.deb
 apt-get update
