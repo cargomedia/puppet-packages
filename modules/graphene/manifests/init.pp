299f8d9557dc98cca53435a2898786910e869ed0
ppp0 <philipp.gysin@cargomedia.ch>
2015-12-09 19:01:44 +0100

    Fix graphene

--- modules/graphene/manifests/init.pp
+++ modules/graphene/manifests/init.pp
@@ -1,8 +1,10 @@
-class gstreamer(
+class graphene (
   $version = '1.6.1'
 ) {
 
   require 'apt::source::cargomedia'
 
-  package { 'gstreamer1.0-tools': }
+  package { 'graphene':
+    ensure => $version,
+  }
 }
