324318f986ccb29408c2c525e4b3f9f228fbffa8
Reto Kaiser <reto@cargomedia.ch>
2015-12-12 16:54:49 +0100

    Use a fact "bootstrap" for bootstrapping puppet

--- modules/puppetmaster/manifests/init.pp
+++ modules/puppetmaster/manifests/init.pp
@@ -7,7 +7,6 @@ class puppetmaster (
   $puppetdb_port_ssl = 8081,
   $bootstrap_classes = [
     'puppet::agent',
-    'monit' # See https://github.com/cargomedia/puppet-packages/issues/232
   ],
   $puppetfile = undef,
   $port_webrick = 8140,
