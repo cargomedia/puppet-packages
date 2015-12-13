564a4b2565f718918e0be5340f43e0d6d557f9de
Chris Stasiak <me@krzysztof-stasiak.com>
2015-12-13 13:49:29 +0100

    Fixed entry-real spec

--- modules/monit/spec/entry-real/spec.rb
+++ modules/monit/spec/entry-real/spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'monit::entry real' do
 
-  describe file('/etc/monit/conf.d/puppet') do
+  describe file('/etc/monit/conf.d/filesystem') do
     it { should be_file }
   end
 end
