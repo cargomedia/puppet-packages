e9031e0794c54bbfcaf4a9dd531b97b61873dea9
Chris Stasiak <me@krzysztof-stasiak.com>
2015-12-15 18:33:09 +0100

    Require XPR class by default

--- modules/xpra/manifests/display.pp
+++ modules/xpra/manifests/display.pp
@@ -2,12 +2,11 @@ define xpra::display (
   $extra_params = '--no-pulseaudio --no-mdns'
 ) {
 
-  include 'xpra'
+  require 'xpra'
 
   daemon { "xpra-display-${name}":
     binary           => '/usr/bin/xpra',
-    args             => "start :${name} --no-daemon ${extra_params}",
-    require          => Class['xpra']
+    args             => "start :${name} --no-daemon ${extra_params}"
   }
 
 }
