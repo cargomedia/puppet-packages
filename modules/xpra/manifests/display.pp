ec12be3bdf1bfde09db34fa6219fa3ac3ff082f8
Chris Stasiak <me@krzysztof-stasiak.com>
2015-12-15 18:32:45 +0100

    Made daemonization not configurable

--- modules/xpra/manifests/display.pp
+++ modules/xpra/manifests/display.pp
@@ -1,12 +1,12 @@
 define xpra::display (
-  $extra_params = '--no-daemon --no-pulseaudio --no-mdns'
+  $extra_params = '--no-pulseaudio --no-mdns'
 ) {
 
   include 'xpra'
 
   daemon { "xpra-display-${name}":
     binary           => '/usr/bin/xpra',
-    args             => "start :${name} ${extra_params}",
+    args             => "start :${name} --no-daemon ${extra_params}",
     require          => Class['xpra']
   }
 
