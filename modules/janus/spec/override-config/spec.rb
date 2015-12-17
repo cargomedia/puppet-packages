3c463c4b1b852d35df07f378657ffade2c6697eb
ppp0 <philipp.gysin@cargomedia.ch>
2015-12-17 17:45:50 +0100

    Reenable parsing of log file to refine spec test expectation

--- modules/janus/spec/override-config/spec.rb
+++ modules/janus/spec/override-config/spec.rb
@@ -9,12 +9,12 @@ describe 'janus::service::override-config' do
   describe port (55556) do
     it {should be_listening}
   end
-  #
-  # describe command('cat /var/log/syslog') do
-  #   its(:stdout) { should match /janus+.*Reading configuration from \/tmp\/januxx\.foo\.conf/ }
-  #   its(:stdout) { should match /janus+.*configs_folder: \/tmp/ }
-  #   its(:stdout) { should match /janus+.*Configuration file: \/tmp\/janus\.plugin\.cm\.rtpbroadcast\.cfg/ }
-  #   its(:stdout) { should match /janus+.*Configuration file: \/tmp\/janus\.transport\.websockets\.cfg/ }
-  #   its(:stdout) { should match /janus+.*Configuration file: \/tmp\/janus\.transport\.http\.cfg/ }
-  # end
+
+  describe command('cat /var/log/janus/janus.log') do
+    its(:stdout) { should match /Reading configuration from \/tmp\/januxx\.foo\.conf/ }
+    its(:stdout) { should match /configs_folder: \/tmp/ }
+    its(:stdout) { should match /Configuration file: \/tmp\/janus\.plugin\.cm\.rtpbroadcast\.cfg/ }
+    its(:stdout) { should match /Configuration file: \/tmp\/janus\.transport\.websockets\.cfg/ }
+    its(:stdout) { should match /Configuration file: \/tmp\/janus\.transport\.http\.cfg/ }
+  end
 end
