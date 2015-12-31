ef071d4c04d8a80e67d9e27c7387c62f18905ef2
tomaszdurka <tomasz@durka.pl>
2015-12-31 18:23:34 +0100

    Add job-temp-files config entry

--- modules/cm_janus/manifests/init.pp
+++ modules/cm_janus/manifests/init.pp
@@ -26,20 +26,23 @@ class cm_janus (
     owner   => '0',
     group   => '0',
     mode    => '0755',
+    before  => Daemon['cm-janus'],
     notify  => Service['cm-janus'],
   }
 
   user { 'cm-janus':
     ensure => present,
     system => true,
+    before  => Daemon['cm-janus'],
   }
 
-  file { '/var/log/cm-janus':
+  file { ['/var/log/cm-janus', '/var/lib/cm-janus', '/var/lib/cm-janus/jobs-temp-files']:
     ensure  => directory,
     owner   => 'cm-janus',
     group   => 'cm-janus',
     mode    => '0755',
     require => User['cm-janus'],
+    before  => Daemon['cm-janus'],
   }
 
   logrotate::entry{ $module_name:
@@ -49,6 +52,7 @@ class cm_janus (
   package { 'cm-janus':
     ensure   => latest,
     provider => 'npm',
+    before  => Daemon['cm-janus'],
     notify   => Service['cm-janus'],
   }
 
@@ -56,9 +60,5 @@ class cm_janus (
     binary  => '/usr/bin/node',
     args    => '/usr/bin/cm-janus -c /etc/cm-janus/config.yaml',
     user    => 'cm-janus',
-    require => [
-      File['/var/log/cm-janus'],
-      File['/etc/cm-janus/config.yaml']
-    ]
   }
 }
