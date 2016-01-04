4a6e7fcdb7c16607b2163551230168c420decaec
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-04 10:17:33 +0100

    Added ERROR/WARN check in specs

--- modules/coturn/spec/default/spec.rb
+++ modules/coturn/spec/default/spec.rb
@@ -15,7 +15,7 @@ describe 'coturn' do
     it { should be_listening }
   end
 
-  describe file('/etc/turnserver.conf') do
+  describe file('/etc/coturn/turnserver.conf') do
     its(:content) { should match /^mobility/ }
     its(:content) { should match /^user=admin:admin/ }
     its(:content) { should match /^user=super:0x892bd158de5ce05f7792112eca1be3ca/ }
@@ -28,6 +28,11 @@ describe 'coturn' do
 
   describe command('timeout 2 turnutils_uclient -u super -w super -m 1 -n 1 -M -B -z 1 127.0.0.1') do
     its(:stdout) { should match /1: start_mclient: msz=2, tot_send_msgs=0, tot_recv_msgs=0, tot_send_bytes ~ 0, tot_recv_bytes ~ 0/ }
+  end
+
+  describe file('/var/log/coturn/turnserver.log') do
+    its(:content) { should_not match /WARNING:/ }
+    its(:content) { should_not match /ERROR:/ }
   end
 
 end
