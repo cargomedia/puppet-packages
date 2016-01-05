4f461849b2e7997052b8a37d3815a19b19442948
ppp0 <philipp.gysin@cargomedia.ch>
2016-01-05 18:28:24 +0100

    Fix tests

--- modules/ufw/spec/ports/spec.rb
+++ modules/ufw/spec/ports/spec.rb
@@ -2,13 +2,9 @@ require 'spec_helper'
 
 describe 'ufw::ports' do
 
-  describe command('ufw status numbered') {
-    its(:stdout) { should match /Description: weird server/ }
-    its(:stdout) { should match /21,23:25\/tcp/ }
-    its(:stdout) { should match /10000:15000\/udp/ }
-  }
-
   describe iptables do
-    it { should have_rule('-m multiport --dports 10000:15000').with_table('filter').with_chain('ufw-user-input') }
+    it { should have_rule('-p tcp -m tcp --dport 80 -j DROP').with_table('filter').with_chain('ufw-user-input') }
+    it { should have_rule('-p tcp -m tcp --dport 999 -j ACCEPT').with_table('filter').with_chain('ufw-user-input') }
+    it { should have_rule('-p udp -m udp --dport 999 -j ACCEPT').with_table('filter').with_chain('ufw-user-input') }
   end
 end
