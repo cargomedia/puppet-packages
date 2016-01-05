087501ede0b8036027e3afea8ff4c90db4f7b8bd
tomaszdurka <tomasz@durka.pl>
2016-01-05 15:46:40 +0100

    Refactor mjr2webm script
    - simplify code
    - change defaults to use "copy" codec
    - add stream mapping

--- modules/mjr_convert/templates/mjr2webm.sh
+++ modules/mjr_convert/templates/mjr2webm.sh
@@ -1,157 +1,58 @@
 #!/bin/bash
 set -e
-#
-# Copyright (c) 2015 Me
-# Rev: PA1
-# Convert local Janus .mjr files (audio and video) to an .webm file
-# containing video and 2ch audio (vp8 + opus)
-#
-# dependencies:
-#    janus-pp-rec (Janus postprocessing) must have been compiled and installed
-#    ffmpeg (2015->) must have been installed.
-#    (static versions for download exist on the internet)
-#
-# input is:
-# mjr2webm -v myvideo.mjr -a myaudio.mjr out.webm
-myhelp () {
-    echo $0
-    echo 'Usage: mjr2webm --video [videofile] --audio [audiofile] --ffmpegVideoParams [ffmpegVideoParams] --ffmpegAudioParams [ffmpegAudioParams] [outputfile]'
-    echo '   "[videofile]" must be a .mjr video file output from Janus'
-    echo '   "[audiofile]" must be a .mjr audio file output from Janus'
-    echo '   "[ffmpegVideoParams]" overwrite video ffmpeg params'
-    echo '   "[ffmpegAudioParams]" overwrite audio ffmpeg params'
-    echo '   "[outputfile]", if not specified, is "default.webm"; if specified, it must end with filetype ".webm"'
+function error {
+  >&2 echo $1
+  exit 1
 }
 
+# set defaults
+ffmpegParams="-map 0:v -map 1:a -codec copy"
 
-if test $# -eq 0; then
-   myhelp
-   exit 0
-fi
-
-if ! which ffmpeg > /dev/null; then
-   if ! which ./ffmpeg > /dev/null; then
-     echo "Aborting: No 'ffmpeg' found" 1>&2
-     exit 1
-   else
-      ffmpeg=./ffmpeg
-   fi
-else
-   ffmpeg=ffmpeg
-fi
-
-if ! which janus-pp-rec > /dev/null; then
-     echo "Aborting: No 'janus-pp-rec' found" 1>&2
-     exit 1
-fi
-
-
-ffmpegVideoParams="-c:v libvpx -b:v 500k -r 25"
-ffmpegAudioParams="-c:a libopus -b:a 64k"
-
+# parse optional arguments
 while test $# -gt 0
 do
   case $1 in
-
-  # Normal option processing
     --help)
-      # usage and help
-      myhelp
-      exit 0
-      break
+      echo "Usage: mjr2webm [--ffmpeg-params <ffmpeg-params>] <audio-mjr-source> <video-mjr-source> <output-file>"
+      exit 1
       ;;
-    --video)
+    --ffmpeg-params)
       shift
-      video=$1
+      ffmpegParams=$1
       ;;
-    --audio)
-      shift
-      audio=$1
-      ;;
-    --ffmpeg-audio-params)
-      shift
-      ffmpegAudioParams=$1
-      ;;
-    --ffmpeg-video-params)
-      shift
-      ffmpegVideoParams=$1
-      ;;
-  # ...
-  # Done with options
     *)
       break
       ;;
   esac
-
   shift
 done
 
-if [ -z $audio ] && [ -z $video ]; then
-  echo "Must provide audio or video params (or both)"
-  exit 1
+videoMjr=$1
+audioMjr=$2
+outputFile=$3
+
+if [ -z $videoMjr ] || [ ! -f $videoMjr ] || [[ "$videoMjr" != *.mjr ]]; then
+  error "Must specify existing <video-mjr-source>"
 fi
 
-if ! [ -z $video ]; then
-  if ! [ -f $video ]; then
-     echo "Videofile $video does not exist"
-     exit 1
-  else
-     if ! [[ "$video" == *.mjr ]]; then
-        echo "Video file must end with .mjr"
-        exit 1
-     fi
-  fi
-fi
-if ! [ -z $audio ]; then
-  if ! [ -f $audio ]; then
-     echo "Audio $audio does not exist"
-     exit 1
-  else
-     if ! [[ "$audio" == *.mjr ]]; then
-        echo "Audio file must end with .mjr"
-        exit 1
-     fi
-  fi
+if [ -z $audioMjr ] || [ ! -f $audioMjr ] || [[ "$audioMjr" != *.mjr ]]; then
+    error "Must specify existing <audio-mjr-source>"
 fi
 
-if [ -z "$1" ]; then
-    echo "Using default.webm as output file"
-    out=default.webm
-else
-    out=$1
-    if ! [[ "$out" == *.webm ]]; then
-       echo "Output file must end with .webm"
-       exit 1
-    fi
+if [ -z $outputFile ]; then
+  error "Must specify <output-file>"
 fi
 
 TFILE="$$"
-opus=$TFILE.opus
-webm=$TFILE.webm
-inputs=""
-params=""
+videoSource=$TFILE.webm
+audioSource=$TFILE.opus
 
-# convert video to webm
-if ! [ -z $video ]; then
-  janus-pp-rec $video $webm
-  inputs="$inputs -i $webm"
-  params="$params $ffmpegVideoParams"
-fi
+janus-pp-rec $videoMjr $videoSource
+janus-pp-rec $audioMjr $audioSource
 
-# convert audio to opus
-if ! [ -z $audio ]; then
-  janus-pp-rec $audio $opus
-  inputs="$inputs -i $opus"
-  params="$params$ffmpegAudioParams"
-fi
-echo $ffmpeg $inputs $params $out
-$ffmpeg $inputs $params $out
+command="ffmpeg -i $videoSource -i $audioSource $ffmpegParams $outputFile"
+echo $command
+$command
 
-if [ -e $webm ]; then
-  rm $webm
-fi
-if [ -e $opus ]; then
-  rm $opus
-fi
-
-echo "done!"
+rm $videoSource
+rm $audioSource
