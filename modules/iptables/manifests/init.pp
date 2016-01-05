03f09ce52f20badd51ff1c636e95b4ff160652f6
ppp0 <philipp.gysin@cargomedia.ch>
2016-01-05 18:18:21 +0100

    Revert "Add option for "useful ICMP""
    This reverts commit ed665a9031f604b2a6cf00a9e382a089d1a2c7ec.

--- modules/iptables/manifests/init.pp
+++ modules/iptables/manifests/init.pp
@@ -1,6 +1,5 @@
 class iptables(
   $stateful = true,
-  $allow_useful_ICMP = false,
 ) {
 
   require 'apt'
@@ -14,37 +13,6 @@ class iptables(
     iptables::entry { 'Stateful firewall init':
       chain => 'INPUT',
       rule  => '-m state --state RELATED,ESTABLISHED -j ACCEPT',
-    }
-  }
-
-  if ($allow_useful_ICMP) {
-    iptables::entry { 'Allow icmp type 3':
-      chain => 'INPUT',
-      rule  => '-p icmp  -m icmp  --icmp-type 3  -m state --state NEW  -j ACCEPT',
-    }
-    ->
-
-    iptables::entry { 'Allow icmp type 0/0':
-      chain => 'INPUT',
-      rule  => '-p icmp  -m icmp  --icmp-type 0/0  -m state --state NEW  -j ACCEPT',
-    }
-    ->
-
-    iptables::entry { 'Allow icmp type 8/0':
-      chain => 'INPUT',
-      rule  => '-p icmp  -m icmp  --icmp-type 8/0  -m state --state NEW  -j ACCEPT',
-    }
-    ->
-
-    iptables::entry { 'Allow icmp type 11/0':
-      chain => 'INPUT',
-      rule  => '-p icmp  -m icmp  --icmp-type 11/0  -m state --state NEW  -j ACCEPT',
-    }
-    ->
-
-    iptables::entry { 'Allow icmp type 11/1':
-      chain => 'INPUT',
-      rule  => '-p icmp  -m icmp  --icmp-type 11/1  -m state --state NEW  -j ACCEPT',
     }
   }
 }
