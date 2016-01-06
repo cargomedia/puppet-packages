3ce1c6556cd6cfe613b55c56457bf337c53776ee
ppp0 <philipp.gysin@cargomedia.ch>
2016-01-06 11:48:05 +0100

    Status active, not inactive

--- modules/ufw/manifests/service.pp
+++ modules/ufw/manifests/service.pp
@@ -3,7 +3,7 @@ class ufw::service {
   exec { 'Activate ufw':
     provider => shell,
     command  => 'echo "y" | ufw enable >/dev/null',
-    unless   => 'ufw status | grep -q \'Status: inactive\'',
+    unless   => 'ufw status | grep -q \'Status: active\'',
     path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     user     => 'root',
     require  => Package['ufw'],
