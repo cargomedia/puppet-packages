2be90b612d21ed5c6f8510d29bb15df28c0d1f47
ppp0 <philipp.gysin@cargomedia.ch>
2016-01-14 18:19:49 +0100

    Simplify RAID checks for SAS

--- modules/raid/spec/sas2ircu/spec.rb
+++ modules/raid/spec/sas2ircu/spec.rb
@@ -2,15 +2,11 @@ require 'spec_helper'
 
 describe 'raid::sas2ircu' do
 
-  describe package('sas2ircu-status') do
+  describe package('sas2ircu') do
     it { should be_installed }
   end
 
   describe command('monit summary') do
-    its(:stdout) { should match /Process 'sas2ircu-statusd'/ }
-  end
-
-  describe file('/etc/default/sas2ircu-statusd') do
-    it { should be_file }
+    its(:stdout) { should match /Program 'raid-sas'/ }
   end
 end
