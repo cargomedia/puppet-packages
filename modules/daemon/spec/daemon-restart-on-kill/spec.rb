b3e80e8e2e523b0cdd3a0a78a48efc42f08a7644
ppp0 <philipp.gysin@cargomedia.ch>
2016-01-18 11:15:34 +0100

    Adjust specs

--- modules/daemon/spec/daemon-restart-on-kill/spec.rb
+++ modules/daemon/spec/daemon-restart-on-kill/spec.rb
@@ -7,23 +7,7 @@ describe 'daemon' do
     it { should be_running }
   end
 
-# With *sysvinit* we check monit for process
-  check_initv = "monit summary | grep -qE 'Process+.+my-program'"
-
-# With *systemd* we check monit for program
-  check_systemd = "monit summary | grep -qE 'Program+.+my-program+.+ok'"
-
-  describe command("if (test -e /bin/systemctl); then #{check_systemd}; else #{check_initv}; fi") do
-    its(:exit_status) { should eq 0 }
-  end
-
-# With *sysvinit* we don't test anything
-  check_initv = "echo ok"
-
-# With *systemd* we check that monit has alerted
-  check_systemd = "journalctl -u monit --no-pager | grep -qE 'my-program+.+\/bin\/systemctl+.+failed'"
-
-  describe command("if (test -e /bin/systemctl); then #{check_systemd}; else #{check_initv}; fi") do
+  describe command("monit summary | grep -E 'Pro.+my-program.+[Running|ok]'") do
     its(:exit_status) { should eq 0 }
   end
 end
