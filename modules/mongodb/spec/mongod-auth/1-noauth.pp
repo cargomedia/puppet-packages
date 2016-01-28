a86143f82652ab08211b497194c945b693f02b10
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-28 13:49:48 +0100

    Updated/extended specs

--- modules/mongodb/spec/mongod-auth/1-noauth.pp
+++ modules/mongodb/spec/mongod-auth/1-noauth.pp
@@ -1,25 +1,7 @@
 node default {
 
-  $root_username = 'siteRootAdmin'
-  $root_password = 'abc123'
-  $root_dbname = 'admin'
-
   mongodb::core::mongod { 'server':
     port => 27017,
     auth => false,
-  }
-  ->
-
-  mongodb_user { $root_username:
-    database => $root_dbname,
-    password => $root_password,
-    roles    => [ { 'role' => 'root', 'db' => $root_dbname } ]
-  }
-  ->
-
-  mongodb::mongorc::autologin { 'autologin-root':
-    username => $root_username,
-    password => $root_password,
-    database => $root_dbname,
   }
 }
