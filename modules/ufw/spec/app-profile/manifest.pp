7e9a0c4c5616e8b2042336ed6d89d585b2130297
Chris Stasiak <me@krzysztof-stasiak.com>
2016-02-08 12:32:54 +0100

    Added spec for rules from/to

--- modules/ufw/spec/app-profile/manifest.pp
+++ modules/ufw/spec/app-profile/manifest.pp
@@ -1,8 +1,21 @@
 node default {
 
   ufw::application{ 'foo':
-    app_name => 'food',
+    app_name        => 'food',
     app_description => 'food - the foo daemon',
-    app_ports => '21,23:25/tcp|10000:15000/udp',
+    app_ports       => '21,23:25/tcp|10000:15000/udp',
   }
+  ->
+
+  ufw::rule { 'food from 127.0.0.0/8 with port 25':
+    app_or_port => 25,
+    from        => '127.0.0.0/8'
+  }
+  ->
+
+  ufw::rule { 'food to 10.0.0.0/8':
+    app_or_port => 'food',
+    to          => '10.0.0.0/8'
+  }
+
 }
