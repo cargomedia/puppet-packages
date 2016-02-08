5de088ebd2004397ef281dc8cc38f3e1a654d47b
Chris Stasiak <me@krzysztof-stasiak.com>
2016-02-08 15:15:50 +0100

    Renamed app_or_port_default with target

--- modules/ufw/manifests/rule.pp
+++ modules/ufw/manifests/rule.pp
@@ -19,14 +19,14 @@ define ufw::rule(
   }
 
   $is_port = ((count(split($app_or_port, ',')) > 1) or is_integer($app_or_port))
-  $app_or_port_default = $is_port ? {
+  $target = $is_port ? {
     true => "port ${$app_or_port}",
     default => "app ${app_or_port}",
   }
 
-  exec { "Set ${app_or_port_default} allow to ${allow} from ${from} to ${to}":
+  exec { "Set ${target} allow to ${allow} from ${from} to ${to}":
     provider => shell,
-    command  => "ufw ${verb_allow} ${proto} from ${from} to ${to} ${app_or_port_default}",
+    command  => "ufw ${verb_allow} ${proto} from ${from} to ${to} ${target}",
     unless   => "ufw status | grep -iqE '^${app_or_port}+.+${verb_allow}'",
     path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     user     => 'root',
