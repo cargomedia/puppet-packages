7ef2528bd09c2e6cbd2dbc48cf30ad92e5b64a0e
Chris Stasiak <me@krzysztof-stasiak.com>
2016-02-08 14:09:24 +0100

    Made qfw:rule more configurable

--- modules/ufw/manifests/rule.pp
+++ modules/ufw/manifests/rule.pp
@@ -2,7 +2,8 @@ define ufw::rule(
   $app_or_port = $title,
   $allow = true,
   $from = 'any',
-  $to = 'any'
+  $to = 'any',
+  $proto = undef
 ) {
 
   include 'ufw'
@@ -12,14 +13,20 @@ define ufw::rule(
     default => 'allow',
   }
 
-  $app_or_port_default = is_integer($app_or_port) ? {
+  $proto_default = $proto ? {
+    undef   => '',
+    default => "proto ${proto}",
+  }
+
+  $is_port = ((count(split($app_or_port, ',')) > 1) or is_integer($app_or_port))
+  $app_or_port_default = $is_port ? {
     true => "port ${$app_or_port}",
     default => "app ${app_or_port}",
   }
 
   exec { "Set ${app_or_port_default} allow to ${allow} from ${from} to ${to}":
     provider => shell,
-    command  => "ufw ${verb_allow} from ${from} to ${to} ${app_or_port_default}",
+    command  => "ufw ${verb_allow} ${proto_default} from ${from} to ${to} ${app_or_port_default}",
     unless   => "ufw status | grep -iqE '^${app_or_port}+.+${verb_allow}'",
     path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     user     => 'root',
