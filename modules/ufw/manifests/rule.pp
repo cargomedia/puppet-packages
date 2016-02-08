1f6a80991a9dc4b6a11ce643f4b2e2a5e34c4289
Chris Stasiak <me@krzysztof-stasiak.com>
2016-02-08 12:31:36 +0100

    Made from/to configurable fro UFW/rule

--- modules/ufw/manifests/rule.pp
+++ modules/ufw/manifests/rule.pp
@@ -1,6 +1,8 @@
 define ufw::rule(
   $app_or_port = $title,
   $allow = true,
+  $from = 'any',
+  $to = 'any'
 ) {
 
   include 'ufw'
@@ -10,9 +12,14 @@ define ufw::rule(
     default => 'allow',
   }
 
-  exec { "Set ${app_or_port} allow to ${allow}":
+  $app_or_port_default = is_integer($app_or_port) ? {
+    true => "port ${$app_or_port}",
+    default => "app ${app_or_port}",
+  }
+
+  exec { "Set ${app_or_port_default} allow to ${allow} from ${from} to ${to}":
     provider => shell,
-    command  => "ufw ${verb_allow} ${app_or_port}",
+    command  => "ufw ${verb_allow} from ${from} to ${to} ${app_or_port_default}",
     unless   => "ufw status | grep -iqE '^${app_or_port}+.+${verb_allow}'",
     path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     user     => 'root',
