d9c690b52468c01d3d37a6a263bcdbdf7e6b1db8
ppp0 <philipp.gysin@cargomedia.ch>
2016-02-10 11:33:01 +0100

    Add configurable server_names_hash_bucket_size

--- modules/nginx/manifests/config.pp
+++ modules/nginx/manifests/config.pp
@@ -1,17 +1,18 @@
 class nginx::config(
-  $daemon_user            = $nginx::params::nx_daemon_user,
-  $worker_processes       = $nginx::params::nx_worker_processes,
-  $worker_connections     = $nginx::params::nx_worker_connections,
-  $worker_rlimit_nofile   = $nginx::params::nx_worker_rlimit_nofile,
-  $proxy_set_header       = $nginx::params::nx_proxy_set_header,
-  $keepalive_timeout      = $nginx::params::nx_keepalive_timeout,
-  $server_tokens          = $nginx::params::nx_server_tokens,
-  $send_timeout           = $nginx::params::nx_send_timeout,
-  $client_max_body_size   = $nginx::params::nx_client_max_body_size,
-  $client_body_timeout    = $nginx::params::nx_client_body_timeout,
-  $client_header_timeout  = $nginx::params::nx_client_header_timeout,
-  $access_log             = $nginx::params::nx_access_log,
-  $confd_purge            = $nginx::params::nx_confd_purge
+  $daemon_user                   = $nginx::params::nx_daemon_user,
+  $worker_processes              = $nginx::params::nx_worker_processes,
+  $worker_connections            = $nginx::params::nx_worker_connections,
+  $worker_rlimit_nofile          = $nginx::params::nx_worker_rlimit_nofile,
+  $proxy_set_header              = $nginx::params::nx_proxy_set_header,
+  $keepalive_timeout             = $nginx::params::nx_keepalive_timeout,
+  $server_tokens                 = $nginx::params::nx_server_tokens,
+  $send_timeout                  = $nginx::params::nx_send_timeout,
+  $client_max_body_size          = $nginx::params::nx_client_max_body_size,
+  $client_body_timeout           = $nginx::params::nx_client_body_timeout,
+  $client_header_timeout         = $nginx::params::nx_client_header_timeout,
+  $access_log                    = $nginx::params::nx_access_log,
+  $confd_purge                   = $nginx::params::nx_confd_purge,
+  $server_names_hash_bucket_size = $nginx::params::nx_server_names_hash_bucket_size
 ) inherits nginx::params {
   File {
     owner => 'root',
