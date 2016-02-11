0c95b63019cb83e49d4db5e7cb9e98319e1a360c
ppp0 <philipp.gysin@cargomedia.ch>
2016-02-11 16:59:45 +0100

    Use apt for all except wheezy

--- modules/php5/manifests/extension/imagick.pp
+++ modules/php5/manifests/extension/imagick.pp
@@ -3,24 +3,32 @@ class php5::extension::imagick (
 ) {
 
   require 'apt'
-  require 'build'
-  require 'php5'
 
-  package { 'libmagickwand-dev':
-    ensure   => present,
-    provider => 'apt',
-  }
-  ->
+  if $::lsbdistcodename == 'wheezy' {
+    require 'build'
+    require 'php5'
 
-  helper::script { 'install php5::extension::imagick':
-    content => template("${module_name}/extension/imagick/install.sh"),
-    unless  => "php --re imagick | grep -w 'imagick version ${version}'",
-    require => Class['php5'],
-  }
-  ->
+    package { 'libmagickwand-dev':
+      ensure   => present,
+      provider => 'apt',
+    }
+    ->
 
-  php5::config_extension { 'imagick':
-    content => template("${module_name}/extension/imagick/conf.ini"),
+    helper::script { 'install php5::extension::imagick':
+      content => template("${module_name}/extension/imagick/install.sh"),
+      unless  => "php --re imagick | grep -w 'imagick version ${version}'",
+      require => Class['php5'],
+    }
+    ->
+    php5::config_extension { 'imagick':
+      content => template("${module_name}/extension/imagick/conf.ini"),
+    }
+
+  } else {
+    package { 'php5-imagick':
+      ensure   => present,
+      provider => 'apt',
+    }
   }
 
 }
