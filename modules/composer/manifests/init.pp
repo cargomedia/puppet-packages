384cbfa0063e64f4e75f13f5b6c1f0b0e44bf140
ppp0 <philipp.gysin@cargomedia.ch>
2016-02-18 16:21:54 +0100

    Limit suhosin exception config to wheezy, add jessie

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -10,7 +10,7 @@ class composer($version = '1.0.0-alpha10') {
     command => "curl -sL http://getcomposer.org/download/${version}/composer.phar > ${phar}",
     path    => ['/usr/local/bin', '/usr/bin', '/bin'],
     unless  => "${binary} --version | grep -w '${version}'",
-    require => [File[$binary], File[$config]],
+    require => [File[$binary]],
   }
 
   file { $binary:
@@ -21,11 +21,14 @@ class composer($version = '1.0.0-alpha10') {
     mode    => '0755',
   }
 
-  file { $config:
-    ensure  => file,
-    content => template("${module_name}/composer.ini"),
-    owner   => '0',
-    group   => '0',
-    mode    => '0644',
+  if $::lsbdistcodename == 'wheezy' {
+    file { $config:
+      ensure  => file,
+      content => template("${module_name}/composer.ini"),
+      owner   => '0',
+      group   => '0',
+      mode    => '0644',
+      before => Exec["curl ${phar}"],
+    }
   }
 }
