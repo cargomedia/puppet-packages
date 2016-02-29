c92491f56289365e88dc43edb27b78d6bb22b2de
ppp0 <philipp.gysin@cargomedia.ch>
2016-02-29 15:00:45 +0100

    Add one-time backing up of network config

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,5 +1,13 @@
 class network {
 
+  exec { 'Backup existing network config':
+    provider => shell,
+    command  => 'cp /etc/network/interfaces /etc/network/interfaces.provisioned',
+    unless   => 'ls /etc/network/interfaces.provisioned',
+    path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    user     => 'root',
+  }
+
   $interfaces = hiera_hash('network::interfaces', { })
   create_resources('network::interface', $interfaces)
 
