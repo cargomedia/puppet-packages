a02b5147ef443344f08e3d6d939a5e123d312c88
Chris Stasiak <me@krzysztof-stasiak.com>
2016-03-08 17:25:10 +0100

    Fixed from-src spec

--- modules/janus/spec/from-src/manifest.pp
+++ modules/janus/spec/from-src/manifest.pp
@@ -1,8 +1,8 @@
 node default {
 
-  janus::plugin::rtpbroadcast { 'from-src': }
   janus::core::janus { 'from-src': }
   janus::transport::websockets { 'from-src': }
+  janus::plugin::rtpbroadcast { 'from-src': }
   janus::plugin::audioroom { 'from-src': }
 
 }
