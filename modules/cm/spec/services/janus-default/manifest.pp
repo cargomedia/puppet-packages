5756f9ba8816d575ddcb0bf69cd0e872d83fe252
Chris Stasiak <me@krzysztof-stasiak.com>
2016-03-08 13:46:08 +0100

    Fixed janus-default

--- modules/cm/spec/services/janus-default/manifest.pp
+++ modules/cm/spec/services/janus-default/manifest.pp
@@ -1,5 +1,6 @@
 node default {
 
   cm::services::janus { 'default':
+    jobs_path => '/opt/janus-cluster/default/var/lib/janus/jobs',
   }
 }
--- modules/cm/spec/services/janus-default/spec.rb
+++ modules/cm/spec/services/janus-default/spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe 'cm::services::janus' do
 
-  # cm-janus cannot run without cm-api https://github.com/cargomedia/puppet-packages/issues/1196
-  # describe service('cm-janus') do
-  #   it { should be_enabled }
-  #   it { should be_running }
-  # end
+  describe service('cm-janus_default') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 
   describe service('janus_default') do
     it { should be_enabled }
