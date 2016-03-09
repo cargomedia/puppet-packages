3f45a8006f9533a063f64e7426d72c82d547f6df
Chris Stasiak <me@krzysztof-stasiak.com>
2016-03-09 09:42:35 +0100

    Removed defaults from setup_dirs

--- modules/janus/manifests/server/setup_dirs.pp
+++ modules/janus/manifests/server/setup_dirs.pp
@@ -1,15 +1,10 @@
 define janus::server::setup_dirs (
   $base_dir,
-  $config_dir = undef,
-  $plugins_folder = undef,
-  $transports_folder = undef,
-  $ssl_config_dir = undef,
+  $config_dir,
+  $plugins_folder,
+  $transports_folder,
+  $ssl_config_dir,
 ) {
-
-  $config_dir_default = $config_dir ? { undef => "${base_dir}/etc/janus", default => $config_dir }
-  $ssl_config_dir_default = $ssl_config_dir_default ? { undef => "${base_dir}/etc/janus/ssl", default => $ssl_config_dir_default }
-  $plugins_folder_default = $plugins_folder_default ? { undef => "${base_dir}/usr/lib/janus/plugins.enabled", default => $plugins_folder_default }
-  $transports_folder_default = $transports_folder_default ? { undef => "${base_dir}/usr/lib/janus/transports.enabled", default => $transports_folder_default }
 
   $base_dirs = "${base_dir}/etc ${base_dir}/var/lib ${base_dir}/var/log ${base_dir}/usr/lib/janus"
 
@@ -22,7 +17,7 @@ define janus::server::setup_dirs (
   ->
 
   file {
-    [ $config_dir_default, $ssl_config_dir_default]:
+    [ $config_dir, $ssl_config_dir]:
       ensure  => directory,
       owner   => '0',
       group   => '0',
@@ -30,7 +25,7 @@ define janus::server::setup_dirs (
       purge   => true,
       recurse => true,
       require => Exec["Create base dirs in janus-cluster for ${title}"];
-    [ $plugins_folder_default, $transports_folder_default ]:
+    [ $plugins_folder, $transports_folder ]:
       ensure  => directory,
       owner   => 'janus',
       group   => 'janus',
