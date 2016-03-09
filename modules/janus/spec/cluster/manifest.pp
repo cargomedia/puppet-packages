57f31243a695cba6d5d9251b5d3c8103deb99463
Chris Stasiak <me@krzysztof-stasiak.com>
2016-03-09 09:47:48 +0100

    Cluster spec - inlined variables

--- modules/janus/spec/cluster/manifest.pp
+++ modules/janus/spec/cluster/manifest.pp
@@ -3,10 +3,6 @@ node default {
   include 'monit'
   include 'bipbip'
 
-  $origin_name = 'origin'
-  $edge1_name = 'edge1'
-  $edge2_name = 'edge2'
-  $edge3_name = 'edge3'
   $cluster_base_dir = '/opt/janus-cluster'
 
   Janus::Server {
@@ -30,54 +26,54 @@ node default {
   }
 
   # origin node
-  janus::server { $origin_name: }
+  janus::server { 'origin': }
 
-  janus::transport::websockets { $origin_name:
+  janus::transport::websockets { 'origin':
     ws_port  => 10000,
   }
 
-  janus::transport::http { $origin_name:
+  janus::transport::http { 'origin':
     port  => 8000,
   }
 
   #  1st edge node
-  janus::server { $edge1_name: }
+  janus::server { 'edge1': }
 
-  janus::transport::http { $edge1_name:
+  janus::transport::http { 'edge1':
     port => 8001,
   }
 
-  janus::transport::websockets { $edge1_name:
+  janus::transport::websockets { 'edge1':
     ws_port  => 10001,
   }
 
-  janus::plugin::audioroom { $edge1_name:
+  janus::plugin::audioroom { 'edge1':
     rest_url => 'http://127.0.0.1:8001/janus',
   }
 
   #  2nd edge node
-  janus::server { $edge2_name: }
+  janus::server { 'edge2': }
 
-  janus::transport::http { $edge2_name:
+  janus::transport::http { 'edge2':
     port => 8002,
   }
 
-  janus::plugin::audioroom { $edge2_name:
+  janus::plugin::audioroom { 'edge2':
     rest_url => 'http://127.0.0.1:8002/janus',
   }
 
   #  3rd edge node
-  janus::server { $edge3_name: }
+  janus::server { 'edge3': }
 
-  janus::transport::websockets { $edge3_name:
+  janus::transport::websockets { 'edge3':
     ws_port  => 10003,
   }
 
-  janus::transport::http { $edge3_name:
+  janus::transport::http { 'edge3':
     port => 8003,
   }
 
-  janus::plugin::rtpbroadcast { $edge3_name:
+  janus::plugin::rtpbroadcast { 'edge3':
     rest_url => 'http://127.0.0.1:8003/janus',
   }
 }
