11f57748fadc94659043d9d9ce4a1bb5360376b2
Reto Kaiser <reto@cargomedia.ch>
2016-03-14 18:06:44 +0100

    Require "apt" for "vagrant" because installer uses DPKG

--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -3,9 +3,12 @@ class vagrant {
   $version = '1.7.3'
   $url = 'https://releases.hashicorp.com/vagrant/1.7.3/vagrant_1.7.3_x86_64.deb'
 
+  require 'apt'
+
   helper::script { 'install vagrant':
     content     => template("${module_name}/install.sh"),
     unless      => "vagrant --version | grep '^Vagrant ${version}$'",
     environment => ['HOME=/tmp'],   # Vagrant needs $HOME (https://github.com/mitchellh/vagrant/issues/2215)
+    require => Class['apt::update'],
   }
 }
