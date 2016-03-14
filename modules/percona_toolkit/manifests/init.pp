048e753d2f3bc2d9ecdfcb4d4e6f18dcd6ab0510
Reto Kaiser <reto@cargomedia.ch>
2016-03-14 17:56:56 +0100

    Require "apt" for "percona_toolkit" because installer uses DPKG

--- modules/percona_toolkit/manifests/init.pp
+++ modules/percona_toolkit/manifests/init.pp
@@ -1,10 +1,13 @@
 class percona_toolkit {
 
+  require 'apt'
+
   $version = '2.2.5-2'
   $version_nobuild = regsubst($version, '-[^-]+$', '')
 
   helper::script { 'install percona-toolkit':
     content => template("${module_name}/install.sh"),
-    unless  => "pt-online-schema-change --version | grep -w ${version_nobuild}"
+    unless  => "pt-online-schema-change --version | grep -w ${version_nobuild}",
+    require => Class['apt::update'],
   }
 }
