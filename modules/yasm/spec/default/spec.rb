00baa7a033ede3c985145807091fd728f92f5e61
ppp0 <philipp.gysin@cargomedia.ch>
2016-03-16 22:03:30 +0100

    Remove unneeded yasm, clean up

--- modules/yasm/spec/default/spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'yasm' do
-
-  describe file('/usr/local/bin/yasm') do
-    it { should be_executable }
-  end
-end
--- modules/yasm/templates/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-curl -sL https://github.com/yasm/yasm/archive/v<%= @version %>.zip > yasm-<%= @version %>.zip
-unzip yasm-<%= @version %>.zip
-cd yasm-<%= @version %>
-./autogen.sh
-make
-make install
