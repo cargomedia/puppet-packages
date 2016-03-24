1bb4b0e04bba4cd3e367127bb7e6411ff325d3f1
ppp0 <philipp.gysin@cargomedia.ch>
2016-03-24 14:56:50 +0100

    Use nginx as http server, remove apache2, mandatory SSL/https, adapt tests, introduce jessie support

--- modules/satis/manifests/init.pp
+++ modules/satis/manifests/init.pp
@@ -1,7 +1,12 @@
-class satis($hostname) {
+class satis(
+  $hostname,
+  $ssl_cert,
+  $ssl_key,
+) {
 
   require 'composer'
   require 'git'
+  include 'nginx'
 
   $version = '68ba9149b30da77ab6d8b37712e5a7d531c5a5f4'
 
@@ -35,9 +40,28 @@ class satis($hostname) {
     group  => 'satis',
     mode   => '0755',
   }
-  ->
 
-  apache2::vhost{ $hostname:
-    content => template("${module_name}/vhost"),
+  nginx::resource::vhost{ "${module_name}-https-redirect":
+    listen_port         => 80,
+    ssl                 => false,
+    server_name         => [$hostname],
+    location_cfg_append => [
+      'return 301 https://$host$request_uri;',
+    ],
+  }
+
+  nginx::resource::vhost { $module_name:
+    server_name         => [$hostname],
+    listen_port         => 443,
+    ssl                 => true,
+    ssl_cert            => $ssl_cert,
+    ssl_key             => $ssl_key,
+    location_cfg_append => [
+      'root /var/lib/satis/public/;',
+      'gzip on;',
+      'gzip_proxied any;',
+      'gzip_http_version 1.0;',
+      'gzip_types application/json text/plain;',
+    ],
   }
 }
