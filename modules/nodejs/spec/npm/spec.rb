579db4fb0d07b72d0f1f7ec403febcc4b350da48
ppp0 <philipp.gysin@cargomedia.ch>
2016-04-04 16:38:10 +0200

    Adjust assertion

--- modules/nodejs/spec/npm/spec.rb
+++ modules/nodejs/spec/npm/spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'package provider npm' do
 
-  describe command('npm list redis -g') do
-    its(:stdout) { should match 'redis' }
+  describe command('npm list socket-redis -g') do
+    its(:stdout) { should match 'socket-redis' }
   end
 end
