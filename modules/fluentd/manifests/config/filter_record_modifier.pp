2eeb2d563de62deacb2d0e9df17b94bda8628894
Reto Kaiser <reto@cargomedia.ch>
2016-04-08 10:41:01 +0200

    Fluentd: Include record_modifier plugin in config

--- modules/fluentd/manifests/config/filter_record_modifier.pp
+++ modules/fluentd/manifests/config/filter_record_modifier.pp
@@ -5,6 +5,8 @@ define fluentd::config::filter_record_modifier (
   $priority = 50,
 ) {
 
+  include 'fluentd::plugin::record_modifier'
+
   fluentd::config { "filter_record_modifier-${title}":
     content  => template( 'fluentd/config/filter_record_modifier.conf.erb' ),
     priority => $priority,
