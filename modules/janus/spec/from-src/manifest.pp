ae04196e1a90559398f47bcb9b4be7ee5fb59f95
Chris Stasiak <me@krzysztof-stasiak.com>
2016-04-20 12:38:37 +0100

    Adjusted specs

--- modules/janus/spec/from-src/manifest.pp
+++ modules/janus/spec/from-src/manifest.pp
@@ -2,7 +2,9 @@ node default {
 
   janus::server { 'from-src': }
   janus::transport::websockets { 'from-src': }
-  janus::plugin::rtpbroadcast { 'from-src': }
+  janus::plugin::rtpbroadcast { 'from-src':
+    hostname => '127.0.0.1'
+  }
   janus::plugin::audioroom { 'from-src': }
 
 }
