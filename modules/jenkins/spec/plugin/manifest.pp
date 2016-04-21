be82eb670ebc05ed1f8582ad40e49dbee2d4081b
Reto Kaiser <reto@cargomedia.ch>
2016-04-21 16:32:08 +0200

    Combine some jenkins specs for faster execution

--- modules/jenkins/spec/plugin/manifest.pp
+++ modules/jenkins/spec/plugin/manifest.pp
@@ -4,12 +4,28 @@ node default {
     hostname => 'example.com'
   }
 
-  jenkins::plugin { 'git':
-    version => '2.4.4',
-  }
-
   jenkins::plugin { 'pagerduty':
     version => '0.2.4',
+  }
+
+  class { 'jenkins::plugin::git':
+    global_config_name  => 'foo',
+    global_config_email => 'bar@example.com',
+  }
+
+  class { 'jenkins::plugin::github':
+    oauth_access_token => 'foo',
+  }
+
+  class { 'jenkins::plugin::ghprb':
+    access_token => 'xxx',
+    admin_list   => ['foo', 'bar']
+  }
+
+  class { 'jenkins::plugin::ansicolor':
+  }
+
+  class { 'jenkins::plugin::embeddable_build_status':
   }
 
 }
--- modules/jenkins/spec/plugin/spec.rb
+++ modules/jenkins/spec/plugin/spec.rb
@@ -2,16 +2,6 @@ require 'spec_helper'
 
 describe 'jenkins::plugin' do
 
-  describe file('/var/lib/jenkins/plugins/git.hpi') do
-    it { should be_file }
-    it { should be_owned_by 'jenkins' }
-  end
-
-  describe file('/var/lib/jenkins/plugins/pagerduty.hpi') do
-    it { should be_file }
-    it { should be_owned_by 'jenkins' }
-  end
-
   # Wait for jenkins to start up
   describe command('timeout --signal=9 30 bash -c "while ! (curl -s http://localhost:8080/ | grep -q "Dashboard"); do sleep 0.5; done"') do
     its(:exit_status) { should eq 0 }
@@ -22,8 +12,12 @@ describe 'jenkins::plugin' do
   end
 
   describe command('curl -s http://localhost:8080/pluginManager/installed') do
-    its(:stdout) { should match /git/ }
     its(:stdout) { should match /pagerduty/ }
+    its(:stdout) { should match /git/ }
+    its(:stdout) { should match /github/ }
+    its(:stdout) { should match /ghprb/ }
+    its(:stdout) { should match /ansicolor/ }
+    its(:stdout) { should match /embeddable-build-status/ }
   end
 
 end
