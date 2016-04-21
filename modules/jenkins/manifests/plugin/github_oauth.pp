c768f7dfc935996c32610278337cdfe97fae5219
Reto Kaiser <reto@cargomedia.ch>
2016-04-21 13:38:08 +0200

    Add matrix-auth plugin

--- modules/jenkins/manifests/plugin/github_oauth.pp
+++ modules/jenkins/manifests/plugin/github_oauth.pp
@@ -5,6 +5,7 @@ class jenkins::plugin::github_oauth(
   $client_secret
 ) {
 
+  require 'jenkins::plugin::matrix_auth'
   require 'jenkins::plugin::github_api'
 
   file { '/var/lib/jenkins/config.d/30-github-oauth.xml':
