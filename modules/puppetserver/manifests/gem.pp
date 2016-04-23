69223e0abc1a20e6f50f2485f9ceaa4543a802e7
Reto Kaiser <reto@cargomedia.ch>
2016-04-23 20:32:43 +0200

    Restart puppetserver after installing a gem

--- modules/puppetserver/manifests/gem.pp
+++ modules/puppetserver/manifests/gem.pp
@@ -3,10 +3,11 @@ define puppetserver::gem {
   include 'puppetserver'
 
   package { "${title} (puppetserver)":
-    name  => $title,
+    name     => $title,
     ensure   => present,
     provider => puppetserver_gem,
     require  => Package['puppetserver'],
+    notify   => Service['puppetserver'],
   }
 
 }
