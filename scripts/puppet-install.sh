9fbf388224b4d84a78d1c8c4c6a12072ce23dc70
Reto Kaiser <reto@cargomedia.ch>
2016-04-23 18:44:13 +0200

    puppet install script: drop support for OSX

--- scripts/puppet-install.sh
+++ scripts/puppet-install.sh
@@ -6,7 +6,7 @@ if [ "$EUID" != "0" ]; then
 	exit 1;
 fi
 
-if (which dpkg-query && ! dpkg-query --show 'lsb-release'); then
+if (which dpkg-query >/dev/null && ! dpkg-query --show 'lsb-release'); then
   apt-get update
   apt-get install -qy lsb-release
 fi
@@ -25,27 +25,6 @@ if (which lsb_release >/dev/null && lsb_release --id | grep -qE "(Debian|Ubuntu)
 	  rm -f "${binary_dest}"
 	  ln -s "/opt/puppetlabs/bin/${binary}" "${binary_dest}"
 	done
-
-elif (uname | grep -q 'Darwin'); then
-	function install_dmg() {
-		local name="$1"
-		local url="$2"
-		local dmg_path=$(mktemp -t ${name}-dmg)
-
-		curl -sLo ${dmg_path} ${url}
-
-		local plist_path=$(mktemp -t puppet-bootstrap)
-		hdiutil attach -plist ${dmg_path} > ${plist_path}
-		mount_point=$(grep -E -o '/Volumes/[-.a-zA-Z0-9]+' ${plist_path})
-
-		pkg_path=$(find ${mount_point} -name '*.pkg' -mindepth 1 -maxdepth 1)
-		installer -pkg ${pkg_path} -target / >/dev/null
-
-		hdiutil eject ${mount_point} >/dev/null
-	}
-
-	install_dmg "Facter" "http://downloads.puppetlabs.com/mac/facter-1.7.1.dmg"
-	install_dmg "Puppet" "http://downloads.puppetlabs.com/mac/puppet-3.2.1.dmg"
 
 else
 	echo 'Your operating system is not supported' 1>&2
