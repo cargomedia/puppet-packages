376e714549c0781e20cdcb6e034e4fb9e94503ca
Reto Kaiser <reto@cargomedia.ch>
2016-04-23 15:56:36 +0200

    Remove not so useful spec

--- modules/bipbip/spec/copperegg/spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'bipbip' do
-
-  describe file('/etc/bipbip/config.yml') do
-    its(:content) { should match /storages:\n\s*-\n\s*name: copperegg\n\s*api_key: foo\n/ }
-  end
-
-end
