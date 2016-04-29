b592d49a8601f2ee71862e828cc6e02621e6d338
Chris Stasiak <me@krzysztof-stasiak.com>
2016-04-29 12:02:32 +0100

    Added specs

---
+++ modules/xorg/spec/config/file/manifest.pp
@@ -0,0 +1,51 @@
+node default {
+
+  xorg::config { 'add new module path 0':
+    section => 'Files',
+    key     => 'ModulePath',
+    value   => '/tmp/0'
+  }
+
+  xorg::config { 'add new module path 0 - duplicate':
+    section => 'Files',
+    key     => 'ModulePath',
+    value   => '/tmp/0'
+  }
+
+  xorg::config { 'add new module path 1':
+    section => 'Files',
+    key     => 'ModulePath',
+    value   => '/tmp/1'
+  }
+
+  xorg::config { 'set device driver':
+    section => 'Device',
+    key     => 'Driver',
+    value   => 'dummy'
+  }
+
+  # HID multitouch
+
+  $xorg_hid_config = '/etc/X11/xorg-multitouch.conf'
+
+  xorg::config { 'set multitouch HID device - definition':
+    section => 'InputClass',
+    key     => 'MatchIsTouchpad',
+    value   => 'true',
+    xorg_config_path => $xorg_hid_config,
+  }
+
+  xorg::config { 'set multitouch HID device = identifier':
+    section => 'InputClass',
+    key     => 'Identifier',
+    value   => 'Multitouch Touchpad',
+    xorg_config_path => $xorg_hid_config,
+  }
+
+  xorg::config { 'set multitouch HID device - driver':
+    section => 'InputClass',
+    key     => 'Driver',
+    value   => 'hid-multitouch',
+    xorg_config_path => $xorg_hid_config,
+  }
+}
