fd8c9c1965d3f1c0157026be2292a7ee35061cc0
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-02 17:55:09 +0200

    Added skeleton class

--- modules/nvidia/manifests/init.pp
+++ modules/nvidia/manifests/init.pp
@@ -1,5 +1,19 @@
 class nvidia {
 
+  require 'apt'
+
+  package { 'nvidia-346':
+    ensure   => present,
+    provider => apt,
+  }
+  ->
+
+  helper::script { 'nvidia: configure X server':
+    content => template("${module_name}/configure_x.sh"),
+    unless  => 'cat /etc/X11/xorg.conf | grep nvidia',
+    require => Package['nvidia-346'],
+  }
+
   @bipbip::entry { 'logparser-nvidia-gpu':
     plugin  => 'log-parser',
     options => {
