3f060103b483c95442a635078225293ed5441c5d
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-03 14:21:34 +0200

    Remove modesetting logic

--- modules/nvidia/templates/configure_x.sh
+++ modules/nvidia/templates/configure_x.sh
@@ -1,24 +1,12 @@
 #!/bin/bash -e
 
-MODESETTING_PATH=/usr/lib/xorg/modules/drivers/modesetting_drv.so
-MODESETTING_BACKUP_PATH=/usr/lib/xorg/modules/drivers/modesetting_drv.so.bak
 XORG_PATH=/etc/X11/xorg.conf
 XORG_BACKUP_PATH=/etc/X11/xorg.conf.puppet-nvidia-module
 
-echo "Step 1: Disable modesetting driver, it breaks Xorg -configure"
-if [ -e $MODESETTING_PATH ]; then
-  mv $MODESETTING_PATH $MODESETTING_BACKUP_PATH
-fi
-
-echo "Step 2: Backup xorg.conf file if exists"
+echo "Step 1: Backup xorg.conf file if exists"
 if [ -e $XORG_PATH ]; then
   mv $XORG_PATH $XORG_BACKUP_PATH
 fi
 
-echo "Step 3: Auto-configure with X's own means"
+echo "Step 2: Auto-configure with X's own means"
 nvidia-xconfig
-
-echo "Step 4: Restore modesetting driver so it doesn't break the updates"
-if [ -e $MODESETTING_BACKUP_PATH ]; then
-  mv $MODESETTING_BACKUP_PATH $MODESETTING_PATH
-fi
