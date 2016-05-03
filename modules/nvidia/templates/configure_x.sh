13348b6e403b070fccc9469f63079b6ecd3885dc
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-03 10:43:09 +0200

    Re-done configure script

--- modules/nvidia/templates/configure_x.sh
+++ modules/nvidia/templates/configure_x.sh
@@ -3,16 +3,26 @@
 # NOTA BENE: this is expected to run AFTER the reboot from apt-get install nvidia-drivers or
 # after removing nouveau modules manually
 
-# unload nouveau module from kernel
+MODESETTING_PATH=/usr/lib/xorg/modules/drivers/modesetting_drv.so
+MODESETTING_BACKUP_PATH=/usr/lib/xorg/modules/drivers/modesetting_drv.so.bak
+XORG_PATH=/etc/X11/xorg.conf
+XORG_BACKUP_PATH=/etc/X11/xorg.conf-puppet-nvidia-module
+
 
 echo "Step 1: Disable modesetting driver, it breaks Xorg -configure"
-mv /usr/lib/xorg/modules/drivers/modesetting_drv.so /usr/lib/xorg/modules/drivers/modesetting_drv.so~
+if [ -e $MODESETTING_PATH ]; then
+  mv $MODESETTING_PATH $MODESETTING_BACKUP_PATH
+fi
 
-echo "Step 2: Auto-configure with X's own means"
-X -configure
+echo "Step 2: Backup xorg.conf file if exists"
+if [ -e $XORG_PATH ]; then
+  mv /etc/X11/xorg.conf /etc/X11/xorg.conf-puppet-nvidia-module
+fi
 
-echo "Step 3: Move the file back to normal place"
-mv -i ~/xorg.conf.new /etc/X11/xorg.conf
+echo "Step 3: Auto-configure with X's own means"
+nvidia-xconfig
 
 echo "Step 4: Restore modesetting driver so it doesn't break the updates"
-mv /usr/lib/xorg/modules/drivers/modesetting_drv.so~ /usr/lib/xorg/modules/drivers/modesetting_drv.so
+if [ -e $MODESETTING_BACKUP_PATH ]; then
+  mv $MODESETTING_BACKUP_PATH $MODESETTING_PATH
+fi
