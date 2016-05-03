e9070ae21c4f1b4b1032f1ef81a9d5a13eb24cbf
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-03 12:07:03 +0200

    Paths fixes

--- modules/nvidia/templates/configure_x.sh
+++ modules/nvidia/templates/configure_x.sh
@@ -6,8 +6,7 @@
 MODESETTING_PATH=/usr/lib/xorg/modules/drivers/modesetting_drv.so
 MODESETTING_BACKUP_PATH=/usr/lib/xorg/modules/drivers/modesetting_drv.so.bak
 XORG_PATH=/etc/X11/xorg.conf
-XORG_BACKUP_PATH=/etc/X11/xorg.conf-puppet-nvidia-module
-
+XORG_BACKUP_PATH=/etc/X11/xorg.conf.puppet-nvidia-module
 
 echo "Step 1: Disable modesetting driver, it breaks Xorg -configure"
 if [ -e $MODESETTING_PATH ]; then
@@ -16,7 +15,7 @@ fi
 
 echo "Step 2: Backup xorg.conf file if exists"
 if [ -e $XORG_PATH ]; then
-  mv /etc/X11/xorg.conf /etc/X11/xorg.conf-puppet-nvidia-module
+  mv $XORG_PATH $XORG_BACKUP_PATH
 fi
 
 echo "Step 3: Auto-configure with X's own means"
