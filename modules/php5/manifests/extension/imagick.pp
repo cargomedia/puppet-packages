3dc52f428bc6553a2d9c7c2b55d038180f129294
ppp0 <philipp.gysin@cargomedia.ch>
2016-05-04 18:33:25 +0200

    Restrict policy for IMagemagick

--- modules/php5/manifests/extension/imagick.pp
+++ modules/php5/manifests/extension/imagick.pp
@@ -18,15 +18,25 @@ class php5::extension::imagick (
       content => template("${module_name}/extension/imagick/install.sh"),
       unless  => "php --re imagick | grep -w 'imagick version ${version}'",
       require => Class['php5'],
-      before => Php5::Config_extension['imagick'],
+      before  => Php5::Config_extension['imagick'],
     }
-
+    $imagemagick_dir = '/etc/ImageMagick'
   } else {
+
     package { 'php5-imagick':
       ensure   => present,
       provider => 'apt',
-      before => Php5::Config_extension['imagick'],
+      before   => Php5::Config_extension['imagick'],
     }
+    $imagemagick_dir = '/etc/ImageMagick-6'
+  }
+
+  file { "$imagemagick_dir/policy.xml":
+    ensure  => file,
+    content => template("${module_name}/extension/imagick/policy.xml"),
+    owner   => '0',
+    group   => '0',
+    mode    => '0644',
   }
 
   php5::config_extension { 'imagick':
