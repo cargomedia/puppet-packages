09e877a8b09f87f5ff2fc1fc9b05cf1857f6dd23
ppp0 <philipp.gysin@cargomedia.ch>
2016-05-04 15:52:40 +0200

    Introduce some retry logic

--- modules/phantomjs/templates/install.sh
+++ modules/phantomjs/templates/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -e
 
 PHANTOMJS='phantomjs-<%= @version %>-linux-x86_64'
-curl -L https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOMJS}.tar.bz2 > ${PHANTOMJS}
-tar xjf ${PHANTOMJS}
+PHANTOMJS_URL='https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOMJS}.tar.bz2'
+
+function download_and_unpack_phantomjs {
+    curl --fail --retry 5 -sL ${PHANTOMJS_URL} > ${PHANTOMJS}
+    tar xjf ${PHANTOMJS}
+}
+
+#try three times to successfully execute the function
+(tries=3; while ! (download_and_unpack_phantomjs); do ((--tries)) || exit; sleep 5; done)
+
 mv ${PHANTOMJS} phantomjs
 rm -rf /usr/local/share/phantomjs
 mv phantomjs /usr/local/share/
