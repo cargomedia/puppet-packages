0f29041714ce9293f33f9e173ac632971be879a7
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-12 10:49:54 +0100

    added pulseaudio service

--- modules/pulseaudio/manifests/init.pp
+++ modules/pulseaudio/manifests/init.pp
@@ -1,7 +1,26 @@
-class pulseaudio {
+class pulseaudio (
+  $client_autospawn = false,
+  $user = 'performer',
+  $daemon_binary = '/usr/bin/pulseaudio',
+  $daemon_daemonize = false,
+){
+
+  user { $user:
+    ensure => present,
+  }
 
   package { 'pulseaudio':
     ensure   => installed,
     provider => apt,
+  }
+
+  daemon { "pulseaudio-${user}":
+    binary            => $daemon_binary,
+    args              => "--start --daemonize=${daemon_daemonize}",
+    user              => 'root',
+    require           => [
+      Package['pulseaudio'],
+      User[$user],
+    ]
   }
 }
