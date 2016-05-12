6c4e88fdeb39d83b17bfa9d2420cae1318b6ffa6
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-12 12:19:43 +0100

    Extended specs

--- modules/pulseaudio/spec/default/spec.rb
+++ modules/pulseaudio/spec/default/spec.rb
@@ -6,4 +6,14 @@ describe 'pulseaudio' do
     it { should be_installed }
   end
 
+  describe service('pulseaudio-dj') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe command('sudo -u dj -H sh -c "pacmd list-cards"') do
+    its(:stdout) { should match /0 card\(s\) available./ }
+    its(:exit_status) { should eq 0 }
+  end
+
 end
