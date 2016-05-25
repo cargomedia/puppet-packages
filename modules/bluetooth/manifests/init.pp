05d62e18704444533e073c18a24a07f41d33a189
Chris Stasiak <me@krzysztof-stasiak.com>
2016-05-25 22:02:24 +0200

    Fixed file permission

--- modules/bluetooth/manifests/init.pp
+++ modules/bluetooth/manifests/init.pp
@@ -12,7 +12,7 @@ class bluetooth (
   file { '/etc/bluetooth/audio.conf':
     ensure  => file,
     content => template("${module_name}/audio.conf"),
-    mode    => '0655',
+    mode    => '0644',
     owner   => '0',
     notify  => Service['bluetooth'],
     before  => Service['bluetooth'],
