ddecd9c82a720453ad6060ff4a97c5b4f61850cf
ppp0 <philipp.gysin@cargomedia.ch>
2016-06-07 16:34:51 +0200

    Modify systemd:unit to always exec start and enable on refresh

--- modules/systemd/manifests/unit.pp
+++ modules/systemd/manifests/unit.pp
@@ -11,13 +11,21 @@ define systemd::unit(
     owner   => '0',
     group   => '0',
     mode    => '0644',
-    notify => Exec['systemctl daemon-reload'],
+    notify  => [
+      Exec['systemctl daemon-reload'],
+      Exec["systemctl start ${name}"],
+      Exec["systemctl enable ${name}"],
+    ]
   }
-  ~>
 
   exec { "systemctl start ${name}":
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     unless      => "systemctl status ${name}",
+    refreshonly => true,
+  }
+
+  exec { "systemctl enable ${name}":
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     refreshonly => true,
   }
 
