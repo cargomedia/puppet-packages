c28732af98a5912b6ef83fab081961845c1383a9
ppp0 <philipp.gysin@cargomedia.ch>
2016-06-09 18:21:41 +0200

    Use latest vagrant, latest bundler

--- modules/ruby/spec/gem/bundler/spec.rb
+++ modules/ruby/spec/gem/bundler/spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 describe 'ruby::gem::bundler' do
 
   describe package('bundler') do
-    it { should be_installed.by('gem').with_version('1.6.4') }
+    it { should be_installed.by('gem') }
   end
 
   describe command('bundle --version') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match 'Bundler version' }
   end
 end
--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -1,7 +1,7 @@
 class vagrant {
 
-  $version = '1.7.3'
-  $url = 'https://releases.hashicorp.com/vagrant/1.7.3/vagrant_1.7.3_x86_64.deb'
+  $version = '1.8.1'
+  $url = 'https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb'
 
   require 'apt'
 
