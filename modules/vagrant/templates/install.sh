17d863fcc4b5d4ebc2e0e20c121c01e113c004e7
ppp0 <philipp.gysin@cargomedia.ch>
2016-06-14 13:24:43 +0200

    Remove vagrant.d directory (plugins etc)

--- modules/vagrant/templates/install.sh
+++ modules/vagrant/templates/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
 curl -sL "<%= @url %>" > vagrant.deb
+for i in $(find /usr /opt /home /var -ipath '*/.vagrant.d'); do
+    rm -rf "${i}"
+done
 dpkg -i --force-confold vagrant.deb
 ln -sf /opt/vagrant/bin/vagrant /usr/local/bin/vagrant
