7016af4a834e1b20a936e26d58006516f10745db
ppp0 <philipp.gysin@cargomedia.ch>
2016-06-14 15:37:06 +0200

    Remove all folder/files except boxes

--- modules/vagrant/templates/install.sh
+++ modules/vagrant/templates/install.sh
@@ -2,7 +2,11 @@
 
 curl -sL "<%= @url %>" > vagrant.deb
 for i in $(find /usr /opt /home /var -ipath '*/.vagrant.d'); do
-    rm -rf "${i}"
+    for j in $(ls -1 "${i}"); do
+        if [ "${j}" != 'boxes' ]; then
+            rm -rf "${i}/${j}"
+        fi
+    done
 done
 dpkg -i --force-confold vagrant.deb
 ln -sf /opt/vagrant/bin/vagrant /usr/local/bin/vagrant
