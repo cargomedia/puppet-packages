3ab7bfbc66aafc36c10564df71c97d278d637ed2
Chris Stasiak <me@krzysztof-stasiak.com>
2016-06-15 12:34:38 +0100

    Made spec more robust - stop lightdm

--- modules/pulseaudio/spec/service_post_command/2-restart.pp
+++ modules/pulseaudio/spec/service_post_command/2-restart.pp
@@ -1,5 +1,10 @@
 node default {
 
+  exec { 'systemctl stop lightdm':
+    path => ['/bin', '/usr/bin', '/usr/local/bin'],
+  }
+  ->
+
   exec { 'systemctl restart pulseaudio-dj':
     path => ['/bin', '/usr/bin', '/usr/local/bin'],
   }
