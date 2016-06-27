96042426f7bf462736ab5ddc152de036d0e22b53
ppp0 <philipp.gysin@cargomedia.ch>
2016-06-27 15:12:14 +0200

    Refactor generation of etc/default/locale

--- modules/locale/manifests/init.pp
+++ modules/locale/manifests/init.pp
@@ -1,9 +1,25 @@
 class locale($lang = 'en_US.UTF-8') {
 
-  file { '/etc/default/locale':
-    content => template("${module_name}/locale.erb"),
-    owner   => '0',
-    group   => '0',
-    mode    => '0644',
+  exec { "Uncomment ${lang} if needed":
+    provider    => shell,
+    command     => "sed -i '/${lang}/s/^# //g' /etc/locale.gen",
+    unless      => "grep -E '^${lang}' /etc/locale.gen",
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  }
+  ~>
+
+  exec { "Generate locale for ${lang}":
+    provider    => shell,
+    command     => "locale-gen ${lang}",
+    refreshonly => true,
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  }
+  ~>
+
+  exec { "Update locale for ${lang}":
+    provider    => shell,
+    command     => "update-locale LANG='${lang}'",
+    refreshonly => true,
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
 }
