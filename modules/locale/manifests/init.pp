82faf37fc653feaf1ad61248ac43faf8be4db844
ppp0 <philipp.gysin@cargomedia.ch>
2016-06-27 16:25:08 +0200

    Consider platform differences (Wheezy exception)

--- modules/locale/manifests/init.pp
+++ modules/locale/manifests/init.pp
@@ -1,17 +1,18 @@
 class locale($lang = 'en_US.UTF-8') {
 
-  exec { "Uncomment ${lang} if needed":
-    provider    => shell,
-    command     => "sed -i '/${lang}/s/^# //g' /etc/locale.gen",
-    unless      => "grep -E '^${lang}' /etc/locale.gen",
-    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  if ($::lsbdistcodename == 'Wheezy') {
+    exec { "Uncomment ${lang} if needed":
+      provider => shell,
+      command  => "sed -i '/${lang}/s/^# //g' /etc/locale.gen",
+      unless   => "grep -qE '^${lang}' /etc/locale.gen",
+      path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    }
   }
-  ~>
 
   exec { "Generate locale for ${lang}":
     provider    => shell,
     command     => "locale-gen ${lang}",
-    refreshonly => true,
+    unless      => "grep -qE '^LANG=${lang}$' /etc/default/locale",
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   }
   ~>
