0c92cdcc4db295e1d70e0c2c7072d5aff6d8970d
Chris Stasiak <me@krzysztof-stasiak.com>
2016-06-29 16:47:44 +0100

    Removed not needed roles; upgraded rtpbroadcast role

--- modules/janus/manifests/role/rtpbroadcast.pp
+++ modules/janus/manifests/role/rtpbroadcast.pp
@@ -1,19 +1,24 @@
-define janus::role::streaming::cluster_member (
+define janus::role::rtpbroadcast (
   $hostname,
-  $origin = false,
-  $repeater = false,
-  $edge = false,
   $bind_address = undef,
+  $nat_1_1_mapping = undef,
   $rtp_port_range_min = 20000,
   $rtp_port_range_max = 25000,
-  $nat_1_1_mapping = undef,
   $core_dump = true,
   $http_port = 8300,
+  $http_base_path = '/janus',
   $ws_port = 8310,
   $rtpb_minport = 8000,
   $rtpb_maxport = 9000,
-  $rtpb_keyframe_distance_alert = 600,
+  $recording_enabled = false,
+  $recording_pattern = 'rec-#{id}-#{time}-#{type}',
+  $thumbnailing_pattern = 'thum-#{id}-#{time}-#{type}',
+  $thumbnailing_interval = 60,
+  $thumbnailing_duration = 10,
+  $job_pattern = 'job-#{md5}',
+  $jobs_path = undef,
   $rest_url = undef,
+  $mountpoint_info_interval = 10,
 ) {
 
   include 'janus::cluster'
@@ -30,6 +35,7 @@ define janus::role::streaming::cluster_member (
   janus::transport::http { $name:
     prefix            => $janus::cluster::prefix,
     port              => $http_port,
+    http_base_path    => $http_base_path
   }
 
   janus::transport::websockets { $name:
@@ -44,17 +50,14 @@ define janus::role::streaming::cluster_member (
     prefix                   => $janus::cluster::prefix,
     minport                  => $rtpb_minport,
     maxport                  => $rtpb_maxport,
-    source_avg_time          => $rtpb_source_avg_time,
-    recording_enabled        => false,
+    mountpoint_info_interval => $mountpoint_info_interval,
+    recording_enabled        => $recording_enabled,
+    thumbnailing_duration    => $thumbnailing_duration,
+    thumbnailing_interval    => $thumbnailing_interval,
+    thumbnailing_pattern     => $thumbnailing_pattern,
+    recording_pattern        => $recording_pattern,
+    job_pattern              => $job_pattern,
+    jobs_path                => $jobs_path,
     rest_url                 => $rest_url_final,
-  }
-
-  if $edge or $repeater or $origin {
-
-  # CLUSTER ROLE REGISTER CURL
-    janus::role::streaming::cluster_register { $title:
-      url     => '',
-      content => '{"type" => "repeater"}'
-    }
   }
 }
