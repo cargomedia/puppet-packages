4ae24e40ea615beed0e13dea48b55ec26d8a841f
ppp0 <philipp.gysin@cargomedia.ch>
2016-07-05 16:51:08 +0200

    Make lint happy

--- modules/sysvinit/manifests/script.pp
+++ modules/sysvinit/manifests/script.pp
@@ -6,8 +6,8 @@ define sysvinit::script(
   include 'sysctl::entry::core_pattern'
 
   $unless_start_on_create = $start_on_create ? {
-    false => 'true',
-    default => 'false',
+    false => true,
+    default => false,
   }
 
   file { "/etc/init.d/${name}":
@@ -19,7 +19,7 @@ define sysvinit::script(
   }
   ~>
 
-  # The unless clause will evaluate to *true* (do not execute) if $start_on_create is false
+    # The unless clause will evaluate to *true* (do not execute) if $start_on_create is false
   exec { "/etc/init.d/${name} start":
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     unless      => "${unless_start_on_create} || /etc/init.d/${name} status",
