ea73d4afb817740c3030a16056f60c42f765d994
ppp0 <philipp.gysin@cargomedia.ch>
2016-07-06 17:39:38 +0200

    Remove wowza

--- modules/wowza/manifests/jar/flexjson.pp
+++
@@ -1,9 +0,0 @@
-class wowza::jar::flexjson($version = '2.1') {
-
-  require 'wowza'
-
-  wowza::jar { 'flexjson':
-    get_command => "curl -sL http://central.maven.org/maven2/net/sf/flexjson/flexjson/${version}/flexjson-${version}.jar",
-    version     => $version,
-  }
-}
