54ab523652628588662579717a9954746fa4411e
Philipp Pfaff <ppp0@users.noreply.github.com>
2016-07-26 15:18:32 +0200

    Revert "Optionally prevent autostart of daemons at creation time"

--- modules/systemd/manifests/unit.pp
+++ modules/systemd/manifests/unit.pp
@@ -1,15 +1,9 @@
 define systemd::unit(
   $content,
-  $start_on_create = true,
 ) {
 
   require 'systemd'
   include 'systemd::daemon_reload'
-
-  $unless_start_on_create = $start_on_create ? {
-    false => true,
-    default => false,
-  }
 
   file { "/etc/systemd/system/${name}.service":
     ensure  => file,
@@ -17,14 +11,13 @@ define systemd::unit(
     owner   => '0',
     group   => '0',
     mode    => '0644',
-    notify  => Exec['systemctl daemon-reload'],
+    notify => Exec['systemctl daemon-reload'],
   }
   ~>
 
-    # The unless clause will evaluate to *true* (do not execute) if $start_on_create is false
   exec { "systemctl start ${name}":
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    unless      => "${unless_start_on_create} || systemctl status ${name}",
+    unless      => "systemctl status ${name}",
     refreshonly => true,
   }
 
