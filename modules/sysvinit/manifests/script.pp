e04ef7cd98eb52581d5cc07b729326803430aa9f
Philipp Pfaff <ppp0@users.noreply.github.com>
2016-07-26 15:18:32 +0200

    Revert "Optionally prevent autostart of daemons at creation time"

--- modules/sysvinit/manifests/script.pp
+++ modules/sysvinit/manifests/script.pp
@@ -1,14 +1,8 @@
 define sysvinit::script(
   $content,
-  $start_on_create = true,
 ) {
 
   include 'sysctl::entry::core_pattern'
-
-  $unless_start_on_create = $start_on_create ? {
-    false => true,
-    default => false,
-  }
 
   file { "/etc/init.d/${name}":
     ensure  => file,
@@ -19,10 +13,9 @@ define sysvinit::script(
   }
   ~>
 
-    # The unless clause will evaluate to *true* (do not execute) if $start_on_create is false
   exec { "/etc/init.d/${name} start":
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    unless      => "${unless_start_on_create} || /etc/init.d/${name} status",
+    unless      => "/etc/init.d/${name} status",
     refreshonly => true,
   }
 
