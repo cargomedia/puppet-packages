329d1dd2440bde6f3019e57267844430467f77a3
Chris Stasiak <me@krzysztof-stasiak.com>
2016-07-28 16:03:01 +0100

    Made needrestart more generic; Removed double service exists check;

--- modules/needrestart/manifests/init.pp
+++ modules/needrestart/manifests/init.pp
@@ -1,6 +1,16 @@
-class needrestart {
+class needrestart (
+  $restart_helper_path = '/usr/local/bin/needrestart-service'
+){
 
   package { 'needrestart':
     provider => apt
+  }
+
+  file { $restart_helper_path:
+    ensure  => file,
+    content => template("${module_name}/needrestart.sh.erb"),
+    owner   => '0',
+    group   => '0',
+    mode    => '0755',
   }
 }
--- modules/needrestart/manifests/service.pp
+++ modules/needrestart/manifests/service.pp
@@ -4,19 +4,7 @@ define needrestart::service (
 
   require 'needrestart'
 
-  $service_needrestart_helper = "/usr/local/bin/needrestart-${service_name}"
-
-  file { $service_needrestart_helper:
-    ensure  => file,
-    content => template("${module_name}/needrestart.sh.erb"),
-    owner   => '0',
-    group   => '0',
-    mode    => '0755',
-  }
-  ->
-
   apt::config { "999-needrestart-service-${service_name}":
-    content => "DPkg::Post-Invoke {'${service_needrestart_helper}';};\n"
+    content => "DPkg::Post-Invoke {'${needrestart::restart_helper_path} ${service_name}';};\n"
   }
-
 }
