b388d227e9c8bd03a392c6551a27b051c6c6428b
Chris Stasiak <me@krzysztof-stasiak.com>
2016-08-02 11:30:25 +0100

    Added switch for Wheezy and Jessie as behaves differently

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -6,10 +6,24 @@ class cron {
     ensure   => present,
     provider => 'apt',
   }
-  ->
 
-  daemon { 'cron':
-    binary => '/usr/sbin/cron',
-    args   => '-f',
+  # see https://github.com/cargomedia/puppet-packages/pull/1414#issuecomment-236863404
+  if $::facts['lsbdistcodename'] == 'wheezy' {
+
+    service { 'cron':
+      enable => true,
+    }
+
+    @monit::entry { 'cron':
+      content => template("${module_name}/monit"),
+      require => Service['cron'],
+    }
+
+  } else {
+
+    daemon { 'cron':
+      binary => '/usr/sbin/cron',
+      args   => '-f',
+    }
   }
 }
--- modules/cron/spec/monit/spec.rb
+++ modules/cron/spec/monit/spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'cron::monit' do
   describe command('monit summary') do
-    its(:stdout) { should match /crond.*[Running|Status ok]/ }
+    its(:stdout) { should match /cron.*[Running|Status ok]/ }
   end
 end
