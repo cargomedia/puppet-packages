e69ca3d275f1c5849248cda216a3ba40eaa1147b
ppp0 <philipp.gysin@cargomedia.ch>
2016-08-03 10:24:00 +0200

    Modify specs to accomodate for systemd

--- modules/mysql/spec/server-no-passwd/spec.rb
+++ modules/mysql/spec/server-no-passwd/spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe 'mysql::server no password' do
 
-  describe command('/etc/init.d/mysql status') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match 'Uptime' }
-  end
-
   describe command('mysql -e "show status"') do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should match 'Uptime' }
