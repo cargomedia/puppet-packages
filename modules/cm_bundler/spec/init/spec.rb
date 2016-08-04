e89914809857e5ab22fe91a3d861e5806bc8a81a
Michel Meyer <michel.meyer@cargomedia.ch>
2016-08-04 14:12:50 +0200

    Fix spec

--- modules/cm_bundler/spec/init/spec.rb
+++ modules/cm_bundler/spec/init/spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'cm_bundler' do
 
-  describe package('cm_bundler') do
+  describe package('cm-bundler') do
     it { should be_installed.by('npm') }
   end
 
-  describe command('cm_bundler --version') do
+  describe command('cm-bundler --version') do
     its(:exit_status) { should eq 0 }
   end
 end
