a050de2012f18b975f49e826a0b95562517929a5
Chris Stasiak <me@krzysztof-stasiak.com>
2016-08-15 17:57:59 +0100

    Re-added default sysVinit support + custom systemD support

--- modules/memcached/manifests/init.pp
+++ modules/memcached/manifests/init.pp
@@ -12,17 +12,42 @@ class memcached (
     provider => 'apt',
   }
 
-  exec { 'true && /etc/init.d/memcached stop':
-    path         => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    provider     => shell,
-    subscribe    => Package['memcached'],
-    refreshonly  => true,
-  }
+  if $::facts['lsbdistcodename'] == 'wheezy' {
 
-  daemon { 'memcached':
-    binary  => '/usr/bin/memcached',
-    args    => "-p ${port} -m ${memory} -u ${user} -c ${max_connections} -v",
-    require => Package['memcached'],
+    service { 'memcached':
+      enable  => true,
+      require => Package['memcached'],
+    }
+
+    file { '/etc/memcached.conf':
+      ensure  => file,
+      content => template("${module_name}/memcached.conf"),
+      owner   => '0',
+      group   => '0',
+      mode    => '0644',
+      notify  => Service['memcached'],
+    }
+
+    @monit::entry { 'memcached':
+      content => template("${module_name}/monit"),
+      require => Service['memcached'],
+    }
+
+  } else {
+
+    exec { 'true && /etc/init.d/memcached stop':
+      path         => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+      provider     => shell,
+      subscribe    => Package['memcached'],
+      refreshonly  => true,
+    }
+
+    daemon { 'memcached':
+      binary  => '/usr/bin/memcached',
+      args    => "-p ${port} -m ${memory} -u ${user} -c ${max_connections} -v",
+      require => Package['memcached'],
+    }
+
   }
 
   @bipbip::entry { 'memcached':
