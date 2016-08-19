3807ab3f46cce28644a9cfd5fb37952a561efb01
Chris Stasiak <me@krzysztof-stasiak.com>
2016-08-19 17:23:30 +0100

    Freeze capistrano version to 3.4.1 for Wheezy

--- modules/capistrano/manifests/init.pp
+++ modules/capistrano/manifests/init.pp
@@ -1,11 +1,13 @@
 class capistrano {
 
+  $version = ($::facts['lsbdistcodename'] == 'wheezy') ? { true => '3.4.1', default => present }
+
   ruby::gem { 'net-ssh':
     ensure => '2.8.0',
   }
   ->
 
   ruby::gem { 'capistrano':
-    ensure => present,
+    ensure => $version,
   }
 }
