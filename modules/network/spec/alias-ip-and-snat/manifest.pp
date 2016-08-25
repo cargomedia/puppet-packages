62abb7ace473c7c73abf58b34afdc7dd128bb84f
ppp0 <philipp.gysin@cargomedia.ch>
2016-08-25 19:42:09 +0200

    Final version of specs

--- modules/network/spec/alias-ip-and-snat/manifest.pp
+++ modules/network/spec/alias-ip-and-snat/manifest.pp
@@ -8,7 +8,7 @@ node default {
   }
 
   exec { 'Start listening on 1337':
-    command     => 'nc -lvnp 1337 -s 10.10.20.122 2>/tmp/stderr_output &',
+    command     => 'nc -lvnp 1337 2>/tmp/stderr_output &',
     user        => 'root',
     path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
     provider    => shell,
