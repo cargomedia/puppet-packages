14bdc8988a0de468666ddf7a7a8e0284a7169688
ppp0 <philipp.gysin@cargomedia.ch>
2016-08-25 18:56:37 +0200

    Corrected silly error on specs

--- modules/network/spec/alias-ip-and-snat/spec.rb
+++ modules/network/spec/alias-ip-and-snat/spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'network::alias_ip_and_snat' do
 
-  describe command('nc -lvnp 1337 -s 10.10.20.122 2>/tmp/stderr_output &') do
+  describe command ('nc -lvnp 1337 -s 10.10.20.122 2>/tmp/stderr_output &')  do
     its(:exit_status) { should eq 0}
   end
 
-  describe command('curl --proxy '' --max-time 1 http://10.10.20.122:1337')  do
+  describe command('curl --proxy "" --max-time 1 http://10.10.20.122:1337')  do
     its(:exit_status) { should eq 28}
   end
 
