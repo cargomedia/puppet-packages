7959ce99c3e8065de41b717d3c232fd718971ea7
Tomasz Durka <tomasz@durka.pl>
2016-09-02 14:59:20 +0200

    Create proper non-critical_unit for critical_units

--- modules/systemd/spec/critical_units/spec.rb
+++ modules/systemd/spec/critical_units/spec.rb
@@ -10,7 +10,15 @@ describe 'systemd::critical_units' do
     it { should be_symlink }
   end
 
+  describe file('/etc/systemd/system/critical-units.target.wants/critical-units.target') do
+    it { should_not exist}
+  end
+
   describe command('systemctl list-dependencies --plain critical-units.target | grep my-daemon.service') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('systemctl is-active critical-units.target') do
     its(:exit_status) { should eq 0 }
   end
 end
