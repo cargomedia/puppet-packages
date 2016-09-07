d74135227adc04c4f2c2c5510b1115ff0911fa1a
Chris Stasiak <me@krzysztof-stasiak.com>
2016-09-07 15:54:11 +0200

    Simplified specs

--- modules/apt/spec/preferences/manifest.pp
+++ modules/apt/spec/preferences/manifest.pp
@@ -1,16 +1,7 @@
 node default {
 
-  $version = 'version 8:6.8.9.9-5+deb8u2'
-
   apt::preferences { 'imagemagick-common':
-    pin => $version,
-  }
-
-  apt::preferences { 'libmagickcore-6.q16-2':
-    pin => $version,
-  }
-
-  apt::preferences { 'libmagickwand-6.q16-2':
-    pin => $version,
+    pin          => 'version 8:6.8.9.9-5+deb8u2',
+    pin_priority => 1000,
   }
 }
--- modules/apt/spec/preferences/spec.rb
+++ modules/apt/spec/preferences/spec.rb
@@ -3,19 +3,9 @@ require 'spec_helper'
 describe 'apt::preferences' do
 
   describe file('/etc/apt/preferences.d/imagemagick-common.pref') do
-    its(:content) { should match // }
+    its(:content) { should match /^Package: imagemagick-common$/ }
+    its(:content) { should match /^Pin: version 8:6.8.9.9-5+deb8u2$/ }
+    its(:content) { should match /^Pin-Priority: 1000$/ }
   end
 
-  describe file('/etc/apt/preferences.d/libmagickcore-6.q16-2.pref') do
-    its(:content) { should match // }
-  end
-
-  describe file('/etc/apt/preferences.d/libmagickwand-6.q16-2.pref') do
-    its(:content) { should match // }
-  end
-
-  describe command('sudo apt-get -y install imagemagick') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /Unpacking imagemagick-common \(8:6\.8\.9\.9-5\+deb8u2\)/}
-  end
 end
