fa81fc00791e441e187715af9884d14a18d2c739
Chris Stasiak <me@krzysztof-stasiak.com>
2016-09-07 16:05:13 +0200

    Fixed regexp

--- modules/apt/spec/preferences/spec.rb
+++ modules/apt/spec/preferences/spec.rb
@@ -4,7 +4,7 @@ describe 'apt::preferences' do
 
   describe file('/etc/apt/preferences.d/imagemagick-common.pref') do
     its(:content) { should match /^Package: imagemagick-common$/ }
-    its(:content) { should match /^Pin: version 8:6.8.9.9-5+deb8u2$/ }
+    its(:content) { should match /^Pin: version 8:6.8.9.9\-5\+deb8u2$/ }
     its(:content) { should match /^Pin-Priority: 1000$/ }
   end
 
