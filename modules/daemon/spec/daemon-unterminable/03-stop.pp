b2a6f50344879c9a13e18d629aac5c7599cc5552
ppp0 <philipp.gysin@cargomedia.ch>
2016-09-29 12:17:20 +0200

    Remove sysvinit and deps (in daemon)

--- modules/daemon/spec/daemon-unterminable/03-stop.pp
+++ modules/daemon/spec/daemon-unterminable/03-stop.pp
@@ -1,14 +1,7 @@
 node default {
 
-  if ($::facts['service_provider'] == 'debian') {
-    exec { 'stop my-program':
-      command => '/etc/init.d/my-program stop || echo "stop failed" >> /tmp/spec-log'
-    }
-  }
-  if ($::facts['service_provider'] == 'systemd') {
-    service { 'my-program':
-      ensure   => 'stopped',
-    }
+  service { 'my-program':
+    ensure   => 'stopped',
   }
 
 }
--- modules/daemon/spec/daemon-unterminable/spec.rb
+++ modules/daemon/spec/daemon-unterminable/spec.rb
@@ -2,14 +2,6 @@ require 'spec_helper'
 
 describe 'daemon' do
 
-  # With *sysvinit* we don't KILL the program if it doesn't respond to TERM
-  check_initv = [
-    'pgrep -f "^/bin/bash /tmp/my-program$"',
-    'pgrep -f "^/bin/bash /tmp/my-program-child$"',
-    '/etc/init.d/my-program status',
-    'grep "stop failed" /tmp/spec-log',
-  ].join(' && ')
-
   # With *systemd* we KILL the whole process cgroup if the process doesn't respond to TERM
   check_systemd = [
     '! pgrep -f "^/bin/bash /tmp/my-program$"',
@@ -17,7 +9,7 @@ describe 'daemon' do
     'systemctl status my-program | grep -q "Active: failed (Result: signal)"',
   ].join(' && ')
 
-  describe command("if (test -e /bin/systemctl); then #{check_systemd}; else #{check_initv}; fi") do
+  describe command("#{check_systemd}") do
     its(:exit_status) { should eq 0 }
   end
 
