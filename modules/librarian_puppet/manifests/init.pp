a5ada4a9fb9fcf0d2185b14f6e998d5cc8384dd2
Chris Stasiak <me@krzysztof-stasiak.com>
2016-09-30 12:14:15 +0200

    Reverted librarian_puppet regression

--- modules/librarian_puppet/manifests/init.pp
+++ modules/librarian_puppet/manifests/init.pp
@@ -3,12 +3,6 @@ class librarian_puppet(
 ) {
 
   # Workaround for https://github.com/rodjek/librarian-puppet/issues/330
-  package { 'libssl1.0.0':
-    ensure => latest,
-    provider => apt,
-  }
-  ->
-
   ruby::gem { 'activesupport':
     ensure => '4.2.5',
   }
