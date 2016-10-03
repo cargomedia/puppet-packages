1ea22be6fedfca501f2e825e1d10c027c0860e20
ppp0 <philipp.gysin@cargomedia.ch>
2016-10-03 15:11:42 +0200

    Modify installed user, modify postinstall service stop handling, spec test

--- modules/memcached/manifests/init.pp
+++ modules/memcached/manifests/init.pp
@@ -2,7 +2,6 @@ class memcached (
   $bind_ip = '0.0.0.0',
   $port = 11211,
   $memory = 2048,
-  $user = 'nobody',
   $max_connections = 10000
 ) {
 
@@ -13,18 +12,28 @@ class memcached (
     provider => 'apt',
   }
 
-  exec { 'true && /etc/init.d/memcached stop':
-    path         => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    provider     => shell,
-    subscribe    => Package['memcached'],
-    before       => Daemon['memcached'],
-    refreshonly  => true,
+  exec { 'systemctl stop memcached;systemctl disable memcached':
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    provider    => shell,
+    require     => Package['memcached'],
+    before      => [File['/etc/init.d/memcached'], Daemon['memcached']],
   }
+
+  file { '/etc/init.d/memcached':
+    ensure  => file,
+    content => template("${module_name}/init-replacement"),
+    mode    => '0755',
+    owner   => 0,
+    group   => 0;
+  }
+
+  $user = 'memcache'
 
   daemon { 'memcached':
     binary  => '/usr/bin/memcached',
     args    => "-p ${port} -m ${memory} -u ${user} -l ${bind_ip} -c ${max_connections} -v",
     require => Package['memcached'],
+    user    => $user,
   }
 
   @bipbip::entry { 'memcached':
