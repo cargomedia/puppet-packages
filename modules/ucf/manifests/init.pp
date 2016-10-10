2d9e2e74b7c45ffc9041c9dbeb0b525a65894117
ppp0 <philipp.gysin@cargomedia.ch>
2016-10-10 17:24:56 +0200

    Introduce module ucf.conf

---
+++ modules/ucf/manifests/init.pp
@@ -0,0 +1,16 @@
+class ucf {
+
+  require 'apt'
+
+  file { '/etc/ucf.conf':
+    ensure  => file,
+    content => template("${module_name}/ucf.conf"),
+    owner   => '0',
+    group   => '0',
+    mode    => '0644',
+    before  => Package['ucf'],
+  }
+
+  package { 'ucf': }
+
+}
