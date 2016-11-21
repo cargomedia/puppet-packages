5df34c69ff6f32d95895723f30950146a410bbb0
Krzysztof Stasiak <me@krzysztof-stasiak.com>
2016-11-21 11:21:01 +0000

    Spec formatting

--- modules/copperegg_revealcloud/spec/tags-from-facts/manifest.pp
+++ modules/copperegg_revealcloud/spec/tags-from-facts/manifest.pp
@@ -3,6 +3,7 @@ node default {
   class { 'copperegg_revealcloud':
     api_key     => 'my_key',
     label       => 'foo',
+    tags        => ['custom1', 'custom2'],
     enable_node => false,
   }
 }
--- modules/copperegg_revealcloud/spec/tags-from-facts/spec.rb
+++ modules/copperegg_revealcloud/spec/tags-from-facts/spec.rb
@@ -4,10 +4,12 @@ describe 'copperegg_revealcloud' do
 
   describe process('revealcloud') do
     it { should be_running }
-    its(:args) { should match '-t tag1' }
-    its(:args) { should match '-t tag2' }
-    its(:args) { should match '-t service1' }
-    its(:args) { should match '-t service2' }
+    its(:args) { should match '-t custom1' }
+    its(:args) { should match '-t custom2' }
+    its(:args) { should match '-t hiera1' }
+    its(:args) { should match '-t hiera2' }
+    its(:args) { should match '-t facts1' }
+    its(:args) { should match '-t facts2' }
   end
 
 end
