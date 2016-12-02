ec4c1fa8235f9a6e25b387acb74f102af361b29f
Reto Kaiser <reto@cargomedia.ch>
2016-12-02 18:01:35 +0100

    Install nginx from jessie-backports with OpenSSL 1.02

--- modules/nginx/manifests/package.pp
+++ modules/nginx/manifests/package.pp
@@ -2,27 +2,56 @@ class nginx::package {
 
   require 'apt'
 
-  $distro_family = $::facts['lsbdistid'] ? {
-    'Ubuntu' => 'ubuntu',
-    default => 'debian',
-  }
+  if ($::facts['lsbdistcodename'] == 'jessie') {
 
-  apt::source { 'nginx':
-    entries => [
-      "deb http://nginx.org/packages/${distro_family}/ ${::facts['lsbdistcodename']} nginx",
-      "deb-src http://nginx.org/packages/${distro_family}/ ${::facts['lsbdistcodename']} nginx"
-    ],
-    keys    => {
-      'nginx' => {
-        key     => '7BD9BF62',
-        key_url => 'http://nginx.org/keys/nginx_signing.key',
+    # Jessie has OpenSSL 1.0.1, but we need 1.0.2 for Chrome
+    # Workaround is to install nginx from backports which is built against OpenSSL 1.0.2
+
+    require 'apt::source::backports'
+
+    apt::preference { [
+      'nginx-full',
+      'nginx-common',
+      'libssl1.0.0',
+    ]:
+      pin          => 'release a=jessie-backports',
+      pin_priority => 1000,
+      before       => Package['nginx-full'],
+    }
+    ->
+
+    package { 'nginx-full':
+      ensure   => present,
+      provider => 'apt',
+      require  => User['nginx'],
+    }
+
+    user { 'nginx':
+      ensure     => present,
+      system     => true,
+    }
+
+  } else {
+
+    apt::source { 'nginx':
+      entries => [
+        "deb http://nginx.org/packages/debian/ ${::facts['lsbdistcodename']} nginx",
+        "deb-src http://nginx.org/packages/debian/ ${::facts['lsbdistcodename']} nginx"
+      ],
+      keys    => {
+        'nginx' => {
+          key     => '7BD9BF62',
+          key_url => 'http://nginx.org/keys/nginx_signing.key',
+        }
       }
     }
-  }
-  ->
+    ->
 
-  package { 'nginx':
-    ensure   => present,
-    provider => 'apt',
+    package { 'nginx':
+      ensure   => present,
+      provider => 'apt',
+    }
+
   }
+
 }
