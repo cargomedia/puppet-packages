d031306e3811d0aced2eb99fd9e21cff9c7360d4
ppp0 <philipp.gysin@cargomedia.ch>
2016-12-07 17:35:16 +0000

    Restore PR to status in #1508

--- modules/network/manifests/nat.pp
+++ modules/network/manifests/nat.pp
@@ -4,27 +4,15 @@ class network::nat (
   $to_source,
 ) {
 
+  include 'ufw'
+
   sysctl::entry { 'ip4_forward':
     entries => {
       'net.ipv4.ip_forward' => '1',
     }
   }
 
-  iptables::entry { 'Set up NAT':
-    table => 'nat',
-    chain => 'POSTROUTING',
-    rule  => "-o ${ifname_public} -j SNAT --to-source ${to_source}",
-  }
-  ->
-
-  iptables::entry { 'Allow inbound traffic for established connection':
-    chain => 'FORWARD',
-    rule  => "-i ${ifname_public} -o ${ifname_private} -m state --state RELATED,ESTABLISHED -j ACCEPT",
-  }
-  ->
-
-  iptables::entry { 'Allow outbound traffic':
-    chain => 'FORWARD',
-    rule  => "-i ${ifname_private} -o ${ifname_public} -j ACCEPT",
+  ufw::rules::before { 'snat':
+    rules_content => template("${module_name}/snat.rules.erb"),
   }
 }
