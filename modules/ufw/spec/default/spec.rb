bb5bf095ed99d5a640cb6f137011f98c8df6c574
ppp0 <philipp.gysin@cargomedia.ch>
2016-12-07 19:48:38 +0000

    Add rules to let traffic to the private network through, adapt specs

--- modules/ufw/spec/default/spec.rb
+++ modules/ufw/spec/default/spec.rb
@@ -6,23 +6,12 @@ describe 'ufw::default' do
     it { should be_installed }
   end
 
-  describe file('/var/log/ufw/ufw.log.1') do
-    it { should be_file }
-    its(:content) { should match /foo to bar/}
-  end
-
-  describe file ('/var/log/syslog.1') do
-    its(:content) { should_not match /foo to bar/}
-  end
-
-  describe file ('/var/log/ufw/ufw.log') do
-    it { should be_file }
-  end
-
   describe iptables do
     it { should have_rule('-s 10.0.0.0/8').with_table('filter').with_chain('ufw-before-input') }
     it { should have_rule('-s 172.16.0.0/12').with_table('filter').with_chain('ufw-before-input') }
     it { should have_rule('-s 192.168.0.0/16').with_table('filter').with_chain('ufw-before-input') }
+    it { should have_rule('-d 10.0.0.0/8').with_table('filter').with_chain('ufw-before-input') }
+    it { should have_rule('-d 172.16.0.0/12').with_table('filter').with_chain('ufw-before-input') }
+    it { should have_rule('-d 192.168.0.0/16').with_table('filter').with_chain('ufw-before-input') }
   end
-
 end
