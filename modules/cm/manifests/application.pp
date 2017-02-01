0f4f469443ec726a059ed22967c42857e3b52874
Alexis Peter <alexis.peter@cargomedia.ch>
2017-02-01 16:32:18 +0100

    removed remaining references to svm module

--- modules/cm/manifests/application.pp
+++ modules/cm/manifests/application.pp
@@ -13,7 +13,6 @@ class cm::application (
   require 'php5::extension::curl'
   require 'php5::extension::runkit'
   require 'php5::extension::stats'
-  require 'php5::extension::svm'
   require 'php5::extension::mcrypt'
   require 'php5::extension::gearman'
   require 'php5::extension::mongo'
