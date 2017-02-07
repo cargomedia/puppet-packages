46a73fe8d6c382ae8ca6994c5c22476e52c376ce
Krzysztof Stasiak <me@krzysztof-stasiak.com>
2017-02-07 16:29:42 +0100

    Added upsource application

---
+++ modules/jetbrains/manifests/upsource.pp
@@ -0,0 +1,20 @@
+class jetbrains::upsource (
+  $host,
+  $ssl_cert,
+  $ssl_key,
+  $version = '3.5',
+  $build   = '3597',
+  $port    = 8082,
+) {
+
+  jetbrains::application { 'upsource':
+    host         => $host,
+    ssl_cert     => $ssl_cert,
+    ssl_key      => $ssl_key,
+    version      => $version,
+    build        => $build,
+    port         => $port,
+    download_url => "https://download.jetbrains.com/upsource/upsource-${version}.${build}.zip"
+  }
+
+}
