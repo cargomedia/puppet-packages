4580d1519e0bce910b6cfff52d6f6f09901a4892
Krzysztof Stasiak <me@krzysztof-stasiak.com>
2017-02-07 13:20:55 +0100

    Simplified install script

--- modules/jetbrains/templates/install_hub.sh
+++ modules/jetbrains/templates/install_hub.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /tmp
 curl -L https://download.jetbrains.com/hub/<%= @version %>/hub-ring-bundle-<%= @version %>.<%= @build %>.zip > hub-ring-bundle.zip
 unzip hub-ring-bundle.zip
 cp -R hub-ring-bundle-<%= @version %>.<%= @build %>/* <%= @home_path %>/
 echo <%= @version %>.<%= @build %> > <%= @home_path %>/version.docker.image
-rm -f hub-ring-bundle.zip
