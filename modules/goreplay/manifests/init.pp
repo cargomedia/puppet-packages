ccf65f73af4cf5cff83684c34f581e006b831e5d
ppp0 <philipp.gysin@cargomedia.ch>
2017-03-07 18:57:44 +0100

    Refactorings according to review

--- modules/goreplay/manifests/init.pp
+++ modules/goreplay/manifests/init.pp
@@ -1,7 +1,10 @@
-class goreplay( $version = '0.15.1') {
+class goreplay ($version = '0.15.1') {
+
+  $regexed_version = regsubst($version, '\.', '\\.', 'G')
 
   helper::script { 'install goreplay':
     content => template("${module_name}/install.sh.erb"),
-    unless  => 'which gor',
+    unless  => "/usr/local/bin/gor 2>/dev/null | grep -E '^Version: v${regexed_version}'",
   }
+
 }
