0babdbf4fa934c1860f4840c3c99d7dd0bc3ef74
Reto Kaiser <reto@retokaiser.com>
2017-05-04 08:40:26 +0200

    Configure polipo proxy

---
+++ spec/docker/Debian-8/conf/proxy-env.sh
@@ -0,0 +1,6 @@
+export HTTP_PROXY=http://localhost:8123/
+export http_proxy=http://localhost:8123/
+export HTTPS_PROXY=http://localhost:8123/
+export https_proxy=http://localhost:8123/
+export NO_PROXY="127.0.0.1,localhost"
+export no_proxy="127.0.0.1,localhost"
