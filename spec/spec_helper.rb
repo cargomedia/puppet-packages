eb88492f7fe34c96a90577bb4e943202a4ca3475
Reto Kaiser <reto@retokaiser.com>
2017-05-04 08:40:47 +0200

    Interactive debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,30 +5,62 @@ require 'lib/puppet'
 
 RSpec.configure do |configuration|
 
+  dir_root = File.expand_path('../../', __FILE__)
   debug = ENV['debug']
   keep_box = ENV['keep_box'] # todo
-  box_name = ENV['box'] # todo
-  box = Box.new(Dir.getwd)
+  box_name = ENV['box']
+  box = Box.new(dir_root)
 
   configuration.raise_errors_for_deprecations!
 
-  docker_dir = File.expand_path("../../spec/docker/#{box_name}/", __FILE__)
-  docker_image = Docker::Image.build_from_dir(docker_dir)
+  docker_image = Docker::Image.build_from_dir("#{dir_root}/spec/docker/#{box_name}/")
 
   Specinfra.configuration.backend = :docker
   Specinfra.configuration.docker_image = docker_image.id
   Specinfra.configuration.docker_container_create_options = {
     'HostConfig' => {
       'Binds' => [
-        box.dir_outside.to_s + ':' + box.dir_inside.to_s
+        box.dir_outside.to_s + ':' + box.dir_inside.to_s,
+        "#{dir_root}/spec/.proxy_cache:/tmp/proxy-cache"
       ]
     }
   }
+  Specinfra.configuration.docker_container_exec_options = {
+  }
+
+  def start_debug
+    interactive_debug = true
+
+    if interactive_debug
+      puts "Something went wrong..."
+      # todo: print error information
+      container_id = Specinfra.backend.instance_variable_get(:@container).id
+      puts 'Run the following command to attach to the running container:'
+      puts "  sudo docker exec -it #{container_id} bash"
+      sleep(1000)
+      # todo: ask user to retry operation, or continue
+    end
+  end
 
   configuration.before :all do
     puppet = Puppet.new(box, self, debug)
     puppet.apply_facts
     puppet.configure_hiera
-    puppet.apply_manifests
+
+    begin
+      puppet.apply_manifests
+    rescue => e
+      start_debug
+      raise e
+    end
+  end
+
+  configuration.around(:each) do |example|
+    begin
+      example.run
+      raise if example.exception
+    rescue => e
+      start_debug
+    end
   end
 end
