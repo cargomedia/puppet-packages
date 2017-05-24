97a7991280114a0167ffe6b08cc78f360609de01
Michel Meyer <michel.meyer@cargomedia.ch>
2017-05-24 16:45:21 +0200

    Move journald transform in a config with high prio

--- modules/fluentd/manifests/config/source_journald.pp
+++ modules/fluentd/manifests/config/source_journald.pp
@@ -19,18 +19,6 @@ define fluentd::config::source_journald (
         path /var/lib/fluentd/journald_pos_<%= @name %>
       </storage>
     </source>
-
-    <filter <%= @fluentd_tag %>.**>
-      @type record_transformer
-      renew_record true
-      enable_ruby true
-      keep_keys message,timestamp,journal
-      <record>
-        message ${record["MESSAGE"]}
-        timestamp ${time.iso8601(3)}
-        journal ${r=record;{'transport' => r["_TRANSPORT"], 'unit' => r["_SYSTEMD_UNIT"], 'pid' => r["_PID"], 'uid' => r["_UID"]}}
-      </record>
-    </filter>
     |- EOT
 
     fluentd::config { "source-journald-${name}":
@@ -38,4 +26,22 @@ define fluentd::config::source_journald (
       content  => inline_template($config_template),
     }
 
+    $tranformer_config = @(EOT)
+    <filter <%= @fluentd_tag %>.**>
+              @type record_transformer
+              renew_record true
+              enable_ruby true
+              keep_keys level,message,timestamp,journal
+              <record>
+                message ${record["MESSAGE"]}
+                timestamp ${time.iso8601(3)}
+                journal ${r=record;{'transport' => r["_TRANSPORT"], 'unit' => r["_SYSTEMD_UNIT"], 'pid' => r["_PID"], 'uid' => r["_UID"]}}
+              </record>
+            </filter>
+    |- EOT
+
+    fluentd::config { "tranformer-journald-${name}":
+      priority => 60,
+      content  => inline_template($tranformer_config)
+    }
   }
