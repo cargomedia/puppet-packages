392e5c60a1b2a809bc8e5b42386bb1e3585ca635
Michel Meyer <michel.meyer@cargomedia.ch>
2017-06-02 09:51:56 +0200

    Test if fluentd log are received from journal

--- modules/fluentd/spec/source_journald/1-manifest.pp
+++ modules/fluentd/spec/source_journald/1-manifest.pp
@@ -6,6 +6,20 @@ node default {
     fluentd_output => true,
   }
 
+  file { '/tmp/my-source':
+    ensure => file,
+    mode   => '0644',
+    group  => '0',
+    owner  => '0',
+  }
+
+  fluentd::config::source_tail { 'my-source':
+    path        => '/tmp/my-source',
+    fluentd_tag => 'source',
+    format      => '/foo (?<message>.+)/',
+    require     => File['/tmp/my-source'],
+  }
+
   fluentd::config::match_copy { 'dump_to_file':
     pattern  => '**',
     priority => 85,
