cbbe5e1f87c1114d70531e60e3ed5d373b2a3d32
Michel Meyer <michel.meyer@cargomedia.ch>
2017-06-20 12:23:04 +0200

    Fix syntax

--- modules/apt/manifests/source/newrelic_infra.pp
+++ modules/apt/manifests/source/newrelic_infra.pp
@@ -6,8 +6,8 @@ class apt::source::newrelic_infra {
     ],
     keys    => {
       'newrelic-infra' => {
-        key        => "A758B3FBCD43BE8D123A3476BB29EE038ECCE87C",
-        key_url    => "https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg",
+        key        => 'A758B3FBCD43BE8D123A3476BB29EE038ECCE87C',
+        key_url    => 'https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg',
       }
     }
   }
