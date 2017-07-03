82e76cc6d2c258517b46d319359c1a77b350fd6f
Philipp Pfaff <philipp.pfaff@cargomedia.ch>
2017-07-03 17:15:33 +0200

    Remove everything log file from UFW

--- modules/ufw/manifests/init.pp
+++ modules/ufw/manifests/init.pp
@@ -28,11 +28,6 @@ class ufw (
       purge   => true,
       recurse => true,
       require => Package['ufw'];
-    '/var/log/ufw':
-      ensure  => directory,
-      owner   => '0',
-      group   => '0',
-      mode    => '0644';
     '/etc/default/ufw':
       ensure  => file,
       content => template("${module_name}/default.ufw.erb"),
@@ -45,26 +40,10 @@ class ufw (
       owner   => '0',
       group   => '0',
       mode    => '0644';
-    '/var/log/ufw/ufw.log':
-      ensure  => file,
-      owner   => '0',
-      group   => '0',
-      mode    => '0644',
-      before  => Rsyslog::Config['20-ufw'];
   }
 
   ufw::rules::before {['00-default_dist', '10-private_network_allow']:
     require => [File['/etc/ufw/before.d'], Package['ufw']],
-  }
-
-  rsyslog::config { '20-ufw':
-    content => template("${module_name}/rsyslog.erb"),
-  }
-
-  logrotate::entry { $module_name:
-    path              => '/var/log/ufw/ufw.log',
-    rotation_newfile  => 'create 0644',
-    postrotate_script => '/etc/init.d/rsyslog rotate > /dev/null 2>&1 || true',
   }
 
   Ufw::Rules::Before <| |> ~> Exec['Rebuild before.rules']
