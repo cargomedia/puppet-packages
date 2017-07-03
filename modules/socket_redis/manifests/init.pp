923bec3c2c22f789a4cc3c014842625bd3d68c6c
Philipp Pfaff <philipp.pfaff@cargomedia.ch>
2017-07-03 14:08:07 +0200

    Remove file-based logging from socket-redis

--- modules/socket_redis/manifests/init.pp
+++ modules/socket_redis/manifests/init.pp
@@ -2,7 +2,6 @@ class socket_redis (
   $version     = 'latest',
   $redisHost   = 'localhost',
   $socketPorts = [8090],
-  $logDir      = '/var/log/socket-redis',
   $statusPort  = '8085',
   $statusToken = undef,
 ) {
@@ -18,19 +17,8 @@ class socket_redis (
     system => true,
   }
 
-  file { $logDir:
-    ensure => directory,
-    owner  => 'socket-redis',
-    group  => 'socket-redis',
-    mode   => '0644',
-  }
-
-  logrotate::entry { $module_name:
-    path => "${logDir}/*.log",
-  }
-
   $arg1 = '/usr/bin/socket-redis'
-  $arg2 = "--log-dir=${logDir} --status-port=${statusPort} --redis-host=${redisHost}"
+  $arg2 = "--status-port=${statusPort} --redis-host=${redisHost}"
   $arg3 = inline_template("--socket-ports=<%= @socketPorts.join(',')%>")
   $arg4 = $statusToken ? { undef => '', default => "--status-secret=${statusToken}" }
 
@@ -39,7 +27,7 @@ class socket_redis (
     args         => "${arg1} ${arg2} ${arg3} ${arg4}",
     user         => 'socket-redis',
     limit_nofile => 10000,
-    require      => [Package['socket-redis'], File[$logDir]],
+    require      => [Package['socket-redis']],
   }
 
   package { 'socket-redis':
