1fb5daf992bf9810ad5fa491774f761631497145
Philipp Pfaff <philipp.pfaff@cargomedia.ch>
2017-07-05 17:19:37 +0200

    Remove all replication checks

--- modules/mysql/manifests/server/slave.pp
+++ modules/mysql/manifests/server/slave.pp
@@ -16,27 +16,4 @@ class mysql::server::slave (
     before  => Package['mysql-server'],
     notify  => Service['mysql'],
   }
-  ->
-
-  file { '/usr/local/bin/mysql-replication-check':
-    ensure  => file,
-    content => template("${module_name}/replication-check.sh"),
-    owner   => '0',
-    group   => '0',
-    mode    => '0755',
-    require => [Service['mysql'], Bipbip::Entry['is-replication-running']],
-  }
-
-  mysql::user { 'bipbip@localhost':
-    password => '',
-    require => Bipbip::Entry['is-replication-running'],
-  }
-
-  @bipbip::entry { 'is-replication-running':
-    plugin  => 'command',
-    options => {
-      'command'      => '/usr/local/bin/mysql-replication-check bipbip',
-      'metric_group' => 'mysql',
-    },
-  }
 }
