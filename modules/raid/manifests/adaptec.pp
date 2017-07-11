914eb6ac30fa910e24cde66c4ccc74535c74b202
Michel Meyer <michel.meyer@cargomedia.ch>
2017-07-11 14:59:36 +0200

    Use raid-adpatec.rb script + add bipbip to sudoers

--- modules/raid/manifests/adaptec.pp
+++ modules/raid/manifests/adaptec.pp
@@ -11,31 +11,27 @@ class raid::adaptec {
     require  => Class['apt::source::cargomedia'],
   }
 
-  file { '/usr/local/sbin/aacraid-status':
+  file { '/usr/local/sbin/raid-adpatec':
     ensure  => file,
-    content => template("${module_name}/adaptec/aacraid-status"),
+    content => template("${module_name}/adaptec/raid-adpatec.rb"),
     owner   => '0',
     group   => '0',
     mode    => '0755',
   }
 
-  @monit::entry { 'raid-adaptec':
-    content => template("${module_name}/adaptec/monit"),
-    require => File['/usr/local/sbin/aacraid-status'],
+  @bipbip::entry { 'raid-adaptec':
+    plugin  => 'command_status',
+    command => '/usr/local/sbin/raid-adpatec status',
+    require => File['/usr/local/sbin/raid-adpatec'],
   }
 
-  file { '/usr/local/sbin/arcconf-write-cache-on-devices.pl':
-    ensure  => file,
-    content => template("${module_name}/adaptec/arcconf-write-cache-on-devices.pl"),
-    owner   => '0',
-    group   => '0',
-    mode    => '0755',
+  @sudo::config { 'raid-bipbip':
+    content => 'bipbip ALL=NOPASSWD: /usr/sbin/arcconf',
   }
-  ->
 
   helper::script { 'set hard drive write cache off if adaptec raid':
     content => template("${module_name}/adaptec/set-write-cache-off.sh"),
-    unless  => 'test "$(/usr/local/sbin/arcconf-write-cache-on-devices.pl)" = ""',
-    require => Package['arcconf'],
+    unless  => 'test "$(/usr/local/sbin/raid-adpatec list_devices_with_cache)" != "off"',
+    require => [Package['arcconf'], File['/usr/local/sbin/raid-adpatec']],
   }
 }
