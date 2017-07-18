28078bffa4b71f1debfec7fc75f47ebd57375a33
Michel Meyer <michel.meyer@cargomedia.ch>
2017-07-18 12:21:59 +0200

    Add Augeas usage example

--- modules/augeas/spec/init/manifest.pp
+++ modules/augeas/spec/init/manifest.pp
@@ -1,4 +1,26 @@
 node default {
 
-  class { 'augeas': }
+  class {'augeas': }
+
+  $config = @("END"/L)
+  foo=bar
+  bar=1
+  | END
+
+  file { '/tmp/foo':
+    ensure  => present,
+    content => $config,
+  }
+
+  $changes = @("END"/L)
+  set bar 42
+  set baz 22
+  | END
+
+  augeas { 'config_foo':
+    incl    => '/tmp/foo',
+    lens    => 'Shellvars.lns',
+    changes => $changes,
+    require => File['/tmp/foo'],
+  }
 }
