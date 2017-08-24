b03cd6882447fc72bf4350100152984fc711c61b
Philipp Pfaff <philipp.pfaff@cargomedia.ch>
2017-08-24 17:06:57 +0200

    Alert separation stopped vs failed

--- modules/systemd/manifests/critical_units.pp
+++ modules/systemd/manifests/critical_units.pp
@@ -11,11 +11,21 @@ class systemd::critical_units {
     purge => true,
   }
 
-  @bipbip::entry { 'critical-units':
-    plugin  => 'systemd-unit',
+  @bipbip::entry { 'critical-units-failing':
+    plugin  => 'command-status',
     options => {
       'unit_name'    => 'critical-units.target',
-      'metric_group' => 'critical-units',
+      'metric_group' => 'critical-units-failing',
+      'command' => 'for s in `sudo systemctl --plain list-dependencies critical-units.target | cut -d" " -f 2`;do sudo systemctl is-failed $s;if [ ${?} -eq 0 ];then exit 1;fi;done;',
+    },
+  }
+
+  @bipbip::entry { 'critical-units-stopped':
+    plugin  => 'command-status',
+    options => {
+      'unit_name'    => 'critical-units.target',
+      'metric_group' => 'critical-units-stopped',
+      'command' => 'for s in `sudo systemctl --plain list-dependencies critical-units.target | cut -d" " -f 2`;do sudo systemctl is-active $s;if ! [ ${?} -eq 0 ];then exit 1;fi;done;',
     },
   }
 }
