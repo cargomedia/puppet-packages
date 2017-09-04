70eb5b97702844951612e0d026b2900874cc34fd
Michel Meyer <michel.meyer@cargomedia.ch>
2017-09-04 17:25:24 +0200

    Add ExitOnForwardFailure=yes to autossh

--- modules/autossh/manifests/init.pp
+++ modules/autossh/manifests/init.pp
@@ -16,6 +16,7 @@ define autossh (
     'ServerAliveInterval'   => 30,
     'ServerAliveCountMax'   => 3,
     'StrictHostKeyChecking' => 'no',
+    'ExitOnForwardFailure'  => 'yes',
   })
 
   $argForwards = autossh_forwards($forwards)
