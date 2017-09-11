4344108a48595f50ba82a0ddf02bd5416ef8d4da
tomaszdurka <tomasz@durka.pl>
2017-09-11 16:06:56 +0200

    Replace daemon critical units check with bipbip systemd plugin

--- modules/systemd/manifests/critical_units.pp
+++ modules/systemd/manifests/critical_units.pp
@@ -11,19 +11,11 @@ class systemd::critical_units {
     purge    => true,
   }
 
-  $check_daemon_name = 'critical-units-check'
-
-  file { "/usr/local/bin/${check_daemon_name}":
-    ensure   => file,
-    content  => template("${module_name}/monitoring/${check_daemon_name}.sh"),
-    owner    => '0',
-    group    => '0',
-    mode     => '0755',
-    notify   => Daemon[$check_daemon_name],
-  }
-
-  daemon { $check_daemon_name:
-    binary  => "/usr/local/bin/${check_daemon_name}",
-    require => File["/usr/local/bin/${check_daemon_name}"],
+  @bipbip::entry { 'critical-units':
+    plugin  => 'systemd-unit',
+    options => {
+      'unit_name'    => 'critical-units.target',
+      'metric_group' => 'critical-units',
+    },
   }
 }
