90e534940273cd69697a95313d27b78db1e6a507
Philipp Pfaff <philipp.pfaff@cargomedia.ch>
2017-11-08 18:13:56 +0100

    Adapt tests for network class

--- modules/network/spec/default/manifest.pp
+++ modules/network/spec/default/manifest.pp
@@ -1,10 +1,4 @@
 node default {
 
   require 'network'
-
-  class { 'network::resolv':
-    search     => ['example.local', 'example.com'],
-    nameserver => ['172.168.1.2', '8.8.8.8'],
-    domain     => 'example.com',
-  }
 }
