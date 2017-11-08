62f7b8f315a57e4bbfebd89c340a81eaad633bb1
Philipp Pfaff <philipp.pfaff@cargomedia.ch>
2017-11-08 18:10:34 +0100

    introduce network::route and spec test

---
+++ modules/network/manifests/route.pp
@@ -0,0 +1,22 @@
+define network::route (
+  $destination = 'default gw',
+  $via,
+)
+{
+  # This file is only executed at boot time
+  file { "/etc/network/if-up.d/99-${title}":
+    ensure  => file,
+    content => template("${module_name}/route/if-up.erb"),
+    owner   => '0',
+    group   => '0',
+    mode    => '0750',
+  }
+
+  exec { "[${title}] Set route to ${destination} via ${via}":
+    provider => shell,
+    command  => "ip route add ${destination} via ${via}",
+    unless   => "ip ro sh | grep -q '${destination} via ${via}'",
+    path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    user     => 'root',
+  }
+}
