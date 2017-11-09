eb09856376788291866803f183376a77b6224d90
Michel Meyer <michel.meyer@cargomedia.ch>
2017-11-09 13:55:15 +0100

    Use refreshonly and notification instead of unless

--- modules/network/manifests/route.pp
+++ modules/network/manifests/route.pp
@@ -1,8 +1,7 @@
 define network::route (
   $destination = 'default gw',
   $via,
-)
-{
+) {
   # This file is only executed at boot time
   file { "/etc/network/if-up.d/99-${title}":
     ensure  => file,
@@ -13,11 +12,11 @@ define network::route (
   }
 
   exec { "[${title}] Set route to ${destination} via ${via}":
-    provider => shell,
-    command  => "/etc/network/if-up.d/99-${title}",
-    unless   => "ip ro sh | grep -q '${destination} via ${via}'",
-    path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    user     => 'root',
-    require  => File["/etc/network/if-up.d/99-${title}"]
+    provider    => shell,
+    command     => "/etc/network/if-up.d/99-${title}",
+    refreshonly => true,
+    path        => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+    user        => 'root',
+    subscribe   => File["/etc/network/if-up.d/99-${title}"],
   }
 }
