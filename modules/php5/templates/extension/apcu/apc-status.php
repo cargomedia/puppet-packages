0fc64c1d37030880c6469182d60ce0e5dae8d4e7
tomaszdurka <tomasz@durka.pl>
2018-01-25 12:19:48 +0100

    Convert fpm scripts to prometheus format

--- modules/php5/templates/extension/apcu/apc-status.php
+++ modules/php5/templates/extension/apcu/apc-status.php
@@ -9,10 +9,18 @@ $total_mem_size = (int) $infoAlloc['num_seg'] * (int) $infoAlloc['seg_size'];
 $avail_mem_size = (int) $infoAlloc['avail_mem'];
 $used_mem_size = $total_mem_size - $avail_mem_size;
 
-echo json_encode(array(
-    'opcode_mem_size' => (int) $infoOpcode['mem_size'],
-    'user_mem_size'   => (int) $infoUser['mem_size'],
-    'total_mem_size'  => $total_mem_size,
-    'avail_mem_size'  => $avail_mem_size,
-    'used_mem_size'   => $used_mem_size,
-));
+$prefix = 'php';
+$metrics = [
+    'apc_opcode_mem_size_bytes' => (int) $infoOpcode['mem_size'],
+    'apc_user_mem_size_bytes'   => (int) $infoUser['mem_size'],
+    'apc_total_mem_size_bytes'  => $total_mem_size,
+    'apc_avail_mem_size_bytes'  => $avail_mem_size,
+    'apc_used_mem_size_bytes'   => $used_mem_size,
+];
+
+foreach ($metrics as $name => $value) {
+    $help = ucfirst(str_replace('_', ' ', $name));
+    echo "# HELP {$prefix}_{$name} {$help}\n";
+    echo "# TYPE {$prefix}_{$name} gauge\n";
+    echo "{$prefix}_{$name} {$value}\n";
+}
--- modules/php5/templates/extension/opcache/opcache-status.php
+++ modules/php5/templates/extension/opcache/opcache-status.php
@@ -1,4 +1,16 @@
 <?php
 
 $status = opcache_get_status(false);
-echo json_encode($status);
+$metrics = new RecursiveIteratorIterator(new RecursiveArrayIterator($status));
+$prefix = 'php_opcache';
+
+foreach ($metrics as $name => $value) {
+    $help = ucfirst(str_replace('_', ' ', $name));
+    echo "# HELP {$prefix}_{$name} {$help}\n";
+    echo "# TYPE {$prefix}_{$name} gauge\n";
+    echo "{$prefix}_{$name} {$value}\n";
+}
+
+
+
+
