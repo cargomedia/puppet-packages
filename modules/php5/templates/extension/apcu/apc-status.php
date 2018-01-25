5a2b03dcf7a76a2ff00128577e4cfc3493c52053
tomaszdurka <tomasz@durka.pl>
2018-01-25 14:15:29 +0100

    Adjust metrics to bipbip rules

--- modules/php5/templates/extension/apcu/apc-status.php
+++ modules/php5/templates/extension/apcu/apc-status.php
@@ -8,19 +8,36 @@ $infoAlloc = @apc_sma_info(true);
 $total_mem_size = (int) $infoAlloc['num_seg'] * (int) $infoAlloc['seg_size'];
 $avail_mem_size = (int) $infoAlloc['avail_mem'];
 $used_mem_size = $total_mem_size - $avail_mem_size;
+$used_mem_percentage = $used_mem_size / $total_mem_size;
 
 $prefix = 'php';
 $metrics = [
-    'apc_opcode_mem_size_bytes' => (int) $infoOpcode['mem_size'],
-    'apc_user_mem_size_bytes'   => (int) $infoUser['mem_size'],
-    'apc_total_mem_size_bytes'  => $total_mem_size,
-    'apc_avail_mem_size_bytes'  => $avail_mem_size,
-    'apc_used_mem_size_bytes'   => $used_mem_size,
+    [
+        'name'  => 'apc_opcode_memory_bytes',
+        'type'  => 'gauge',
+        'value' => (int) $infoOpcode['mem_size'],
+    ],
+    [
+        'name'  => 'apc_user_memory_bytes',
+        'type'  => 'gauge',
+        'value' => (int) $infoUser['mem_size'],
+    ],
+    [
+        'name'  => 'apc_used_memory_bytes',
+        'type'  => 'gauge',
+        'value' => $used_mem_size,
+    ],
+    [
+        'name'  => 'apc_used_memory_ratio',
+        'type'  => 'gauge',
+        'value' => $used_mem_percentage,
+    ],
 ];
 
-foreach ($metrics as $name => $value) {
+foreach ($metrics as $metric) {
+    $name = $metric['name'];
     $help = ucfirst(str_replace('_', ' ', $name));
     echo "# HELP {$prefix}_{$name} {$help}\n";
-    echo "# TYPE {$prefix}_{$name} gauge\n";
-    echo "{$prefix}_{$name} {$value}\n";
+    echo "# TYPE {$prefix}_{$name} {$metric['type']}\n";
+    echo "{$prefix}_{$name} {$metric['value']}\n";
 }
--- modules/php5/templates/extension/opcache/opcache-status.php
+++ modules/php5/templates/extension/opcache/opcache-status.php
@@ -1,16 +1,48 @@
 <?php
-
 $status = opcache_get_status(false);
-$metrics = new RecursiveIteratorIterator(new RecursiveArrayIterator($status));
-$prefix = 'php_opcache';
+$prefix = 'php';
+$metrics = [
+    [
+        'name'  => 'opcache_free_memory_bytes',
+        'type'  => 'gauge',
+        'value' => $status['memory_usage']['free_memory'],
+    ],
+    [
+        'name'  => 'opcache_current_wasted_ratio',
+        'type'  => 'gauge',
+        'value' => $status['memory_usage']['current_wasted_percentage'],
+    ],
+    [
+        'name'  => 'opcache_num_cached_keys',
+        'type'  => 'gauge',
+        'value' => $status['opcache_statistics']['num_cached_keys'],
+    ],
+    [
+        'name'  => 'opcache_hit_rate_ratio',
+        'type'  => 'gauge',
+        'value' => $status['opcache_statistics']['opcache_hit_rate'],
+    ],
+    [
+        'name'  => 'opcache_misses_total',
+        'type'  => 'counter',
+        'value' => $status['opcache_statistics']['misses'],
+    ],
+    [
+        'name'  => 'opcache_hits_total',
+        'type'  => 'counter',
+        'value' => $status['opcache_statistics']['hits'],
+    ],
+    [
+        'name'  => 'opcache_oom_restarts_total',
+        'type'  => 'counter',
+        'value' => $status['opcache_statistics']['oom_restarts'],
+    ],
+];
 
-foreach ($metrics as $name => $value) {
+foreach ($metrics as $metric) {
+    $name = $metric['name'];
     $help = ucfirst(str_replace('_', ' ', $name));
     echo "# HELP {$prefix}_{$name} {$help}\n";
-    echo "# TYPE {$prefix}_{$name} gauge\n";
-    echo "{$prefix}_{$name} {$value}\n";
+    echo "# TYPE {$prefix}_{$name} {$metric['type']}\n";
+    echo "{$prefix}_{$name} {$metric['value']}\n";
 }
-
-
-
-
